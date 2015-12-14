ef1dbf7cbab83ce8678f16a3f04ed344e96e69a7
Chris Broadfoot <cbro@golang.org>
2015-12-14 11:16:49 -0800

    docs/appengine: add mailgun sample
    Change-Id: I82330897bd1bec1646c639e1fc274708e84a2bd1

---
+++ docs/appengine/mail/mailgun/mailgun_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/mailgun/mailgun-go"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
+)
+
+func SendSimpleMessageHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	httpc := urlfetch.Client(ctx)
+
+	mg := mailgun.NewMailgun(
+		"YOUR_DOMAIN_NAME", // Domain name
+		"YOUR_API_KEY",     // API Key
+		"YOUR_PUBLIC_KEY",  // Public Key
+	)
+	mg.SetClient(httpc)
+
+	msg, id, err := mg.Send(mg.NewMessage(
+		/* From */ "Excited User <mailgun@YOUR_DOMAIN_NAME>",
+		/* Subject */ "Hello",
+		/* Body */ "Testing some Mailgun awesomness!",
+		/* To */ "bar@example.com", "YOU@YOUR_DOMAIN_NAME",
+	))
+	if err != nil {
+		msg := fmt.Sprintf("Could not send message: %v, ID %d, %+v", err, id, msg)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("Message sent!"))
+}
+
+func SendComplexMessageHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	httpc := urlfetch.Client(ctx)
+
+	mg := mailgun.NewMailgun(
+		"YOUR_DOMAIN_NAME", // Domain name
+		"YOUR_API_KEY",     // API Key
+		"YOUR_PUBLIC_KEY",  // Public Key
+	)
+	mg.SetClient(httpc)
+
+	message := mg.NewMessage(
+		/* From */ "Excited User <mailgun@YOUR_DOMAIN_NAME>",
+		/* Subject */ "Hello",
+		/* Body */ "Testing some Mailgun awesomness!",
+		/* To */ "foo@example.com",
+	)
+	message.AddCC("baz@example.com")
+	message.AddBCC("bar@example.com")
+	message.SetHtml("<html>HTML version of the body</html>")
+	message.AddAttachment("files/test.jpg")
+	message.AddAttachment("files/test.txt")
+
+	msg, id, err := mg.Send(message)
+	if err != nil {
+		msg := fmt.Sprintf("Could not send message: %v, ID %d, %+v", err, id, msg)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("Message sent!"))
+}
