45f625b25a6dd067e71d6d3f0c21d845f88863b8
Chris Broadfoot <cbro@golang.org>
2016-04-03 21:36:10 -0700

    golang-samples: add test for license headers

---
+++ nothing.go
@@ -0,0 +1,7 @@
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package samples
+
+// This file is a workaround for golang.org/issue/15093.
