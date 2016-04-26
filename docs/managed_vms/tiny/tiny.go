daa88c2112b9e793ad7f2961b33c82d888644b5a
Chris Broadfoot <cbro@golang.org>
2016-04-25 21:31:21 -0700

    docs/managed_vms: add tiny (minimal program showing main structure)

---
+++ docs/managed_vms/tiny/tiny.go
@@ -0,0 +1,16 @@
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Sample tiny demonstrates overall program structure:
+// use of the main package and calling appengine.Main from the main function.
+package main
+
+import "google.golang.org/appengine"
+
+func main() {
+	// ...
+	appengine.Main()
+}
+
+// ...
