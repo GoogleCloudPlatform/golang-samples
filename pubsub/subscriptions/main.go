effef49d22e3f2523aca3e10ea62cbf35c1de50b
Jaana Burcu Dogan <burcujdogan@gmail.com>
2016-09-01 19:04:45 -0700

    pubsub: Add subscriptions (#100)
    * pubsub: Add subscriptions
    
    * pubsub: return errors from the calls
    
    * pubsub: address cbro review and add tests
    
    * pubsub: Fix wrong existence check on topics
    
    * pubsub: Remove println, fix subscription match
    
    * pubsub: Address review comments

---
+++ pubsub/subscriptions/main.go
@@ -0,0 +1,164 @@
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Command subscriptions is a tool to manage Google Cloud Pub/Sub subscriptions by using the Pub/Sub API.
+// See more about Google Cloud Pub/Sub at https://cloud.google.com/pubsub/docs/overview.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	// [START imports]
+	"golang.org/x/net/context"
+
+	"cloud.google.com/go/pubsub"
+	// [END imports]
+)
+
+func main() {
+	ctx := context.Background()
+	// [START auth]
+	proj := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	if proj == "" {
+		fmt.Fprintf(os.Stderr, "GOOGLE_CLOUD_PROJECT environment variable must be set.\n")
+		os.Exit(1)
+	}
+	client, err := pubsub.NewClient(ctx, proj)
+	if err != nil {
+		log.Fatalf("Could not create pubsub Client: %v", err)
+	}
+	// [END auth]
+
+	// Print all the subscriptions in the project.
+	fmt.Println("Listing all subscriptions from the project:")
+	subs, err := list(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, sub := range subs {
+		fmt.Printf("%v\n", sub.Name())
+	}
+
+	t := createTopicIfNotExists(client)
+
+	const sub = "example-subscription"
+	// Create a new subscription.
+	if err := create(client, sub, t); err != nil {
+		log.Fatal(err)
+	}
+
+	// Pull messages via the subscription.
+	if err := pullMsgs(client, sub, t); err != nil {
+		log.Fatal(err)
+	}
+
+	// Delete the subscription.
+	if err := delete(client, sub); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func list(client *pubsub.Client) ([]*pubsub.Subscription, error) {
+	ctx := context.Background()
+	// [START get_all_subscriptions]
+	var subs []*pubsub.Subscription
+	it := client.Subscriptions(ctx)
+	for {
+		s, err := it.Next()
+		if err == pubsub.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		subs = append(subs, s)
+	}
+	// [END get_all_subscriptions]
+	return subs, nil
+}
+
+func pullMsgs(client *pubsub.Client, name string, topic *pubsub.Topic) error {
+	ctx := context.Background()
+
+	// publish 10 messages on the topic.
+	for i := 0; i < 10; i++ {
+		_, err := topic.Publish(ctx, &pubsub.Message{
+			Data: []byte(fmt.Sprintf("hello world #%d", i)),
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	// [START pull_messages]
+	sub := client.Subscription(name)
+	it, err := sub.Pull(ctx)
+	if err != nil {
+		return err
+	}
+	defer it.Stop()
+
+	// Consume 10 messages.
+	for i := 0; i < 10; i++ {
+		msg, err := it.Next()
+		if err == pubsub.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Got message: %q\n", string(msg.Data))
+		msg.Done(true)
+	}
+	// [END pull_messages]
+	return nil
+}
+
+func create(client *pubsub.Client, name string, topic *pubsub.Topic) error {
+	ctx := context.Background()
+	// [START create_subscription]
+	sub, err := client.NewSubscription(ctx, name, topic, 20*time.Second, nil)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Created subscription: %v\n", sub)
+	// [END create_subscription]
+	return nil
+}
+
+func delete(client *pubsub.Client, name string) error {
+	ctx := context.Background()
+	// [START delete_subscription]
+	sub := client.Subscription(name)
+	if err := sub.Delete(ctx); err != nil {
+		return err
+	}
+	fmt.Println("Subscription deleted.")
+	// [END delete_subscription]
+	return nil
+}
+
+func createTopicIfNotExists(c *pubsub.Client) *pubsub.Topic {
+	ctx := context.Background()
+
+	const topic = "example-topic"
+	// Create a topic to subscribe to.
+	t := c.Topic(topic)
+	ok, err := t.Exists(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if ok {
+		return t
+	}
+
+	t, err = c.NewTopic(ctx, topic)
+	if err != nil {
+		log.Fatalf("Failed to create the topic: %v", err)
+	}
+	return t
+}
---
+++ pubsub/subscriptions/main_test.go
@@ -0,0 +1,109 @@
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"sync"
+	"testing"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
+)
+
+var topic *pubsub.Topic
+
+const (
+	subName   = "golang-samples-subscription"
+	topicName = "golang-samples-topic"
+)
+
+var once sync.Once // guards cleanup related operations that needs to be executed only for once.
+
+func setup(t *testing.T) *pubsub.Client {
+	netctx := context.Background()
+	ctx := testutil.SystemTest(t)
+
+	client, err := pubsub.NewClient(netctx, ctx.ProjectID)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+
+	// Cleanup resources from the previous failed tests.
+	once.Do(func() {
+		// create a topic to subscribe to.
+		topic = client.Topic(topicName)
+		ok, err := topic.Exists(netctx)
+		if err != nil {
+			t.Fatalf("failed to check if topic exists: %v", err)
+		}
+		if !ok {
+			if topic, err = client.NewTopic(netctx, topicName); err != nil {
+				t.Fatalf("failed to create the topic: %v", err)
+			}
+		}
+
+		// delete the sub if already exists
+		sub := client.Subscription(subName)
+		ok, err = sub.Exists(netctx)
+		if err != nil {
+			t.Fatalf("failed to check if sub exists: %v", err)
+		}
+		if ok {
+			if err := client.Subscription(subName).Delete(netctx); err != nil {
+				t.Fatalf("failed to cleanup the topic (%q): %v", subName, err)
+			}
+		}
+	})
+	return client
+}
+
+func TestCreate(t *testing.T) {
+	c := setup(t)
+	if err := create(c, subName, topic); err != nil {
+		t.Fatalf("failed to create a subscription: %v", err)
+	}
+	ok, err := c.Subscription(subName).Exists(context.Background())
+	if err != nil {
+		t.Fatalf("failed to check if sub exists: %v", err)
+	}
+	if !ok {
+		t.Fatalf("got none; want sub = %q", subName)
+	}
+}
+
+func TestList(t *testing.T) {
+	c := setup(t)
+
+	subs, err := list(c)
+	if err != nil {
+		t.Fatalf("failed to list subscriptions: %v", err)
+	}
+	var ok bool
+	s := c.Subscription(subName)
+	for _, sub := range subs {
+		if s.Name() == sub.Name() {
+			ok = true
+			break
+		}
+	}
+	if !ok {
+		t.Fatalf("got %+v; want a list with subscription %q", subs, subName)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	c := setup(t)
+	if err := delete(c, subName); err != nil {
+		t.Fatalf("failed to delete subscription (%q): %v", subName, err)
+	}
+	ok, err := c.Subscription(subName).Exists(context.Background())
+	if err != nil {
+		t.Fatalf("failed to check if sub exists: %v", err)
+	}
+	if ok {
+		t.Fatalf("got sub = %q; want none", subName)
+	}
+}
