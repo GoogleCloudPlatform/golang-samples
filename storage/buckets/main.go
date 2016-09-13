1b3b5e9545f27a808418a4e4e3685fe17434da14
Jaana Burcu Dogan <burcujdogan@gmail.com>
2016-09-13 14:41:10 -0700

    storage: add buckets sample

---
+++ storage/buckets/main.go
@@ -0,0 +1,95 @@
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Sample buckets creates a bucket, lists buckets and deletes a bucket
+// using the Google Storage API. More documentation is available at
+// https://cloud.google.com/storage/docs/json_api/v1/.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"cloud.google.com/go/storage"
+)
+
+func main() {
+	ctx := context.Background()
+
+	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	if projectID == "" {
+		fmt.Fprintf(os.Stderr, "GOOGLE_CLOUD_PROJECT environment variable must be set.\n")
+		os.Exit(1)
+	}
+
+	// [START setup]
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// [END setup]
+
+	// Give the bucket a unique name.
+	name := fmt.Sprintf("golang-example-buckets-%d", time.Now().Unix())
+	if err := create(client, projectID, name); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("created bucket: %v\n", name)
+
+	// list buckets from the project
+	buckets, err := list(client, projectID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("buckets: %+v\n", buckets)
+
+	// delete the bucket
+	if err := delete(client, name); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("deleted bucket: %v\n", name)
+}
+
+func create(client *storage.Client, projectID, bucketName string) error {
+	ctx := context.Background()
+	// [START create_bucket]
+	if err := client.Bucket(bucketName).Create(ctx, projectID, nil); err != nil {
+		return err
+	}
+	// [END create_bucket]
+	return nil
+}
+
+func list(client *storage.Client, projectID string) ([]string, error) {
+	ctx := context.Background()
+	// [START list_buckets]
+	var buckets []string
+	it := client.Buckets(ctx, projectID)
+	for {
+		battrs, err := it.Next()
+		if err == storage.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		buckets = append(buckets, battrs.Name)
+	}
+	// [END list_buckets]
+	return buckets, nil
+}
+
+func delete(client *storage.Client, bucketName string) error {
+	ctx := context.Background()
+	// [START delete_bucket]
+	if err := client.Bucket(bucketName).Delete(ctx); err != nil {
+		return err
+	}
+	// [END delete_bucket]
+	return nil
+}
---
+++ storage/buckets/main_test.go
@@ -0,0 +1,66 @@
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
+
+	"cloud.google.com/go/storage"
+	"golang.org/x/net/context"
+)
+
+var bucketName = fmt.Sprintf("golang-example-buckets-%d", time.Now().Unix())
+
+func setup(t *testing.T) *storage.Client {
+	ctx := context.Background()
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+	return client
+}
+
+func TestCreate(t *testing.T) {
+	tc := testutil.SystemTest(t)
+	c := setup(t)
+	if err := create(c, tc.ProjectID, bucketName); err != nil {
+		t.Fatalf("failed to create bucket (%q): %v", bucketName, err)
+	}
+}
+
+func TestList(t *testing.T) {
+	tc := testutil.SystemTest(t)
+	c := setup(t)
+	buckets, err := list(c, tc.ProjectID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var ok bool
+outer:
+	for attempt := 0; attempt < 5; attempt++ { // for eventual consistency
+		for _, b := range buckets {
+			if b == bucketName {
+				ok = true
+				break outer
+			}
+		}
+		time.Sleep(2 * time.Second)
+	}
+	if !ok {
+		t.Errorf("got bucket list: %v; want %q in the list", buckets, bucketName)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	c := setup(t)
+	if err := delete(c, bucketName); err != nil {
+		t.Fatalf("failed to delete bucket (%q): %v", bucketName, err)
+	}
+}
