91e2123fb5be0bc08a23d10078db6c4f6a0e02a1
Chris Broadfoot <cbro@golang.org>
2016-09-15 23:39:44 -0700

    speech/caption: use cloud.google.com/go/speech/apiv1beta1 package

--- speech/caption/caption_test.go
+++ speech/caption/caption_test.go
@@ -5,49 +5,27 @@
 package main
 
 import (
-	"io/ioutil"
 	"testing"
 
 	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
-
-	"golang.org/x/net/context"
-	"google.golang.org/api/option"
-	"google.golang.org/api/transport"
-	speech "google.golang.org/genproto/googleapis/cloud/speech/v1beta1"
 )
 
 func TestRecognize(t *testing.T) {
 	testutil.SystemTest(t)
 
-	ctx := context.Background()
-	conn, err := transport.DialGRPC(ctx,
-		option.WithEndpoint("speech.googleapis.com:443"),
-		option.WithScopes("https://www.googleapis.com/auth/cloud-platform"),
-	)
+	resp, err := recognize("./quit.raw")
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer conn.Close()
-	c := speech.NewSpeechClient(conn)
-
-	data, err := ioutil.ReadFile("./quit.raw")
-	if err != nil {
-		t.Fatal(err)
+	if len(resp.Results) == 0 {
+		t.Fatal("got no results; want at least one")
 	}
 
-	rresp, err := recognize(ctx, c, &data)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(rresp.Results) < 1 {
-		t.Fatal("want recognize results; got none")
-	}
-
-	result := rresp.Results[0]
+	result := resp.Results[0]
 	if len(result.Alternatives) < 1 {
 		t.Fatal("got no alternatives; want at least one")
 	}
 	if got, want := result.Alternatives[0].Transcript, "quit"; got != want {
-		t.Errorf("got transcript: %q; want %q", got, want)
+		t.Errorf("Transcript: got %q; want %q", got, want)
 	}
 }
