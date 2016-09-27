32476bc5d4bbe72d5a67d8952f28b82936d0fe7b
Jaana Burcu Dogan <jbd@google.com>
2016-09-27 13:52:01 -0700

    logging: fix the broken build

--- logging/simplelog/simplelog.go
+++ logging/simplelog/simplelog.go
@@ -14,8 +14,8 @@ import (
 	"golang.org/x/net/context"
 
 	// [START imports]
-	// NOTE: This will become cloud.google.com/go/logging soon.
-	"cloud.google.com/go/preview/logging"
+	"cloud.google.com/go/logging"
+	"cloud.google.com/go/logging/logadmin"
 
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
@@ -35,13 +35,19 @@ func main() {
 
 	// [START setup]
 	ctx := context.Background()
-	client, err := logging.NewClient(ctx, projID,
-		// Admin scope is required to delete logs.
-		option.WithScopes(logging.AdminScope))
-
+	client, err := logging.NewClient(ctx, projID)
 	if err != nil {
 		log.Fatalf("Failed to create logging client: %v", err)
 	}
+
+	adminClient, err := logadmin.NewClient(ctx, projID,
+		// Admin scope is required to delete logs.
+		option.WithScopes(logging.AdminScope),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create logadmin client: %v", err)
+	}
+
 	client.OnError = func(err error) {
 		// Print an error to the local log.
 		// For example, if Flush() failed.
@@ -57,7 +63,7 @@ func main() {
 
 	case "read":
 		log.Print("Fetching and printing log entries.")
-		entries, err := getEntries(client, projID)
+		entries, err := getEntries(adminClient, projID)
 		if err != nil {
 			log.Fatalf("Could not get entries: %v", err)
 		}
@@ -71,7 +77,7 @@ func main() {
 
 	case "delete":
 		log.Print("Deleting log.")
-		if err := deleteLog(client); err != nil {
+		if err := deleteLog(adminClient); err != nil {
 			log.Fatalf("Could not delete log: %v", err)
 		}
 
@@ -107,29 +113,29 @@ func structuredWrite(client *logging.Client) {
 	// [END write_log_entry]
 }
 
-func deleteLog(client *logging.Client) error {
+func deleteLog(adminClient *logadmin.Client) error {
 	ctx := context.Background()
 
 	// [START delete_log]
 	const name = "log-example"
-	if err := client.DeleteLog(ctx, name); err != nil {
+	if err := adminClient.DeleteLog(ctx, name); err != nil {
 		return err
 	}
 	// [END delete_log]
 	return nil
 }
 
-func getEntries(client *logging.Client, projID string) ([]*logging.Entry, error) {
+func getEntries(adminClient *logadmin.Client, projID string) ([]*logging.Entry, error) {
 	ctx := context.Background()
 
 	// [START list_log_entries]
 	var entries []*logging.Entry
 	const name = "log-example"
-	iter := client.Entries(ctx,
+	iter := adminClient.Entries(ctx,
 		// Only get entries from the log-example log.
-		logging.Filter(fmt.Sprintf(`logName = "projects/%s/logs/%s"`, projID, name)),
+		logadmin.Filter(fmt.Sprintf(`logName = "projects/%s/logs/%s"`, projID, name)),
 		// Get most recent entries first.
-		logging.OrderBy("timestamp desc"),
+		logadmin.OrderBy("timestamp desc"),
 	)
 
 	// Fetch the most recent 20 entries.
