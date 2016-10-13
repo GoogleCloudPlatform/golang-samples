def737ae7ee975a6f96d0dfb126074e2b4a6437e
Jonathan Amsterdam <jba@users.noreply.github.com>
2016-10-13 18:33:18 -0400

    pubsub: change pubsub.Done to iterator.Done (#135)
    We'll eventually remove pubsub.Done.

--- pubsub/topics/main.go
+++ pubsub/topics/main.go
@@ -15,6 +15,7 @@ import (
 	"golang.org/x/net/context"
 
 	"cloud.google.com/go/pubsub"
+	"google.golang.org/api/iterator"
 	// [END imports]
 )
 
@@ -80,7 +81,7 @@ func list(client *pubsub.Client) ([]*pubsub.Topic, error) {
 	it := client.Topics(ctx)
 	for {
 		topic, err := it.Next()
-		if err == pubsub.Done {
+		if err == iterator.Done {
 			break
 		}
 		if err != nil {
