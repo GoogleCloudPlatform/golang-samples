410e04b5956b964dfb262009bf7e2a85938a0e81
Chris Broadfoot <cbro@golang.org>
2016-10-26 21:16:56 -0700

    Add license headers, package level comment

--- docs/appengine/firebase/tictactoe/main.go
+++ docs/appengine/firebase/tictactoe/main.go
@@ -1,3 +1,8 @@
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Sample tictactoe implements a tic-tac-toe game using Firebase as a communication mechanism.
 package tictactoe
 
 import (
