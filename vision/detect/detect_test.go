c9744007f8a8b633fb9ac763bea1fddea5e20873
Chris Broadfoot <cbro@golang.org>
2017-01-31 19:56:27 -0800

    vision: add detect program
    This has samples for all 7 vision APIs:
    * Faces
    * Labels
    * Landmarks
    * Text
    * Logos
    * Image Properties (color palette)
    * Safe Search

---
+++ vision/detect/detect_test.go
@@ -0,0 +1,27 @@
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
+)
+
+func TestLabel(t *testing.T) {
+	testutil.SystemTest(t)
+
+	var buf bytes.Buffer
+	err := findLabels(&buf, "../testdata/cat.jpg")
+	if err != nil {
+		t.Fatalf("got %v, want nil err", err)
+	}
+	got := buf.String()
+	if !strings.Contains(got, "cat") {
+		t.Errorf("got %q, want to contain cat", got)
+	}
+}
