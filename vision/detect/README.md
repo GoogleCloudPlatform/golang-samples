c6836b575df2ee95728a8ec9a5b1869e9f867c67
Chris Broadfoot <cbro@golang.org>
2017-02-02 16:20:37 -0800

    vision/detect: add README, test images, tests
    Change-Id: I19afd7df76d6ff945d76dc2413c93a61ade9c3c6

---
+++ vision/detect/README.md
@@ -0,0 +1,27 @@
+# Google Cloud Vision API Go example
+
+## Authentication
+
+* Create a project with the [Google Cloud Console][cloud-console], and enable
+  the [Vision API][vision-api].
+* From the Cloud Console, create a service account,
+  download its json credentials file, then set the 
+  `GOOGLE_APPLICATION_CREDENTIALS` environment variable:
+
+  ```bash
+  export GOOGLE_APPLICATION_CREDENTIALS=/path/to/your-project-credentials.json
+  ```
+
+[cloud-console]: https://console.cloud.google.com
+[vision-api]: https://console.cloud.google.com/apis/api/vision.googleapis.com/overview?project=_
+[adc]: https://cloud.google.com/docs/authentication#developer_workflow
+
+## Run the sample
+
+To build and run the sample:
+
+```bash
+go run detect.go main.go ../testdata/cat.jpg
+
+go run detect.go main.go gs://...
+```
--- vision/detect/detect_test.go
+++ vision/detect/detect_test.go
@@ -6,22 +6,55 @@ package main
 
 import (
 	"bytes"
+	"io"
 	"strings"
 	"testing"
 
 	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
 )
 
-func TestLabel(t *testing.T) {
+func TestDetect(t *testing.T) {
 	testutil.SystemTest(t)
 
-	var buf bytes.Buffer
-	err := detectLabels(&buf, "../testdata/cat.jpg")
-	if err != nil {
-		t.Fatalf("got %v, want nil err", err)
+	tests := []struct {
+		name        string
+		local, gcs  func(io.Writer, string) error
+		path        string
+		wantContain string
+	}{
+		{"Labels", detectLabels, nil, "cat.jpg", "cat"},
+		{"Labels2", detectLabels, detectLabelsGCS, "wakeupcat.jpg", "whiskers"},
+		{"Faces", detectFaces, detectFacesGCS, "face_no_surprise.jpg", "Anger"},
+		{"Landmarks", detectLandmarks, detectLandmarksGCS, "landmark.jpg", "Palace"},
+		{"Logos", detectLogos, detectLogosGCS, "logos.png", "Google"},
+		{"Properties", detectProperties, detectPropertiesGCS, "landmark.jpg", "%"},
+		{"SafeSearch", detectSafeSearch, detectSafeSearchGCS, "wakeupcat.jpg", "Spoofed"},
+		{"Text", detectText, detectTextGCS, "text.jpg", "Preparing to install"},
 	}
-	got := buf.String()
-	if !strings.Contains(got, "cat") {
-		t.Errorf("got %q, want to contain cat", got)
+
+	for _, tt := range tests {
+		var buf bytes.Buffer
+		err := tt.local(&buf, "../testdata/"+tt.path)
+		if err != nil {
+			t.Fatalf("got %v, want nil err", err)
+		}
+		if got := buf.String(); !strings.Contains(got, tt.wantContain) {
+			t.Errorf("Local %s(%q): got %q, want to contain %q", tt.name, tt.path, got, tt.wantContain)
+		}
+	}
+
+	for _, tt := range tests {
+		if tt.gcs == nil {
+			continue
+		}
+
+		var buf bytes.Buffer
+		err := tt.gcs(&buf, "gs://python-docs-samples-tests/vision/"+tt.path)
+		if err != nil {
+			t.Fatalf("got %v, want nil err", err)
+		}
+		if got := buf.String(); !strings.Contains(got, tt.wantContain) {
+			t.Errorf("GCS %s(%q): got %q, want to contain %q", tt.name, tt.path, got, tt.wantContain)
+		}
 	}
 }
