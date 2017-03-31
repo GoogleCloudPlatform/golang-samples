4161505c114b2e428831df112daf72b359c6d886
Chris Broadfoot <cbro@golang.org>
2017-03-31 15:22:42 -0700

    gofmt

--- docs/appengine/tools/devserver/devserver.go
+++ docs/appengine/tools/devserver/devserver.go
@@ -1,16 +1,16 @@
 package devserver
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 
-    "google.golang.org/appengine"
+	"google.golang.org/appengine"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "IsDevAppServer: %v", appengine.IsDevAppServer())
+	fmt.Fprintf(w, "IsDevAppServer: %v", appengine.IsDevAppServer())
 }
