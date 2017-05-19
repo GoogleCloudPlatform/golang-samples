fa696c657dcefa0dc70e16e6093e92d68b83c1a7
Chris Broadfoot <cbro@golang.org>
2017-05-19 11:34:13 -0700

    storage/objects: use a single bucket per project, improve tests (#230)
    Add tests for listing by prefix (to validate the behaviour present in
    the comments).
    
    Use a simplified mechanism to create and clean up test buckets.
    This change means that given a single test project, integration tests cannot
    run concurrently. Our tests are sharded across projects, so this
    simplifies setup and cleanup.

--- storage/objects/main_test.go
+++ storage/objects/main_test.go
@@ -5,10 +5,13 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"log"
+	"strings"
 	"testing"
 	"time"
+
+	"google.golang.org/api/iterator"
 
 	"cloud.google.com/go/storage"
 	"golang.org/x/net/context"
@@ -24,45 +27,75 @@ func TestObjects(t *testing.T) {
 		log.Fatal(err)
 	}
 
-	object := fmt.Sprintf("golang-example-objects-object-%d", time.Now().Unix())
+	var (
+		bucket    = tc.ProjectID + "-samples-object-bucket-1"
+		dstBucket = tc.ProjectID + "-samples-object-bucket-2"
 
-	// TODO(jbd): Clean garbage buckets that are older than 1 day.
-	bucket := fmt.Sprintf("golang-example-objects-bucket-%d", time.Now().Unix())
-	dstBucket := fmt.Sprintf("golang-example-objects-dstbucket-%d", time.Now().Unix())
+		object1 = "foo.txt"
+		object2 = "foo/a.txt"
+	)
 
-	ensureBucketExists(ctx, client, tc.ProjectID, bucket)
-	ensureBucketExists(ctx, client, tc.ProjectID, dstBucket)
+	cleanBucket(t, ctx, client, tc.ProjectID, bucket)
+	cleanBucket(t, ctx, client, tc.ProjectID, dstBucket)
 
-	if err := list(client, bucket); err != nil {
-		t.Fatalf("cannot list objects: %v", err)
+	if err := write(client, bucket, object1); err != nil {
+		t.Fatalf("write(%q): %v", object1, err)
 	}
-	if err := listByPrefix(client, bucket, "golang-example", ""); err != nil {
-		t.Fatalf("cannot list objects by prefix: %v", err)
+	if err := write(client, bucket, object2); err != nil {
+		t.Fatalf("write(%q): %v", object2, err)
 	}
-	if err := write(client, bucket, object); err != nil {
-		t.Fatalf("cannot write object: %v", err)
+
+	{
+		// Should only show "foo/a.txt", not "foo.txt"
+		var buf bytes.Buffer
+		if err := list(&buf, client, bucket); err != nil {
+			t.Fatalf("cannot list objects: %v", err)
+		}
+		if got, want := buf.String(), object1; !strings.Contains(got, want) {
+			t.Errorf("List() got %q; want to contain %q", got, want)
+		}
+		if got, want := buf.String(), object2; !strings.Contains(got, want) {
+			t.Errorf("List() got %q; want to contain %q", got, want)
+		}
 	}
-	data, err := read(client, bucket, object)
+
+	{
+		// Should only show "foo/a.txt", not "foo.txt"
+		const prefix = "foo/"
+		var buf bytes.Buffer
+		if err := listByPrefix(&buf, client, bucket, prefix, ""); err != nil {
+			t.Fatalf("cannot list objects by prefix: %v", err)
+		}
+		if got, want := buf.String(), object1; strings.Contains(got, want) {
+			t.Errorf("List(%q) got %q; want NOT to contain %q", prefix, got, want)
+		}
+		if got, want := buf.String(), object2; !strings.Contains(got, want) {
+			t.Errorf("List(%q) got %q; want to contain %q", prefix, got, want)
+		}
+	}
+
+	data, err := read(client, bucket, object1)
 	if err != nil {
 		t.Fatalf("cannot read object: %v", err)
 	}
 	if got, want := string(data), "Hello\nworld"; got != want {
 		t.Errorf("contents = %q; want %q", got, want)
 	}
-	_, err = attrs(client, bucket, object)
+	_, err = attrs(client, bucket, object1)
 	if err != nil {
 		t.Errorf("cannot get object metadata: %v", err)
 	}
-	if err := makePublic(client, bucket, object); err != nil {
+	if err := makePublic(client, bucket, object1); err != nil {
 		t.Errorf("cannot to make object public: %v", err)
 	}
-	err = move(client, bucket, object)
+	err = move(client, bucket, object1)
 	if err != nil {
 		t.Fatalf("cannot move object: %v", err)
 	}
-	object += "-rename"
-	err = copyToBucket(client, dstBucket, bucket, object)
-	if err != nil {
+	// object1's new name.
+	object1 = object1 + "-rename"
+
+	if err := copyToBucket(client, dstBucket, bucket, object1); err != nil {
 		t.Errorf("cannot copy object to bucket: %v", err)
 	}
 	if err := addBucketACL(client, bucket); err != nil {
@@ -83,36 +116,39 @@ func TestObjects(t *testing.T) {
 	if err := deleteBucketACL(client, bucket); err != nil {
 		t.Errorf("cannot delete bucket acl: %v", err)
 	}
-	if err := addObjectACL(client, bucket, object); err != nil {
+	if err := addObjectACL(client, bucket, object1); err != nil {
 		t.Errorf("cannot add object acl: %v", err)
 	}
-	if err := objectACL(client, bucket, object); err != nil {
+	if err := objectACL(client, bucket, object1); err != nil {
 		t.Errorf("cannot get object acl: %v", err)
 	}
-	if err := objectACLFiltered(client, bucket, object, storage.AllAuthenticatedUsers); err != nil {
+	if err := objectACLFiltered(client, bucket, object1, storage.AllAuthenticatedUsers); err != nil {
 		t.Errorf("cannot filter object acl: %v", err)
 	}
-	if err := deleteObjectACL(client, bucket, object); err != nil {
+	if err := deleteObjectACL(client, bucket, object1); err != nil {
 		t.Errorf("cannot delete object acl: %v", err)
 	}
 
 	key := []byte("my-secret-AES-256-encryption-key")
 	newKey := []byte("My-secret-AES-256-encryption-key")
 
-	if err := writeEncryptedObject(client, bucket, object, key); err != nil {
+	if err := writeEncryptedObject(client, bucket, object1, key); err != nil {
 		t.Errorf("cannot write an encrypted object: %v", err)
 	}
-	data, err = readEncryptedObject(client, bucket, object, key)
+	data, err = readEncryptedObject(client, bucket, object1, key)
 	if err != nil {
 		t.Errorf("cannot read the encrypted object: %v", err)
 	}
 	if got, want := string(data), "top secret"; got != want {
 		t.Errorf("object content = %q; want %q", got, want)
 	}
-	if err := rotateEncryptionKey(client, bucket, object, key, newKey); err != nil {
+	if err := rotateEncryptionKey(client, bucket, object1, key, newKey); err != nil {
 		t.Errorf("cannot encrypt the object with the new key: %v", err)
 	}
-	if err := delete(client, bucket, object); err != nil {
+	if err := delete(client, bucket, object1); err != nil {
+		t.Errorf("cannot to delete object: %v", err)
+	}
+	if err := delete(client, bucket, object2); err != nil {
 		t.Errorf("cannot to delete object: %v", err)
 	}
 
@@ -125,7 +161,7 @@ func TestObjects(t *testing.T) {
 	})
 
 	testutil.Retry(t, 10, time.Second, func(r *testutil.R) {
-		if err := delete(client, dstBucket, object+"-copy"); err != nil {
+		if err := delete(client, dstBucket, object1+"-copy"); err != nil {
 			r.Errorf("cannot to delete copy object: %v", err)
 		}
 	})
@@ -137,13 +173,29 @@ func TestObjects(t *testing.T) {
 	})
 }
 
-func ensureBucketExists(ctx context.Context, client *storage.Client, projectID, bucket string) {
+// cleanBucket ensures there's a fresh bucket with a given name, deleting the existing bucket if it already exists.
+func cleanBucket(t *testing.T, ctx context.Context, client *storage.Client, projectID, bucket string) {
 	b := client.Bucket(bucket)
 	_, err := b.Attrs(ctx)
-	if err == storage.ErrBucketNotExist {
-		err = b.Create(ctx, projectID, nil)
+	if err == nil {
+		it := b.Objects(ctx, nil)
+		for {
+			attrs, err := it.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				t.Fatalf("Bucket.Objects(%q): %v", bucket, err)
+			}
+			if err := b.Object(attrs.Name).Delete(ctx); err != nil {
+				t.Fatalf("Bucket(%q).Object(%q).Delete: %v", bucket, attrs.Name, err)
+			}
+		}
+		if err := b.Delete(ctx); err != nil {
+			t.Fatalf("Bucket.Delete(%q): %v", bucket, err)
+		}
 	}
-	if err != nil {
-		log.Fatalf("bucket ensuring failed: %v", err)
+	if err := b.Create(ctx, projectID, nil); err != nil {
+		t.Fatalf("Bucket.Create(%q): %v", bucket, err)
 	}
 }
