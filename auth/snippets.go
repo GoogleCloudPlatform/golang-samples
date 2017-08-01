b96188f77b4945bc6fe1440234b42dd538e152d3
Jaana Burcu Dogan <jbd@google.com>
2017-08-01 16:47:03 -0700

    auth: add implicit auth snippet

---
+++ auth/snippets.go
@@ -0,0 +1,42 @@
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Package snippets contain Google Cloud authentication snippets.
+package snippets
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"google.golang.org/api/iterator"
+
+	"cloud.google.com/go/storage"
+)
+
+func adc() {
+	ctx := context.Background()
+
+	// [START auth_cloud_implicit]
+	// Instantiate a client. If you don't specify credentials
+	// when constructing the client, the client library will look
+	// for credentials in the environment.
+	storageClient, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	it := storageClient.Buckets(ctx, "project-id")
+	for {
+		bucketAttrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(bucketAttrs.Name)
+	}
+	// [END auth_cloud_implicit]
+}
