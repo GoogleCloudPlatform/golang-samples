1a79f82d691c863f98d6cb5493cefbce79cfc77a
Jaana Burcu Dogan <jbd@google.com>
2017-08-01 17:05:29 -0700

    address feedback

--- auth/snippets.go
+++ auth/snippets.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by the Apache 2.0
 // license that can be found in the LICENSE file.
 
-// Package snippets contain Google Cloud authentication snippets.
-package snippets
+// Package authsnippets contains Google Cloud authentication snippets.
+package authsnippets
 
 import (
 	"fmt"
@@ -11,6 +11,8 @@ import (
 
 	"cloud.google.com/go/storage"
 	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	cloudkms "google.golang.org/api/cloudkms/v1"
 	"google.golang.org/api/iterator"
 )
 
@@ -18,9 +20,10 @@ func adc() {
 	ctx := context.Background()
 
 	// [START auth_cloud_implicit]
-	// Instantiate a client. If you don't specify credentials
-	// when constructing the client, the client library will look
-	// for credentials in the environment.
+
+	// For API packages whose import path is starting with "cloud.google.com/go",
+	// such as cloud.google.com/go/storage in this case, if there are no credentials
+	// provided, the client library will look for credentials in the environment.
 	storageClient, err := storage.NewClient(ctx)
 	if err != nil {
 		log.Fatal(err)
@@ -37,5 +40,20 @@ func adc() {
 		}
 		fmt.Println(bucketAttrs.Name)
 	}
+
+	// For packages whose import path is starting with "google.golang.org/api",
+	// such as google.golang.org/api/cloudkms/v1, use the
+	// golang.org/x/oauth2/google package as shown below.
+	oauthClient, err := google.DefaultClient(ctx, cloudkms.CloudPlatformScope)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	kmsService, err := cloudkms.New(oauthClient)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// [END auth_cloud_implicit]
+	_ = kmsService
 }
