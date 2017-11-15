6ff9ac1a4952f896c40b34e1cc2dba8c9930732e
Chris Broadfoot <cbro@golang.org>
2017-11-14 17:40:11 -0800

    add tests

---
+++ internal/testutil/testgen.go
@@ -0,0 +1,62 @@
+// Copyright 2017 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package testutil
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/broady/preprocess/lib/preprocess"
+	"golang.org/x/tools/imports"
+)
+
+type genTest struct {
+	t        *testing.T
+	template string
+	labels   []string
+}
+
+func Generated(t *testing.T, templateFile string) genTest {
+	return genTest{
+		t:        t,
+		template: templateFile,
+	}
+}
+
+func (g genTest) Labels(labels ...string) genTest {
+	g.labels = labels
+	return g
+}
+
+func (g genTest) Matches(outFile string) {
+	tmpl, err := ioutil.ReadFile(g.template)
+	if err != nil {
+		g.t.Error("ReadFile(%v): %v", g.template, err)
+		return
+	}
+
+	want, err := ioutil.ReadFile(outFile)
+	if err != nil {
+		g.t.Error("ReadFile(%v): %v", outFile, err)
+		return
+	}
+
+	got, err := preprocess.Process(bytes.NewReader(tmpl), g.labels, "//#")
+	if err != nil {
+		g.t.Error("Preprocess(%v, %v) == %q: %v", g.template, g.labels, err)
+		return
+	}
+
+	got, err = imports.Process(outFile, got, nil)
+	if err != nil {
+		g.t.Errorf("Goimports(%v): %v", outFile, err)
+		return
+	}
+
+	if !bytes.Equal(got, want) {
+		g.t.Errorf("Generated output for %s doesn't match file on disk. Did you edit the generated file instead of the template, or forget to run `go generate`?", outFile)
+	}
+}
--- videointelligence/video_analyze/gen/gen_test.go
+++ videointelligence/video_analyze/gen/gen_test.go
@@ -5,53 +5,16 @@
 package gentest
 
 import (
-	"bytes"
-	"io/ioutil"
 	"testing"
 
-	"golang.org/x/tools/imports"
-
-	"github.com/broady/preprocess/lib/preprocess"
+	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
 )
 
-func TestGen(t *testing.T) {
-	in, err := ioutil.ReadFile("template.go")
-	if err != nil {
-		t.Fatal(err)
-	}
+func TestFresh(t *testing.T) {
+	testutil.Generated(t, "template.go").
+		Matches("../video_analyze.go")
 
-	tests := []struct {
-		outFile string
-		labels  []string
-	}{
-		{
-			outFile: "../video_analyze.go",
-			labels:  []string{},
-		},
-		{
-			outFile: "../video_analyze_gcs.go",
-			labels:  []string{"gcs"},
-		},
-	}
-	for _, tc := range tests {
-		want, err := ioutil.ReadFile(tc.outFile)
-		if err != nil {
-			t.Error(err)
-			continue
-		}
-
-		got, err := preprocess.Process(bytes.NewReader(in), tc.labels, "//#")
-		if err != nil {
-			t.Errorf("%q: %v", tc.outFile, err)
-			continue
-		}
-		got, err = imports.Process(tc.outFile, got, nil)
-		if err != nil {
-			t.Errorf("gofmt %q: %v", tc.outFile, err)
-		}
-
-		if !bytes.Equal(got, want) {
-			t.Errorf("%s does not match. did you edit the generated file instead of the template, or forget to run `go generate`?", tc.outFile)
-		}
-	}
+	testutil.Generated(t, "template.go").
+		Labels("gcs").
+		Matches("../video_analyze_gcs.go")
 }
