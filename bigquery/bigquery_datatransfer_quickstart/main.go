0c388089aadf8a92262c92f7893157faaa0561eb
Tim Swast <swast@google.com>
2018-01-04 13:30:23 -0800

    BQ Data Transfer: Add quickstart sample (#362)
    * BQ Data Transfer: Add quickstart sample
    
    A sample showing how to use the BigQuery Data Transfer API to list
    possible data sources.
    
    * Use ds as name for data source iteration var.
    
    * Remove binary.

---
+++ bigquery/bigquery_datatransfer_quickstart/main.go
@@ -0,0 +1,51 @@
+// Copyright 2017 Google LLC. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// [START bigquery_datatransfer_quickstart]
+// Sample bigquery-quickstart creates a Google BigQuery dataset.
+package main
+
+import (
+	"fmt"
+	"log"
+
+	// Imports the BigQuery Data Transfer client package.
+	"cloud.google.com/go/bigquery/datatransfer/apiv1"
+	"golang.org/x/net/context"
+	"google.golang.org/api/iterator"
+	datatransferpb "google.golang.org/genproto/googleapis/cloud/bigquery/datatransfer/v1"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Sets your Google Cloud Platform project ID.
+	projectID := "YOUR_PROJECT_ID"
+
+	// Creates a client.
+	client, err := datatransfer.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	req := &datatransferpb.ListDataSourcesRequest{
+		Parent: fmt.Sprintf("projects/%s", projectID),
+	}
+	it := client.ListDataSources(ctx, req)
+	fmt.Println("Supported Data Sources:")
+	for {
+		ds, err := it.Next()
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			log.Fatalf("Failed to list sources: %v", err)
+		}
+		fmt.Println(ds.DisplayName)
+		fmt.Println("\tID: ", ds.DataSourceId)
+		fmt.Println("\tFull path: ", ds.Name)
+		fmt.Println("\tDescription: ", ds.Description)
+	}
+}
+
+// [END bigquery_datatransfer_quickstart]
