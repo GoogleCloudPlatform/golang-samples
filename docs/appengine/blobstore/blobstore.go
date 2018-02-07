ab7307976777837837b674cd82498fe77bc6041e
Angela Funk <32649053+angelafunk@users.noreply.github.com>
2018-02-06 18:17:29 -0800

    Change region tag location

--- docs/appengine/blobstore/blobstore.go
+++ docs/appengine/blobstore/blobstore.go
@@ -34,6 +34,11 @@ Upload File: <input type="file" name="file"><br>
 		return
 	}
 	// [END uploading_a_blob_1]
+	w.Header().Set("Content-Type", "text/html")
+ -	err = rootTemplate.Execute(w, uploadURL)
+ -	if err != nil {
+ -		log.Errorf(ctx, "%v", err)
+ -	}
 }
 
 func sampleHandler2(w http.ResponseWriter, r *http.Request) {
