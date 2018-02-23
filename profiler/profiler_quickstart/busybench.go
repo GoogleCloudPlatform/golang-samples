836aa572044524640aa88fc5b21213eeb8e1eb4d
Alexey Alexandrov <aalexand@users.noreply.github.com>
2018-02-23 13:23:11 -0800

    profiler: add profiler quickstart (#398)

---
+++ profiler/profiler_quickstart/busybench.go
@@ -0,0 +1,46 @@
+// Copyright 2018 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// [START profiler_quickstart]
+
+// Sample profiler_quickstart simulates a CPU-intensive workload for profiler.
+package main
+
+import (
+	"log"
+	"runtime"
+
+	"cloud.google.com/go/profiler"
+)
+
+func busyloop() {
+	for {
+		load()
+		// Make sure to yield so that the profiler thread
+		// gets some CPU time even on single core machines
+		// where GOMAXPROCS is 1. Not needed in real programs
+		// as typically the preemption happens naturally.
+		runtime.Gosched()
+	}
+}
+
+func load() {
+	for i := 0; i < (1 << 20); i++ {
+	}
+}
+
+func main() {
+	err := profiler.Start(profiler.Config{
+		Service:              "hello-profiler",
+		NoHeapProfiling:      true,
+		NoGoroutineProfiling: true,
+		DebugLogging:         true,
+	})
+	if err != nil {
+		log.Fatalf("failed to start the profiler: %v", err)
+	}
+	busyloop()
+}
+
+// [END profiler_quickstart]
