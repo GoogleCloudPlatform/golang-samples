12cc8aa377cc77681317bea56c6b733d15e04de0
Christophe Taton <christophe.taton@gmail.com>
2018-03-01 12:14:28 -0800

    endpoints: add example with JWT authentication (#361)
     - Add flag to configure the interface used by the server.
     - Add flag to specify an authentication bearer token in the client.
     - Add service configuration for authentication through Google service
       account.
     - Update client to optionally authenticate request using a JWT token
       signed by a Google service account.
     - Add documentation for service account authentication.
     - `gcloud service-management` -> `gcloud endpoints services`

--- endpoints/getting-started-grpc/client/main.go
+++ endpoints/getting-started-grpc/client/main.go
@@ -35,9 +35,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 
 	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 	"google.golang.org/grpc/metadata"
@@ -46,8 +49,11 @@ import (
 const defaultName = "world"
 
 var (
-	addr = flag.String("addr", "127.0.0.1:50051", "Address of grpc server.")
-	key  = flag.String("api-key", "", "API key.")
+	addr     = flag.String("addr", "127.0.0.1:50051", "Address of grpc server.")
+	key      = flag.String("api-key", "", "API key.")
+	token    = flag.String("token", "", "Authentication token.")
+	keyfile  = flag.String("keyfile", "", "Path to a Google service account key file.")
+	audience = flag.String("audience", "", "Audience.")
 )
 
 func main() {
@@ -61,8 +67,36 @@ func main() {
 	defer conn.Close()
 	c := pb.NewGreeterClient(conn)
 
+	if *keyfile != "" {
+		log.Printf("Authenticating using Google service account key in %s", *keyfile)
+		keyBytes, err := ioutil.ReadFile(*keyfile)
+		if err != nil {
+			log.Fatalf("Unable to read service account key file %s: %v", *keyfile, err)
+		}
+
+		tokenSource, err := google.JWTAccessTokenSourceFromJSON(keyBytes, *audience)
+		if err != nil {
+			log.Fatalf("Error building JWT access token source: %v", err)
+		}
+		jwt, err := tokenSource.Token()
+		if err != nil {
+			log.Fatalf("Unable to generate JWT token: %v", err)
+		}
+		*token = jwt.AccessToken
+		// NOTE: the generated JWT token has a 1h TTL.
+		// Make sure to refresh the token before it expires by calling TokenSource.Token() for each outgoing requests.
+		// Calls to this particular implementation of TokenSource.Token() are cheap.
+	}
+
 	ctx := context.Background()
-	ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs("x-api-key", *key))
+	if *key != "" {
+		log.Printf("Using API key: %s", *key)
+		ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs("x-api-key", *key))
+	}
+	if *token != "" {
+		log.Printf("Using authentication token: %s", *token)
+		ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs("Authorization", fmt.Sprintf("Bearer %s", *token)))
+	}
 
 	// Contact the server and print out its response.
 	name := defaultName
