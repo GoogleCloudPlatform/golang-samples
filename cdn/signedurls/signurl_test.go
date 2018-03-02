67e29bcb4699e5f0e6f3aaf188ce6539be644c7a
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2018-03-02 10:12:10 -0800

    cdn: Add URL signing example (#386)
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

---
+++ cdn/signedurls/signurl_test.go
@@ -0,0 +1,61 @@
+// Copyright 2018 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestReadKeyFile(t *testing.T) {
+	f, err := ioutil.TempFile("", "cdnkey")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(f.Name())
+	f.Close()
+
+	key := `nZtRohdNF9m3cKM24IcK4w==`
+	expected := []byte{0x9d, 0x9b, 0x51, 0xa2, 0x17, 0x4d, 0x17, 0xd9,
+		0xb7, 0x70, 0xa3, 0x36, 0xe0, 0x87, 0x0a, 0xe3}
+	if err := ioutil.WriteFile(f.Name(), []byte(key), 0600); err != nil {
+		t.Fatal(err)
+	}
+	b, err := readKeyFile(f.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(b, expected) {
+		t.Fatalf("got=%v; expected=%v", b, expected)
+	}
+}
+
+func TestSignURL(t *testing.T) {
+	key := []byte{0x9d, 0x9b, 0x51, 0xa2, 0x17, 0x4d, 0x17, 0xd9,
+		0xb7, 0x70, 0xa3, 0x36, 0xe0, 0x87, 0x0a, 0xe3} // base64url: nZtRohdNF9m3cKM24IcK4w==
+
+	// TODO(ahmetb): use subtests introduced in go1.7
+	cases := []struct {
+		url, keyName string
+		expiration   time.Time
+		out          string
+	}{
+		{"http://35.186.234.33/index.html", "my-key", time.Unix(1558131350, 0),
+			"http://35.186.234.33/index.html?Expires=1558131350&KeyName=my-key&Signature=fm6JZSmKNsB5sys8VGr-JE4LiiE="},
+		{"https://www.google.com/", "my-key", time.Unix(1549751401, 0),
+			"https://www.google.com/?Expires=1549751401&KeyName=my-key&Signature=M_QO7BGHi2sGqrJO-MDr0uhDFuc="},
+		{"https://www.example.com/some/path?some=query&another=param", "my-key", time.Unix(1549751461, 0),
+			"https://www.example.com/some/path?some=query&another=param&Expires=1549751461&KeyName=my-key&Signature=sTqqGX5hUJmlRJ84koAIhWW_c3M="},
+	}
+
+	for _, c := range cases {
+		signed := SignURL(c.url, c.keyName, key, c.expiration)
+		if signed != c.out {
+			t.Errorf("want=%v\ngot=%v", c.out, signed)
+		}
+	}
+}
