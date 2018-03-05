09df43710ea1fed6b73bd23fbcc052b3ba968390
Tyler Bui-Palsulich <tbp@google.com>
2018-03-05 14:53:10 -0500

    dlp: basic example of redacting

---
+++ dlp/main.go
@@ -0,0 +1,54 @@
+// Copyright 2018 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// dlp is an example of using the DLP API.
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	dlp "cloud.google.com/go/dlp/apiv2beta1"
+	dlppb "google.golang.org/genproto/googleapis/privacy/dlp/v2beta1"
+)
+
+func main() {
+	c, err := dlp.NewClient(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	rcr := &dlppb.RedactContentRequest{
+		InspectConfig: &dlppb.InspectConfig{
+			InfoTypes: []*dlppb.InfoType{
+				{
+					Name: "US_SOCIAL_SECURITY_NUMBER",
+				},
+			},
+			MinLikelihood: dlppb.Likelihood_LIKELIHOOD_UNSPECIFIED,
+		},
+		ReplaceConfigs: []*dlppb.RedactContentRequest_ReplaceConfig{
+			{
+				InfoType:    &dlppb.InfoType{Name: "US_SOCIAL_SECURITY_NUMBER"},
+				ReplaceWith: "[redacted]",
+			},
+		},
+		Items: []*dlppb.ContentItem{
+			{
+				Type: "text/plain",
+				DataItem: &dlppb.ContentItem_Data{
+					Data: []byte("My SSN is 500112233"),
+				},
+			},
+		},
+	}
+	r, err := c.RedactContent(context.Background(), rcr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	i := r.GetItems()[0]
+	fmt.Println(string(i.GetData()))
+}
