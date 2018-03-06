4069573be1f3c8d52715a482c9bb18cd36c70c80
Neeraj Kashyap <nkash@google.com>
2018-03-06 14:28:12 -0800

    Added sample for streaming Speech API transcription from file

---
+++ speech/livecaption_from_file/livecaption_from_file.go
@@ -0,0 +1,108 @@
+// Copyright 2018 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Command livecaption_from_file streams a local audio file to
+// Google Speech API and outputs the transcript.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	speech "cloud.google.com/go/speech/apiv1"
+	"golang.org/x/net/context"
+	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s <AUDIOFILE>\n", filepath.Base(os.Args[0]))
+		fmt.Fprintf(os.Stderr, "<AUDIOFILE> must be a path to a local audio file. Audio file must be a 16-bit signed little-endian encoded with a sample rate of 16000.\n")
+
+	}
+	flag.Parse()
+	if len(flag.Args()) != 1 {
+		log.Fatal("Please pass path to your local audio file as a command line argument")
+	}
+	audioFile := flag.Arg(0)
+
+	ctx := context.Background()
+
+	// [START speech_streaming_file_recognize]
+	client, err := speech.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	stream, err := client.StreamingRecognize(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Send the initial configuration message.
+	if err := stream.Send(&speechpb.StreamingRecognizeRequest{
+		StreamingRequest: &speechpb.StreamingRecognizeRequest_StreamingConfig{
+			StreamingConfig: &speechpb.StreamingRecognitionConfig{
+				Config: &speechpb.RecognitionConfig{
+					Encoding:        speechpb.RecognitionConfig_LINEAR16,
+					SampleRateHertz: 16000,
+					LanguageCode:    "en-US",
+				},
+			},
+		},
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := os.Open(audioFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	go func() {
+		buf := make([]byte, 1024)
+		for {
+			n, err := f.Read(buf)
+			if err == io.EOF {
+				// Nothing else to pipe, close the stream.
+				if err := stream.CloseSend(); err != nil {
+					log.Fatalf("Could not close stream: %v", err)
+				}
+				return
+			}
+			if err != nil {
+				log.Printf("Could not read from %s: %v", audioFile, err)
+				continue
+			}
+			if err = stream.Send(&speechpb.StreamingRecognizeRequest{
+				StreamingRequest: &speechpb.StreamingRecognizeRequest_AudioContent{
+					AudioContent: buf[:n],
+				},
+			}); err != nil {
+				log.Printf("Could not send audio: %v", err)
+			}
+		}
+	}()
+
+	for {
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Cannot stream results: %v", err)
+		}
+		if err := resp.Error; err != nil {
+			log.Fatalf("Could not recognize: %v", err)
+		}
+		for _, result := range resp.Results {
+			fmt.Printf("Result: %+v\n", result)
+		}
+	}
+	// [END speech_streaming_file_recognize]
+}
