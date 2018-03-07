ad0cad74f08e24cee1164f81b6cd2a7f552f0cda
Tyler Bui-Palsulich <tbp@google.com>
2018-03-07 14:36:34 -0500

    dlp: add README.md

--- dlp/main.go
+++ dlp/main.go
@@ -41,27 +41,17 @@ func main() {
 		infoTypes(os.Stdout, client, flag.Arg(1))
 	case "mask":
 		mask(os.Stdout, client, *project, flag.Arg(1))
-	case "deidfpe":
+	case "fpe":
 		deidentifyFPE(os.Stdout, client, *project, flag.Arg(1), flag.Arg(2), flag.Arg(3))
 	case "riskNumerical":
-		// For example:
-		// dlp -project my-project riskNumerical bigquery-public-data risk-topic risk-sub nhtsa_traffic_fatalities accident_2015 state_number
 		riskNumerical(os.Stdout, client, *project, flag.Arg(1), flag.Arg(2), flag.Arg(3), flag.Arg(4), flag.Arg(5), flag.Arg(6))
 	case "riskCategorical":
-		// For example:
-		// dlp -project my-project riskCategorical bigquery-public-data risk-topic risk-sub nhtsa_traffic_fatalities accident_2015 state_number
 		riskCategorical(os.Stdout, client, *project, flag.Arg(1), flag.Arg(2), flag.Arg(3), flag.Arg(4), flag.Arg(5), flag.Arg(6))
 	case "riskKAnonymity":
-		// For example:
-		// dlp -project my-project riskKAnonymity bigquery-public-data risk-topic risk-sub nhtsa_traffic_fatalities accident_2015 state_number,county
 		riskKAnonymity(os.Stdout, client, *project, flag.Arg(1), flag.Arg(2), flag.Arg(3), flag.Arg(4), flag.Arg(5), strings.Split(flag.Arg(6), ",")...)
 	case "riskLDiversity":
-		// For example:
-		// dlp -project my-project riskLDiversity bigquery-public-data risk-topic risk-sub nhtsa_traffic_fatalities accident_2015 city state_number,county
 		riskLDiversity(os.Stdout, client, *project, flag.Arg(1), flag.Arg(2), flag.Arg(3), flag.Arg(4), flag.Arg(5), flag.Arg(6), strings.Split(flag.Arg(7), ",")...)
 	case "riskKMap":
-		// For example:
-		// dlp -project my-project kMap bigquery-public-data san_francisco bikeshare_trips risk-topic risk-sub US_ZIP_5 zip_code
 		riskKMap(os.Stdout, client, *project, flag.Arg(1), flag.Arg(2), flag.Arg(3), flag.Arg(4), flag.Arg(5), flag.Arg(6), strings.Split(flag.Arg(7), ",")...)
 	default:
 		fmt.Fprintf(os.Stderr, `Usage: %s CMD "string"\n`, os.Args[0])
