4fdad658e4f70e76d4a3f359ba169cd57e716c1a
Tyler Bui-Palsulich <tbp@google.com>
2018-03-14 10:56:18 -0400

    dlp: add comments

--- dlp/deid.go
+++ dlp/deid.go
@@ -29,7 +29,10 @@ import (
 )
 
 // [START dlp_deidentify_masking]
+// mask deidentifies the input by masking all info types with maskingCharacter and
+// prints the result to w.
 func mask(w io.Writer, client *dlp.Client, project, input, maskingCharacter string, numberToMask int32) {
+	// Create a configured request.
 	req := &dlppb.DeidentifyContentRequest{
 		Parent: "projects/" + project,
 		DeidentifyConfig: &dlppb.DeidentifyConfig{
@@ -51,23 +54,29 @@ func mask(w io.Writer, client *dlp.Client, project, input, maskingCharacter stri
 				},
 			},
 		},
+		// The item to analyze.
 		Item: &dlppb.ContentItem{
 			DataItem: &dlppb.ContentItem_Value{
 				Value: input,
 			},
 		},
 	}
+	// Send the request.
 	r, err := client.DeidentifyContent(context.Background(), req)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Print the result.
 	fmt.Fprintln(w, r.GetItem().GetValue())
 }
 
 // [END dlp_deidentify_masking]
 
 // [START dlp_deidentify_date_shift]
-func deidentifyDateShift(w io.Writer, client *dlp.Client, project string, lowerBoundDays, upperBoundDays int32, s string) {
+// deidentifyDateShift shifts dates found in the input between lowerBoundDays and
+// upperBoundDays.
+func deidentifyDateShift(w io.Writer, client *dlp.Client, project string, lowerBoundDays, upperBoundDays int32, input string) {
+	// Create a configured request.
 	req := &dlppb.DeidentifyContentRequest{
 		Parent: "projects/" + project,
 		DeidentifyConfig: &dlppb.DeidentifyConfig{
@@ -89,6 +98,7 @@ func deidentifyDateShift(w io.Writer, client *dlp.Client, project string, lowerB
 				},
 			},
 		},
+		// The InspectConfig is used to identify the DATE fields.
 		InspectConfig: &dlppb.InspectConfig{
 			InfoTypes: []*dlppb.InfoType{
 				{
@@ -96,27 +106,37 @@ func deidentifyDateShift(w io.Writer, client *dlp.Client, project string, lowerB
 				},
 			},
 		},
+		// The item to analyze.
 		Item: &dlppb.ContentItem{
 			DataItem: &dlppb.ContentItem_Value{
-				Value: s,
+				Value: input,
 			},
 		},
 	}
+	// Send the request.
 	r, err := client.DeidentifyContent(context.Background(), req)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Print the result.
 	fmt.Fprintln(w, r.GetItem().GetValue())
 }
 
 // [END dlp_deidentify_date_shift]
 
 // [START dlp_deidentify_fpe]
-func deidentifyFPE(w io.Writer, client *dlp.Client, project, s, keyFileName, cryptoKeyName, surrogateInfoType string) {
+// deidentifyFPE deidentifies the input with FPE (Format Preserving Encryption).
+// keyFileName is the file name with the KMS wrapped key and cryptoKeyName is the
+// full KMS key resource name used to wrap the key. surrogateInfoType is an
+// optional identifier needed for reidentification. surrogateInfoType can be any
+// value not found in your input.
+func deidentifyFPE(w io.Writer, client *dlp.Client, project, input, keyFileName, cryptoKeyName, surrogateInfoType string) {
+	// Read the key file.
 	b, err := ioutil.ReadFile(keyFileName)
 	if err != nil {
 		log.Fatalf("error reading file: %v", err)
 	}
+	// Create a configured request.
 	req := &dlppb.DeidentifyContentRequest{
 		Parent: "projects/" + project,
 		DeidentifyConfig: &dlppb.DeidentifyConfig{
@@ -124,7 +144,7 @@ func deidentifyFPE(w io.Writer, client *dlp.Client, project, s, keyFileName, cry
 				InfoTypeTransformations: &dlppb.InfoTypeTransformations{
 					Transformations: []*dlppb.InfoTypeTransformations_InfoTypeTransformation{
 						{
-							InfoTypes: []*dlppb.InfoType{},
+							InfoTypes: []*dlppb.InfoType{}, // Match all info types.
 							PrimitiveTransformation: &dlppb.PrimitiveTransformation{
 								Transformation: &dlppb.PrimitiveTransformation_CryptoReplaceFfxFpeConfig{
 									CryptoReplaceFfxFpeConfig: &dlppb.CryptoReplaceFfxFpeConfig{
@@ -136,9 +156,11 @@ func deidentifyFPE(w io.Writer, client *dlp.Client, project, s, keyFileName, cry
 												},
 											},
 										},
+										// Set the alphabet used for the output.
 										Alphabet: &dlppb.CryptoReplaceFfxFpeConfig_CommonAlphabet{
 											CommonAlphabet: dlppb.CryptoReplaceFfxFpeConfig_ALPHA_NUMERIC,
 										},
+										// Set the surrogate info type, used for reidentification.
 										SurrogateInfoType: &dlppb.InfoType{
 											Name: surrogateInfoType,
 										},
@@ -150,27 +172,36 @@ func deidentifyFPE(w io.Writer, client *dlp.Client, project, s, keyFileName, cry
 				},
 			},
 		},
+		// The item to analyze.
 		Item: &dlppb.ContentItem{
 			DataItem: &dlppb.ContentItem_Value{
-				Value: s,
+				Value: input,
 			},
 		},
 	}
+	// Send the request.
 	r, err := client.DeidentifyContent(context.Background(), req)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Print the result.
 	fmt.Fprintln(w, r.GetItem().GetValue())
 }
 
 // [END dlp_deidentify_fpe]
 
 // [START reidentify_fpe]
+// reidentify_fpe reidentifies the input with FPE (Format Preserving Encryption).
+// keyFileName is the file name with the KMS wrapped key and cryptoKeyName is the
+// full KMS key resource name used to wrap the key. surrogateInfoType is an
+// the identifier used during deidentification.
 func reidentifyFPE(w io.Writer, client *dlp.Client, project, s, keyFileName, cryptoKeyName, surrogateInfoType string) {
+	// Read the key file.
 	b, err := ioutil.ReadFile(keyFileName)
 	if err != nil {
 		log.Fatalf("error reading file: %v", err)
 	}
+	// Create a configured request.
 	req := &dlppb.ReidentifyContentRequest{
 		Parent: "projects/" + project,
 		ReidentifyConfig: &dlppb.DeidentifyConfig{
@@ -190,9 +221,11 @@ func reidentifyFPE(w io.Writer, client *dlp.Client, project, s, keyFileName, cry
 												},
 											},
 										},
+										// Set the alphabet used for the encrypted fields.
 										Alphabet: &dlppb.CryptoReplaceFfxFpeConfig_CommonAlphabet{
 											CommonAlphabet: dlppb.CryptoReplaceFfxFpeConfig_ALPHA_NUMERIC,
 										},
+										// Set the surrogate info type used during deidentification.
 										SurrogateInfoType: &dlppb.InfoType{
 											Name: surrogateInfoType,
 										},
@@ -204,6 +237,7 @@ func reidentifyFPE(w io.Writer, client *dlp.Client, project, s, keyFileName, cry
 				},
 			},
 		},
+		// The InspectConfig must identify the surrogate info type to reidentify.
 		InspectConfig: &dlppb.InspectConfig{
 			CustomInfoTypes: []*dlppb.CustomInfoType{
 				{
@@ -216,16 +250,19 @@ func reidentifyFPE(w io.Writer, client *dlp.Client, project, s, keyFileName, cry
 				},
 			},
 		},
+		// The item to analyze.
 		Item: &dlppb.ContentItem{
 			DataItem: &dlppb.ContentItem_Value{
 				Value: s,
 			},
 		},
 	}
+	// Send the request.
 	r, err := client.ReidentifyContent(context.Background(), req)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Print the result.
 	fmt.Fprintln(w, r.GetItem().GetValue())
 }
 
