e474f8c0ba9591c680cf6364d7b766b472883b2b
Tyler Bui-Palsulich <26876514+tbpg@users.noreply.github.com>
2018-04-17 15:35:34 -0400

    dialogflow: skip flaky tests (#462)
    See #410.

--- dialogflow/entity_type_management/entity_type_management_test.go
+++ dialogflow/entity_type_management/entity_type_management_test.go
@@ -14,6 +14,7 @@ import (
 )
 
 func TestEntityTypeManagement(t *testing.T) {
+	t.Skip("flaky")
 	tc := testutil.SystemTest(t)
 
 	projectID := tc.ProjectID
--- dialogflow/intent_management/intent_management_test.go
+++ dialogflow/intent_management/intent_management_test.go
@@ -14,6 +14,7 @@ import (
 )
 
 func TestIntentManagement(t *testing.T) {
+	t.Skip("flaky")
 	tc := testutil.SystemTest(t)
 
 	projectID := tc.ProjectID
