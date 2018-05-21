8564bf9ffad2bb70700781673eee935612fdfcf9
Tyler Bui-Palsulich <tbp@google.com>
2018-05-21 10:39:26 -0400

    dlp: skip flaky tests
    The jobs do not finish within 25 minutes.

--- dlp/dlp_snippets/inspect_test.go
+++ dlp/dlp_snippets/inspect_test.go
@@ -122,6 +122,7 @@ func writeObject(ctx context.Context, bucket *storage.BucketHandle, fileName, co
 }
 
 func TestInspectGCS(t *testing.T) {
+	t.Skip("flaky due to timeout")
 	testutil.SystemTest(t)
 	writeTestGCSFiles(t, projectID)
 	tests := []struct {
@@ -182,6 +183,7 @@ func writeTestDatastoreFiles(t *testing.T, projectID string) {
 }
 
 func TestInspectDatastore(t *testing.T) {
+	t.Skip("flaky due to timeout")
 	testutil.SystemTest(t)
 	writeTestDatastoreFiles(t, projectID)
 	tests := []struct {
@@ -261,6 +263,7 @@ func uploadBigQuery(ctx context.Context, d *bigquery.Dataset, schema bigquery.Sc
 }
 
 func TestInspectBigquery(t *testing.T) {
+	t.Skip("flaky due to timeout")
 	testutil.SystemTest(t)
 	if err := createBigqueryTestFiles(projectID, bqDatasetID); err != nil {
 		t.Fatalf("error creating test BigQuery files: %v", err)
