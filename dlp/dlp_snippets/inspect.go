0539f1c3978c27d161a3d6cc40fd37e00dcb384e
mwdaub <mwdaub@gmail.com>
2018-06-13 13:05:46 -0700

    Fix oneof error, take 2

--- dlp/dlp_snippets/inspect.go
+++ dlp/dlp_snippets/inspect.go
@@ -442,7 +442,7 @@ func buildCustomInfoTypes(customDictionaries []string, customRegexes []string) [
 			InfoType: &dlppb.InfoType{
 				Name: fmt.Sprintf("CUSTOM_DICTIONARY_%s", idx),
 			},
-			Type: &dlppb.CustomInfoType_Dictionary{
+			isDictionary_Type: &dlppb.CustomInfoType_Dictionary{
 				WordList: &dlppb.CustomInfoType_Dictionary_WordList{
 					Words: it.Split(","),
 				},
@@ -454,7 +454,7 @@ func buildCustomInfoTypes(customDictionaries []string, customRegexes []string) [
 			InfoType: &dlppb.InfoType{
 				Name: fmt.Sprintf("CUSTOM_REGEX_%s", idx),
 			},
-			Type: &dlppb.CustomInfoType_Regex{
+			isDictionary_Type: &dlppb.CustomInfoType_Regex{
 				Pattern: it,
 			},
 		})
