7f99dfce5f00c375f22c7729929dfa4a55500199
Easwar Swaminathan <easwars@google.com>
2018-07-10 14:28:43 -0700

    Add comments.

--- jobs/cjd-sample/basic-company/main.go
+++ jobs/cjd-sample/basic-company/main.go
@@ -1,3 +1,8 @@
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// [START basic-company]
 package main
 
 import (
@@ -122,3 +127,5 @@ func main() {
 		return
 	}
 }
+
+// [END basic-company]
