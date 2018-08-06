16088c37476b282607d41585828c7518962da1f5
Adam Ross <adamross@google.com>
2018-08-06 08:44:47 -0700

    cloudtasks: Add package comment to pull_queue

--- cloudtasks/pull_queue/main.go
+++ cloudtasks/pull_queue/main.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by the Apache 2.0
 // license that can be found in the LICENSE file.
 
+// Program pull_queue provides a basic CLI for basic Cloud Tasks API interaction.
 package main
 
 import (
