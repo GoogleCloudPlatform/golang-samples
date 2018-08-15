5230ac540a103dc99eb95d98847a35fbbdf7d16e
Dan Sanche <sanche@google.com>
2018-08-15 12:27:20 -0700

    addressed PR comments

--- kms/asymmetric/samples_test.go
+++ kms/asymmetric/samples_test.go
@@ -1,13 +1,21 @@
+// Copyright 2018 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Tests for asymmetric keys feature of Cloud Key Management Service: https://cloud.google.com/kms/
 package main
 
 import (
 	"crypto/ecdsa"
 	"crypto/rsa"
+	"os"
+	"testing"
+	"time"
+
 	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/cloudkms/v1"
-	"testing"
 )
 
 type TestVariables struct {
@@ -17,144 +25,180 @@ type TestVariables struct {
 	rsaDecryptPath string
 	rsaSignPath    string
 	ecSignPath     string
+	rsaDecryptId   string
+	rsaSignId      string
+	ecSignId       string
+	keyRing        string
 }
 
-func setup(t *testing.T) TestVariables {
-	tc := testutil.SystemTest(t)
-	projectId := tc.ProjectID
-	parent := "projects/" + projectId + "/locations/global"
-	keyRing := "kms-asymmetric-samples"
+func getTestVariables(projectID string) (TestVariables, error) {
+	var v TestVariables
+	parent := "projects/" + projectID + "/locations/global"
+	keyRing := "kms-asymmetric-sample"
 
-	rsaDecrypt := parent + "/keyRings/" + keyRing + "/cryptoKeys/rsa-decrypt/cryptoKeyVersions/1"
-	rsaSign := parent + "/keyRings/" + keyRing + "/cryptoKeys/rsa-sign/cryptoKeyVersions/1"
-	ecSign := parent + "/keyRings/" + keyRing + "/cryptoKeys/ec-sign/cryptoKeyVersions/1"
+	rsaDecryptId := "rsa-decrypt"
+	rsaSignId := "rsa-sign"
+	ecSignId := "ec-sign"
+
+	rsaDecrypt := parent + "/keyRings/" + keyRing + "/cryptoKeys/" + rsaDecryptId + "/cryptoKeyVersions/1"
+	rsaSign := parent + "/keyRings/" + keyRing + "/cryptoKeys/" + rsaSignId + "/cryptoKeyVersions/1"
+	ecSign := parent + "/keyRings/" + keyRing + "/cryptoKeys/" + ecSignId + "/cryptoKeyVersions/1"
 
 	message := "test message 123"
 
 	ctx := context.Background()
 	client, err := google.DefaultClient(ctx, cloudkms.CloudPlatformScope)
 	if err != nil {
-		t.Fatal(err)
+		return v, err
 	}
 	kmsClient, err := cloudkms.New(client)
 	if err != nil {
-		t.Fatal(err)
+		return v, err
 	}
-	//create cryptokeys
-	kmsClient.Projects.Locations.KeyRings.Create(parent, &cloudkms.KeyRing{}).KeyRingId(keyRing).Do()
-	kmsClient.Projects.Locations.KeyRings.CryptoKeys.Create(
-		parent+"/keyRings/"+keyRing, &cloudkms.CryptoKey{
-			Purpose: "ASYMMETRIC_DECRYPT",
-			VersionTemplate: &cloudkms.CryptoKeyVersionTemplate{
-				Algorithm: "RSA_DECRYPT_OAEP_2048_SHA256",
-			},
-		}).CryptoKeyId("rsa-decrypt").Do()
-	kmsClient.Projects.Locations.KeyRings.CryptoKeys.Create(
-		parent+"/keyRings/"+keyRing, &cloudkms.CryptoKey{
-			Purpose: "ASYMMETRIC_SIGN",
-			VersionTemplate: &cloudkms.CryptoKeyVersionTemplate{
-				Algorithm: "RSA_SIGN_PSS_2048_SHA256",
-			},
-		}).CryptoKeyId("rsa-sign").Do()
-	kmsClient.Projects.Locations.KeyRings.CryptoKeys.Create(
-		parent+"/keyRings/"+keyRing, &cloudkms.CryptoKey{
-			Purpose: "ASYMMETRIC_SIGN",
-			VersionTemplate: &cloudkms.CryptoKeyVersionTemplate{
-				Algorithm: "EC_SIGN_P224_SHA256",
-			},
-		}).CryptoKeyId("ec-sign").Do()
 
-	v := TestVariables{kmsClient, ctx, message, rsaDecrypt, rsaSign, ecSign}
-	return v
+	v = TestVariables{kmsClient, ctx, message, rsaDecrypt, rsaSign, ecSign, rsaDecryptId, rsaSignId, ecSignId, keyRing}
+	return v, nil
 }
 
-//test equality between two values
-func assertEqual(t *testing.T, a interface{}, b interface{}) {
-	if a != b {
-		t.Errorf("%s != %s", a, b)
+func createKeyHelper(v TestVariables, keyId, keyPath, purpose, algorithm, parent string) bool {
+	if _, err := getAsymmetricPublicKey(v.ctx, v.client, keyPath); err != nil {
+		v.client.Projects.Locations.KeyRings.Create(parent, &cloudkms.KeyRing{}).KeyRingId(v.keyRing).Do()
+		v.client.Projects.Locations.KeyRings.CryptoKeys.Create(
+			parent+"/keyRings/"+v.keyRing, &cloudkms.CryptoKey{
+				Purpose: purpose,
+				VersionTemplate: &cloudkms.CryptoKeyVersionTemplate{
+					Algorithm: algorithm,
+				},
+			},
+		).CryptoKeyId(keyId).Do()
+		return true
 	}
+	return false
+}
+
+func TestMain(m *testing.M) {
+	tc, ok := testutil.ContextMain(m)
+	v, err := getTestVariables(tc.ProjectID)
+	parent := "projects/" + tc.ProjectID + "/locations/global"
+	if ok && err == nil {
+		//Create cryptokeys in the test project if needed.
+		s1 := createKeyHelper(v, v.rsaDecryptId, v.rsaDecryptPath, "ASYMMETRIC_DECRYPT", "RSA_DECRYPT_OAEP_2048_SHA256", parent)
+		s2 := createKeyHelper(v, v.rsaSignId, v.rsaSignPath, "ASYMMETRIC_SIGN", "RSA_SIGN_PSS_2048_SHA256", parent)
+		s3 := createKeyHelper(v, v.ecSignId, v.ecSignPath, "ASYMMETRIC_SIGN", "EC_SIGN_P224_SHA256", parent)
+		if s1 || s2 || s3 {
+			//Leave time for keys to initialize.
+			time.Sleep(20 * time.Second)
+		}
+	}
+	//Run tests.
+	exitCode := m.Run()
+	os.Exit(exitCode)
 }
 
 func TestGetPublicKey(t *testing.T) {
-	v := setup(t)
-
-	rsaDecryptPub, err := getAsymmetricPublicKey(v.client, v.ctx, v.rsaDecryptPath)
+	tc := testutil.SystemTest(t)
+	v, err := getTestVariables(tc.ProjectID)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("intial variable setup failed: %v", err)
+	}
+
+	rsaDecryptPub, err := getAsymmetricPublicKey(v.ctx, v.client, v.rsaDecryptPath)
+	if err != nil {
+		t.Fatalf("getAsymmetricPiblicKey(%s): %v", v.rsaDecryptPath, err)
 	}
 	_, ok := rsaDecryptPub.(*rsa.PublicKey)
-	assertEqual(t, ok, true)
+	if ok != true {
+		t.Errorf("expected *rsa.PublicKey type")
+	}
 
-	rsaSignPub, err := getAsymmetricPublicKey(v.client, v.ctx, v.rsaSignPath)
+	rsaSignPub, err := getAsymmetricPublicKey(v.ctx, v.client, v.rsaSignPath)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("getAsymmetricPiblicKey(%s): %v", v.rsaSignPath, err)
 	}
 	_, ok = rsaSignPub.(*rsa.PublicKey)
-	assertEqual(t, ok, true)
-
-	ecPub, err := getAsymmetricPublicKey(v.client, v.ctx, v.ecSignPath)
+	if ok != true {
+		t.Errorf("expected *rsa.PublicKey type")
+	}
+	ecPub, err := getAsymmetricPublicKey(v.ctx, v.client, v.ecSignPath)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("getAsymmetricPiblicKey(%s): %v", v.ecSignPath, err)
 	}
 	_, ok = ecPub.(*ecdsa.PublicKey)
-	assertEqual(t, ok, true)
+	if ok != true {
+		t.Errorf("expected *ecdsa.PublicKey type")
+	}
 }
 
 func TestRSAEncryptDecrypt(t *testing.T) {
-	v := setup(t)
+	tc := testutil.SystemTest(t)
+	v, err := getTestVariables(tc.ProjectID)
+	if err != nil {
+		t.Fatalf("intial variable setup failed: %v", err)
+	}
 
-	cipherText, err := encryptRSA(v.client, v.ctx, v.message, v.rsaDecryptPath)
+	ciphertext, err := encryptRSA(v.ctx, v.client, v.message, v.rsaDecryptPath)
 	if err != nil {
 		t.Fatal(err)
 	}
-	//cipher text should be 344 characters with base64 and RSA 2048
-	assertEqual(t, len(cipherText), 344)
-	assertEqual(t, cipherText[len(cipherText)-2:], "==")
-	plainText, err := decryptRSA(v.client, v.ctx, cipherText, v.rsaDecryptPath)
-	if err != nil {
-		t.Fatal(err)
+	if len(ciphertext) != 344 {
+		t.Errorf("ciphertext length = %d; want: %d", len(ciphertext), 344)
 	}
-	assertEqual(t, plainText, v.message)
+	if ciphertext[len(ciphertext)-2:] != "==" {
+		t.Errorf("ciphertet ending: %s; want: %s", ciphertext[len(ciphertext)-2:], "==")
+	}
+	plaintext, err := decryptRSA(v.ctx, v.client, ciphertext, v.rsaDecryptPath)
+	if err != nil {
+		t.Fatalf("decryptRSA(%s, %s): %v", ciphertext, v.rsaDecryptPath, err)
+	}
+	if v.message != plaintext {
+		t.Errorf("failed to decypt expected plaintext: want %s, got %s", plaintext, v.message)
+	}
 }
 
 func TestRSASignVerify(t *testing.T) {
-	v := setup(t)
-
-	sig, err := signAsymmetric(v.client, v.ctx, v.message, v.rsaSignPath)
+	tc := testutil.SystemTest(t)
+	v, err := getTestVariables(tc.ProjectID)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("intial variable setup failed: %v", err)
 	}
-	//cipher text should be 344 characters with base64 and RSA 2048
-	assertEqual(t, len(sig), 344)
-	assertEqual(t, sig[len(sig)-2:], "==")
 
-	err = verifySignatureRSA(v.client, v.ctx, sig, v.message, v.rsaSignPath)
+	sig, err := signAsymmetric(v.ctx, v.client, v.message, v.rsaSignPath)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("signAsymmetric(%s, %s): %v", v.message, v.rsaSignPath, err)
 	}
-	err = verifySignatureRSA(v.client, v.ctx, sig, v.message+".", v.rsaSignPath)
-	if err == nil {
+	if len(sig) != 344 {
+		t.Errorf("sig length = %d; want: %d", len(sig), 344)
+	}
+	if sig[len(sig)-2:] != "==" {
+		t.Errorf("sig ending: %s; want: %s", sig[len(sig)-2:], "==")
+	}
+	if err = verifySignatureRSA(v.ctx, v.client, sig, v.message, v.rsaSignPath); err != nil {
+		t.Fatalf("verifySignatureRSA(%s, %s, %s): %v", sig, v.message, v.rsaSignPath, err)
+	}
+	if err = verifySignatureRSA(v.ctx, v.client, sig, v.message+".", v.rsaSignPath); err == nil {
 		t.Errorf("verification for modified message should fail")
 	}
 }
 
 func TestECSignVerify(t *testing.T) {
-	v := setup(t)
-
-	sig, err := signAsymmetric(v.client, v.ctx, v.message, v.ecSignPath)
+	tc := testutil.SystemTest(t)
+	v, err := getTestVariables(tc.ProjectID)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("intial variable setup failed: %v", err)
+	}
+
+	sig, err := signAsymmetric(v.ctx, v.client, v.message, v.ecSignPath)
+	if err != nil {
+		t.Fatalf("signAsymmetric(%s, %s): %v", v.message, v.ecSignPath, err)
 	}
 	if len(sig) < 50 || len(sig) > 300 {
-		t.Errorf("signature length outside expected range. Length: %d", len(sig))
+		t.Errorf("Length = %d; want between 50-300", len(sig))
 	}
 
-	err = verifySignatureEC(v.client, v.ctx, sig, v.message, v.ecSignPath)
-	if err != nil {
-		t.Fatal(err)
+	if err = verifySignatureEC(v.ctx, v.client, sig, v.message, v.ecSignPath); err != nil {
+		t.Fatalf("verifySignatureEC(%s, %s, %s): %v", sig, v.message, v.ecSignPath, err)
 	}
-	err = verifySignatureEC(v.client, v.ctx, sig, v.message+".", v.ecSignPath)
-	if err == nil {
+	if err = verifySignatureEC(v.ctx, v.client, sig, v.message+".", v.ecSignPath); err == nil {
 		t.Errorf("verification for modified message should fail")
 	}
 }
