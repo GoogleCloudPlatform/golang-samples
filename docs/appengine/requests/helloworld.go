6c8e8d4eb53872725fbbc9c6dbe1b338f4065308
Averi Kitsch <akitsch@google.com>
2018-08-20 13:15:30 -0700

    appengine: update region tags (#574)

--- docs/appengine/requests/helloworld.go
+++ docs/appengine/requests/helloworld.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Apache 2.0
 // license that can be found in the LICENSE file.
 
-// [START requests_and_HTTP]
+// [START gae_golang_request_example]
 
 package hello
 
@@ -19,4 +19,4 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello, world</h1>")
 }
 
-// [END requests_and_HTTP]
+// [END gae_golang_request_example]
