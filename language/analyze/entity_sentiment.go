9c29dff704f99e5bfc270a547bfd882905972d23
Alix Hamilton <ajhamilton@google.com>
2018-08-20 13:59:41 -0400

    add entity sentiment tags

--- language/analyze/entity_sentiment.go
+++ language/analyze/entity_sentiment.go
@@ -22,6 +22,7 @@ func betaClient() *language.Client {
 	return client
 }
 
+// [START language_entity_sentiment_text]
 func analyzeEntitySentiment(ctx context.Context, client *language.Client, text string) (*languagepb.AnalyzeEntitySentimentResponse, error) {
 	return client.AnalyzeEntitySentiment(ctx, &languagepb.AnalyzeEntitySentimentRequest{
 		Document: &languagepb.Document{
@@ -32,3 +33,4 @@ func analyzeEntitySentiment(ctx context.Context, client *language.Client, text s
 		},
 	})
 }
+// [END language_entity_sentiment_text]
