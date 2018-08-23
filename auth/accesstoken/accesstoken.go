5dadcd144b3dd3f20042a39bd955643ab256c5b1
Salmaan Rashid <salrashid123@gmail.com>
2018-08-22 20:45:59 -0700

    pubsub->firestore

--- auth/accesstoken/accesstoken.go
+++ auth/accesstoken/accesstoken.go
@@ -4,9 +4,10 @@
 
 // This sample demonstrates AccessTokenCredentials:
 // https://godoc.org/golang.org/x/oauth2/google#JWTAccessTokenSourceFromJSON
+// https://developers.google.com/identity/protocols/OAuth2ServiceAccount#jwt-auth
 
-// To use, create a service accountJSON file and allow it atleast Pub/Sub Viewer IAM
-// permissions to list Topics on a project.
+// To use, create a service accountJSON file and allow it atleast
+// Cloud Datastore Viewer IAM on the project.
 package main
 
 import (
@@ -19,8 +20,7 @@ import (
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 
-	"cloud.google.com/go/pubsub"
-	"google.golang.org/api/iterator"
+	"cloud.google.com/go/firestore"
 	"google.golang.org/api/option"
 )
 
@@ -45,9 +45,9 @@ func main() {
 
 	// [START jwtaccesstoken_sample]
 	// audience values for other services can be found in the repo here similar to
-	// PubSub
-	// https://github.com/googleapis/googleapis/blob/master/google/pubsub/pubsub.yaml
-	var aud string = "https://pubsub.googleapis.com/google.pubsub.v1.Publisher"
+	// FireStore
+	// https://github.com/googleapis/googleapis/blob/master/google/firestore/firestore.yaml
+	var aud string = "https://firestore.googleapis.com/google.firestore.v1beta1.Firestore"
 
 	ctx := context.Background()
 	keyBytes, err := ioutil.ReadFile(*keyfile)
@@ -64,20 +64,17 @@ func main() {
 	}
 	log.Println(jwt.AccessToken)
 
-	client, err := pubsub.NewClient(ctx, *projectID, option.WithTokenSource(tokenSource))
+	client, err := firestore.NewClient(ctx, *projectID, option.WithTokenSource(tokenSource))
 	if err != nil {
-		log.Fatalf("Could not create pubsub Client: %v", err)
+		log.Fatalf("Could not create FireStore Client: %v", err)
 	}
-	topics := client.Topics(ctx)
-	for {
-		topic, err := topics.Next()
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			log.Fatalf("Error listing topics %v", err)
-		}
-		log.Println(topic)
+
+	collections, err := client.Collections(ctx).GetAll()
+	if err != nil {
+		log.Fatalf("Unable to get Collections %v", err)
+	}
+	for _, col := range collections {
+		log.Printf("Collection ID:  %v", col.ID)
 	}
 	// [END jwtaccesstoken_sample]
 }
