d8f49d6ccccd10e8c4513804152b6aee7aadcf6f
Peter Zheng <peterzh@google.com>
2018-10-09 10:12:45 -0700

    Address comments for export-assets quickstart code.

--- asset/quickstart/export-assets/main.go
+++ asset/quickstart/export-assets/main.go
@@ -12,50 +12,50 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-[START asset-quickstart_export_asset]
+// [START asset_quickstart_export_assets]
 
+// Sample asset-quickstart exports assets to given path.
 package main
 
 import (
-	"cloud.google.com/go/asset/v1beta1"
-	"fmt"
-	"golang.org/x/net/context"
-	assetpb "google.golang.org/genproto/googleapis/cloud/asset/v1beta1"
+        "fmt"
+        "log"
+        "os"
+
+        "cloud.google.com/go/asset/v1beta1"
+        "golang.org/x/net/context"
+        assetpb "google.golang.org/genproto/googleapis/cloud/asset/v1beta1"
 )
 
 func main() {
-	context := context.Background()
-	projectId := "[PROJECT]"
-	client, error := asset.NewClient(context)
-	if error != nil {
-		// TODO: Handle error.
-		fmt.Print(error)
-		return
-	}
-	request := &assetpb.ExportAssetsRequest{
-		Parent: fmt.Sprintf("projects/%s", projectId),
-		OutputConfig: &assetpb.OutputConfig{
-			Destination: &assetpb.OutputConfig_GcsDestination{
-				GcsDestination: &assetpb.GcsDestination{
-					Uri: string("gs://[my_gcs_bucket]/[my_asset_dump_file]"),
-				},
-			},
-		},
-	}
-	operation, error := client.ExportAssets(context, request)
-	if error != nil {
-		// TODO: Handle error.
-		fmt.Print(error)
-		return
-	}
-	response, error := operation.Wait(context)
-	if error != nil {
-		// TODO: Handle error.
-		fmt.Print(error)
-		return
-	}
-	// Do things with response.
-	fmt.Print(response)
+        ctx := context.Background()
+        projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+        client, err := asset.NewClient(ctx)
+        if err != nil {
+                log.Fatal(err)
+        }
+        gcsDestination := &assetpb.GcsDestination{
+                Uri: string("gs://[my_gcs_bucket]/[my_asset_dump_file]"),
+        }
+        destination := &assetpb.OutputConfig_GcsDestination{
+                GcsDestination: gcsDestination,
+        }
+        outputConfig := &assetpb.OutputConfig{
+                Destination: destination,
+        }
+        req := &assetpb.ExportAssetsRequest{
+                Parent:       fmt.Sprintf("projects/%s", projectID),
+                OutputConfig: outputConfig,
+        }
+        operation, err := client.ExportAssets(ctx, req)
+        if err != nil {
+                log.Fatal(err)
+        }
+        response, err := operation.Wait(ctx)
+        if err != nil {
+                log.Fatal(err)
+        }
+        fmt.Print(response)
 }
 
-[END asset-quickstart_export_asset]
+// [END asset_quickstart_export_assets]
