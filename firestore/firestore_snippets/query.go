5a1da0b2b463ae34c56cc28d37c446d609935af0
Jonathan Simon <jsimonweb@gmail.com>
2018-10-22 14:04:41 -0700

    Format to address comments.

--- firestore/firestore_snippets/query.go
+++ firestore/firestore_snippets/query.go
@@ -20,26 +20,36 @@ func prepareQuery(ctx context.Context, client *firestore.Client) error {
 		id string
 		c  City
 	}{
-		{id: "SF",
+		{
+			id: "SF",
 			c: City{Name: "San Francisco", State: "CA", Country: "USA",
 				Capital: false, Population: 860000,
-				Regions: []string{"west_coast", "norcal"}}},
-		{id: "LA",
+				Regions: []string{"west_coast", "norcal"}},
+		},
+		{
+			id: "LA",
 			c: City{Name: "Los Angeles", State: "CA", Country: "USA",
 				Capital: false, Population: 3900000,
-				Regions: []string{"west_coast", "socal"}}},
-		{id: "DC",
+				Regions: []string{"west_coast", "socal"}},
+		},
+		{
+			id: "DC",
 			c: City{Name: "Washington D.C.", Country: "USA",
 				Capital: false, Population: 680000,
-				Regions: []string{"east_coast"}}},
-		{id: "TOK",
+				Regions: []string{"east_coast"}},
+		},
+		{
+			id: "TOK",
 			c: City{Name: "Tokyo", Country: "Japan",
 				Capital: true, Population: 9000000,
-				Regions: []string{"kanto", "honshu"}}},
-		{id: "BJ",
+				Regions: []string{"kanto", "honshu"}},
+		},
+		{
+			id: "BJ",
 			c: City{Name: "Beijing", Country: "China",
 				Capital: true, Population: 21500000,
-				Regions: []string{"jingjinji", "hebei"}}},
+				Regions: []string{"jingjinji", "hebei"}},
+		},
 	}
 	for _, c := range cities {
 		if _, err := client.Collection("cities").Doc(c.id).Set(ctx, c.c); err != nil {
