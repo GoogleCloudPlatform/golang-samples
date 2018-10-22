b29e905202905854adcec1b0305d7a21d120dc74
Jonathan Simon <jsimonweb@gmail.com>
2018-10-22 11:05:37 -0700

    Add Firestore Regions sample data and two queries.

--- firestore/firestore_snippets/main.go
+++ firestore/firestore_snippets/main.go
@@ -17,11 +17,12 @@ import (
 // [START fs_class_definition]
 // City represents a city.
 type City struct {
-	Name       string `firestore:"name,omitempty"`
-	State      string `firestore:"state,omitempty"`
-	Country    string `firestore:"country,omitempty"`
-	Capital    bool   `firestore:"capital,omitempty"`
-	Population int64  `firestore:"population,omitempty"`
+	Name       string   `firestore:"name,omitempty"`
+	State      string   `firestore:"state,omitempty"`
+	Country    string   `firestore:"country,omitempty"`
+	Capital    bool     `firestore:"capital,omitempty"`
+	Population int64    `firestore:"population,omitempty"`
+	Regions    []string `firestore:"regions,omitempty"`
 }
 
 // [END fs_class_definition]
