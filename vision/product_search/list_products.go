9098a2b54f8e621e70ea4ef49b3730f6b981d68f
Yu-Han Liu <yuhanliu@google.com>
2018-11-22 22:51:22 -0800

    add list_products

---
+++ vision/product_search/list_products.go
@@ -0,0 +1,70 @@
+// Copyright 2018 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+// Command label uses the Vision API's label detection capabilities to find a label based on an image's content.
+package main
+
+// [START imports]
+import (
+	"flag"
+	"fmt"
+	"os"
+	
+	"context"
+
+	"google.golang.org/api/iterator"
+	vision "cloud.google.com/go/vision/apiv1"
+	visionpb "google.golang.org/genproto/googleapis/cloud/vision/v1"
+)
+// [END imports]
+
+// [START vision_product_search_list_products]
+func listProducts(project string, location string) error {
+	ctx := context.Background()
+	c, err := vision.NewProductSearchClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	req := &visionpb.ListProductsRequest{}
+	req.Parent = fmt.Sprintf("projects/%s/locations/%s", project, location)
+
+	it := c.ListProducts(ctx, req)
+	for {
+		resp, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Product name:", resp.Name)
+		fmt.Println("Product display name:", resp.DisplayName)
+		fmt.Println("Product category:", resp.ProductCategory)
+		fmt.Println("Product labels:", resp.ProductLabels, "\n")
+	}
+
+	return nil
+}
+// [END vision_product_search_list_products]
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: <project-id> <location>\n")
+	}
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	err := listProducts(args[0], args[1])
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
