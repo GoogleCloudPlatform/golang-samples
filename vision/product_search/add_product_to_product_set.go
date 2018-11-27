e6528e98e4778a832905e86d342282dc233cb141
Yu-Han Liu <yuhanliu@google.com>
2018-11-27 13:14:37 -0800

    add product_in_product_set management samples and tests

---
+++ vision/product_search/add_product_to_product_set.go
@@ -0,0 +1,40 @@
+// Copyright 2018 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package productsearch
+
+// [START vision_product_search_add_product_to_product_set]
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	vision "cloud.google.com/go/vision/apiv1"
+	visionpb "google.golang.org/genproto/googleapis/cloud/vision/v1"
+)
+
+func addProductToProductSet(w io.Writer, projectID string, location string, productID string, productSetID string) error {
+	ctx := context.Background()
+	c, err := vision.NewProductSearchClient(ctx)
+	if err != nil {
+		return err
+	}
+
+	req := &visionpb.AddProductToProductSetRequest{
+		Name: fmt.Sprintf("projects/%s/locations/%s/productSets/%s", projectID, location, productSetID),
+		Product: fmt.Sprintf("projects/%s/locations/%s/products/%s", projectID, location, productID),
+	}
+
+	err = c.AddProductToProductSet(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintln(w, "Product added to product set.")
+
+	return nil
+}
+
+// [END vision_product_search_add_product_to_product_set]
