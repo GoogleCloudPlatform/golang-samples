43fbd1add53177f42488ed45330af606ab675204
Dan Sanche <sanche@google.com>
2019-02-13 13:50:00 -0800

    addressed PR comments

--- endpoints/getting-started/client/main.go
+++ endpoints/getting-started/client/main.go
@@ -1,6 +1,16 @@
-// Copyright 2016 Google Inc. All rights reserved.
-// Use of this source code is governed by the Apache 2.0
-// license that can be found in the LICENSE file.
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // Command client performs authenticated requests against an Endpoints API server.
 package main
@@ -40,7 +50,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	resp, err := makeJWTRequest(jwt, *host, 10*time.Second)
+	resp, err := makeJWTRequest(jwt, *host)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -100,9 +110,9 @@ func generateJWT(saKeyfile, saEmail, audience string, expiryLength int64) (strin
 // [START endpoints_jwt_request]
 
 // makeJWTRequest sends an authorized request to your deployed endpoint.
-func makeJWTRequest(signedJWT, url string, timeout time.Duration) (string, error) {
+func makeJWTRequest(signedJWT, url string) (string, error) {
 	client := &http.Client{
-		Timeout: timeout,
+		Timeout: 10 * time.Second,
 	}
 
 	req, err := http.NewRequest("GET", url, nil)
