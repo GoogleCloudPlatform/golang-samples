1f2ffa1f35663f915d573e30d4443d91106c5167
Tyler Bui-Palsulich <tbp@google.com>
2019-03-27 11:21:32 -0400

    functions/codelabs: update

--- functions/codelabs/gopher/gopher.go
+++ functions/codelabs/gopher/gopher.go
@@ -30,9 +30,11 @@ func Gopher(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, fmt.Sprintf("Error reading file: %v", err), http.StatusInternalServerError)
 		return
 	}
+	defer f.Close()
 
 	// Write the gopher image to the response writer.
 	if _, err := io.Copy(w, f); err != nil {
 		http.Error(w, fmt.Sprintf("Error writing response: %v", err), http.StatusInternalServerError)
 	}
+	w.Header().Add("Content-Type", "image/png")
 }
