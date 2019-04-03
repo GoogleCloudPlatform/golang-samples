2f46774d1847833b8f3e575b448d4d66853c694f
Juan Jose Lopez Jaimez <jjlopezjaimez@google.com>
2019-04-03 15:48:53 -0400

    add license header to all the .go files

--- webrisk/webrisk_client.go
+++ webrisk/webrisk_client.go
@@ -1,10 +1,10 @@
-// Copyright 2016 Google Inc. All Rights Reserved.
+// Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+//     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 // Package webrisk implements a client for the Web Risk API v4.
-//
 // API v4 emphasizes efficient usage of the network for bandwidth-constrained
 // applications such as mobile devices. It achieves this by maintaining a small
 // portion of the server state locally such that some queries can be answered
