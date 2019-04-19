406aaf370203bb13d211f873604a906db019ae16
Tyler Bui-Palsulich <tbp@google.com>
2019-04-19 13:26:26 -0400

    jobs/v4: re-enable TestHistogramSearch

--- jobs/v4/howto/histogram_search_sample_test.go
+++ jobs/v4/howto/histogram_search_sample_test.go
@@ -24,7 +24,6 @@ import (
 )
 
 func TestHistogramSearch(t *testing.T) {
-	t.Skip("https://github.com/GoogleCloudPlatform/golang-samples/issues/822")
 	tc := testutil.SystemTest(t)
 	companyID := strings.SplitAfter(testCompany.Name, "companies/")[1]
 	testutil.Retry(t, 10, 1*time.Second, func(r *testutil.R) {
