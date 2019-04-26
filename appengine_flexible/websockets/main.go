046ab5a85c6cbcc60a987ba6651a100dc3fe1edd
Adam Ross <adamross@google.com>
2019-04-26 16:24:33 -0700

    appengine_flexible/websockets: add websocket sample app

---
+++ appengine_flexible/websockets/main.go
@@ -0,0 +1,72 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Sample websockets demonstrates an App Engine Flexible app.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("static")))
+	http.HandleFunc("/ws", websocketHandler)
+	http.HandleFunc("/_ah/health", healthCheckHandler)
+	log.Print("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// upgrader holds the websocket connection.
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// wsHandler addresses inbound websocket connection requests.
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/ws" {
+		http.NotFound(w, r)
+		return
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	defer func() {
+		conn.Close()
+	}()
+
+	if err != nil {
+			log.Printf("upgrader.Upgrade: %v", err)
+			return
+	}
+
+	for {
+    messageType, p, err := conn.ReadMessage()
+    if err != nil {
+				log.Printf("conn.ReadMessage: %v", err)
+        return
+    }
+    if err := conn.WriteMessage(messageType, p); err != nil {
+        log.Printf("conn.WriteMessage: %v", err)
+        return
+		}
+	}
+}
+
+// healthCheckHandler is used by App Engine Flex to check instance health.
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "ok")
+}
