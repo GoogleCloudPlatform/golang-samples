9c54d271f263543207a4729484a3ba6b728adc73
Dan Sanche <sanche@google.com>
2019-05-02 17:13:16 -0700

    removed package comment from majority of files

--- container_registry/container_analysis/src/sample/get_note.go
+++ container_registry/container_analysis/src/sample/get_note.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Samples for the Container Analysis golang libraries: https://cloud.google.com/container-registry/docs/container-analysis
 package sample
 
 // [START containeranalysis_get_note]
--- container_registry/container_analysis/src/sample/samples_test.go
+++ container_registry/container_analysis/src/sample/samples_test.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package sample provides code samples for the Container Analysis libraries: https://cloud.google.com/container-registry/docs/container-analysis
 package sample
 
 import (
