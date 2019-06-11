2663c4cc6880563b4fac409ae65d831a0b9d0f5a
Adam Ross <adamross@google.com>
2019-06-11 12:53:34 -0700

    monitoring/metric/readTimeSeriesValue: update comment to match

--- monitoring/custommetric/custommetric.go
+++ monitoring/custommetric/custommetric.go
@@ -86,7 +86,7 @@ func writeTimeSeriesValue(projectID, metricType string) error {
 
 // [START monitoring_read_timeseries_simple]
 
-// readTimeSeriesValue reads the TimeSeries for the value specified by metric type in a time window from the last 5 minutes.
+// readTimeSeriesValue reads the TimeSeries for the value specified by metric type in a time window from the last 20 minutes.
 func readTimeSeriesValue(projectID, metricType string) error {
 	ctx := context.Background()
 	c, err := monitoring.NewMetricClient(ctx)
