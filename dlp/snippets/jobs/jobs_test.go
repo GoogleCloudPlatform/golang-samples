1c932f0d71d7aa6d96a560767684b8fb5119beeb
Marcus Alder <maralder@google.com>
2019-08-15 13:03:01 -0400

    dlp: fix error

--- dlp/snippets/jobs/jobs_test.go
+++ dlp/snippets/jobs/jobs_test.go
@@ -17,13 +17,156 @@ package jobs
 
 import (
 	"bytes"
+	"context"
+	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
 	"regexp"
 	"strings"
 	"testing"
+	"time"
 
+	dlp "cloud.google.com/go/dlp/apiv2"
+	"cloud.google.com/go/pubsub"
 	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
+	dlppb "google.golang.org/genproto/googleapis/privacy/dlp/v2"
 )
+
+// setupPubSub creates a subscription to the given topic.
+func setupPubSub(projectID, topic, sub string) (*pubsub.Subscription, error) {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return nil, fmt.Errorf("pubsub.NewClient: %v", err)
+	}
+	// Create the Topic if it doesn't exist.
+	t := client.Topic(topic)
+	if exists, err := t.Exists(ctx); err != nil {
+		return nil, fmt.Errorf("error checking PubSub topic: %v", err)
+	} else if !exists {
+		if t, err = client.CreateTopic(ctx, topic); err != nil {
+			return nil, fmt.Errorf("error creating PubSub topic: %v", err)
+		}
+	}
+
+	// Create the Subscription if it doesn't exist.
+	s := client.Subscription(sub)
+	if exists, err := s.Exists(ctx); err != nil {
+		return nil, fmt.Errorf("error checking for subscription: %v", err)
+	} else if !exists {
+		if s, err = client.CreateSubscription(ctx, sub, pubsub.SubscriptionConfig{Topic: t}); err != nil {
+			return nil, fmt.Errorf("failed to create subscription: %v", err)
+		}
+	}
+
+	return s, nil
+}
+
+// riskNumerical computes the numerical risk of the given column.
+func riskNumerical(w io.Writer, projectID, dataProject, pubSubTopic, pubSubSub, datasetID, tableID, columnName string) error {
+	// projectID := "my-project-id"
+	// dataProject := "bigquery-public-data"
+	// pubSubTopic := "dlp-risk-sample-topic"
+	// pubSubSub := "dlp-risk-sample-sub"
+	// datasetID := "nhtsa_traffic_fatalities"
+	// tableID := "accident_2015"
+	// columnName := "state_number"
+	ctx := context.Background()
+	client, err := dlp.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("dlp.NewClient: %v", err)
+	}
+	// Create a PubSub Client used to listen for when the inspect job finishes.
+	pClient, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("Error creating PubSub client: %v", err)
+	}
+	defer pClient.Close()
+
+	// Create a PubSub subscription we can use to listen for messages.
+	s, err := setupPubSub(projectID, pubSubTopic, pubSubSub)
+	if err != nil {
+		return fmt.Errorf("setupPubSub: %v", err)
+	}
+
+	// topic is the PubSub topic string where messages should be sent.
+	topic := "projects/" + projectID + "/topics/" + pubSubTopic
+
+	// Create a configured request.
+	req := &dlppb.CreateDlpJobRequest{
+		Parent: "projects/" + projectID,
+		Job: &dlppb.CreateDlpJobRequest_RiskJob{
+			RiskJob: &dlppb.RiskAnalysisJobConfig{
+				// PrivacyMetric configures what to compute.
+				PrivacyMetric: &dlppb.PrivacyMetric{
+					Type: &dlppb.PrivacyMetric_NumericalStatsConfig_{
+						NumericalStatsConfig: &dlppb.PrivacyMetric_NumericalStatsConfig{
+							Field: &dlppb.FieldId{
+								Name: columnName,
+							},
+						},
+					},
+				},
+				// SourceTable describes where to find the data.
+				SourceTable: &dlppb.BigQueryTable{
+					ProjectId: dataProject,
+					DatasetId: datasetID,
+					TableId:   tableID,
+				},
+				// Send a message to PubSub using Actions.
+				Actions: []*dlppb.Action{
+					{
+						Action: &dlppb.Action_PubSub{
+							PubSub: &dlppb.Action_PublishToPubSub{
+								Topic: topic,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	// Create the risk job.
+	j, err := client.CreateDlpJob(context.Background(), req)
+	if err != nil {
+		return fmt.Errorf("CreateDlpJob: %v", err)
+	}
+	fmt.Fprintf(w, "Created job: %v\n", j.GetName())
+
+	// Wait for the risk job to finish by waiting for a PubSub message.
+	ctx, cancel := context.WithCancel(ctx)
+	err = s.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
+		// If this is the wrong job, do not process the result.
+		if msg.Attributes["DlpJobName"] != j.GetName() {
+			msg.Nack()
+			return
+		}
+		msg.Ack()
+		time.Sleep(500 * time.Millisecond)
+		resp, err := client.GetDlpJob(ctx, &dlppb.GetDlpJobRequest{
+			Name: j.GetName(),
+		})
+		if err != nil {
+			log.Fatalf("GetDlpJob: %v", err)
+		}
+		n := resp.GetRiskDetails().GetNumericalStatsResult()
+		fmt.Fprintf(w, "Value range: [%v, %v]\n", n.GetMinValue(), n.GetMaxValue())
+		var tmp string
+		for p, v := range n.GetQuantileValues() {
+			if v.String() != tmp {
+				fmt.Fprintf(w, "Value at %v quantile: %v\n", p, v)
+				tmp = v.String()
+			}
+		}
+		// Stop listening for more messages.
+		cancel()
+	})
+	if err != nil {
+		return fmt.Errorf("Recieve: %v", err)
+	}
+	return nil
+}
 
 func TestListJobs(t *testing.T) {
 	tc := testutil.SystemTest(t)
@@ -36,7 +179,7 @@ func TestListJobs(t *testing.T) {
 		buf.Reset()
 		err := listJobs(buf, tc.ProjectID, "", "RISK_ANALYSIS_JOB")
 		if err != nil {
-			t.Errorf("listJobs(%s, %s, %s) = error %q, want nil", buf, tc.ProjectID, err)
+			t.Errorf("listJobs(%s, %s, %s) = error %q, want nil", buf, tc.ProjectID, "", err)
 		}
 		s = buf.String()
 	}
