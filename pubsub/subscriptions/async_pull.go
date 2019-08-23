de8e5c4a53a40bbd5f8e47082107f9e24f511f8c
Marcus Alder <maralder@google.com>
2019-08-23 13:36:50 -0400

    pubsub: canonize Go samples (#937)

---
+++ pubsub/subscriptions/async_pull.go
@@ -0,0 +1,76 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package subscriptions
+
+// [START pubsub_subscriber_async_pull]
+// [START pubsub_quickstart_subscriber]
+import (
+	"context"
+	"fmt"
+	"io"
+	"sync"
+
+	"cloud.google.com/go/pubsub"
+)
+
+func pullMsgs(w io.Writer, projectID, subName string, topic *pubsub.Topic) error {
+	// projectID := "my-project-id"
+	// subName := projectID + "-example-sub"
+	// topic of type https://godoc.org/cloud.google.com/go/pubsub#Topic
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("pubsub.NewClient: %v", err)
+	}
+
+	// Publish 10 messages on the topic.
+	var results []*pubsub.PublishResult
+	for i := 0; i < 10; i++ {
+		res := topic.Publish(ctx, &pubsub.Message{
+			Data: []byte(fmt.Sprintf("hello world #%d", i)),
+		})
+		results = append(results, res)
+	}
+
+	// Check that all messages were published.
+	for _, r := range results {
+		_, err := r.Get(ctx)
+		if err != nil {
+			return fmt.Errorf("Get: %v", err)
+		}
+	}
+	// Consume 10 messages.
+	var mu sync.Mutex
+	received := 0
+	sub := client.Subscription(subName)
+	cctx, cancel := context.WithCancel(ctx)
+	err = sub.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
+		msg.Ack()
+		fmt.Fprintf(w, "Got message: %q\n", string(msg.Data))
+		mu.Lock()
+		defer mu.Unlock()
+		received++
+		if received == 10 {
+			cancel()
+		}
+	})
+	if err != nil {
+		return fmt.Errorf("Receive: %v", err)
+	}
+	return nil
+}
+
+// [END pubsub_subscriber_async_pull]
+// [END pubsub_quickstart_subscriber]
