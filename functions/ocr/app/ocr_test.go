43ee0efc07af3424fd3396d3fdcf29169766ca6a
Tyler Bui-Palsulich <tbp@google.com>
2019-09-18 14:08:45 -0400

    functions/ocr: use thanks/merci to test

--- functions/ocr/app/ocr_test.go
+++ functions/ocr/app/ocr_test.go
@@ -146,7 +146,7 @@ func TestTranslateText(t *testing.T) {
 		t.Errorf("language.Parse: %v", err)
 	}
 	data, err := json.Marshal(ocrMessage{
-		Text:     "Hello",
+		Text:     "Thanks",
 		FileName: menuName,
 		Lang:     fr,
 		SrcLang:  en,
@@ -165,8 +165,8 @@ func TestTranslateText(t *testing.T) {
 		t.Errorf("translateText: %v", err)
 	}
 	got := buf.String()
-	if len(got) == 0 {
-		t.Errorf("translateText got empty translation")
+	if want := "Merci"; !strings.Contains(got, want) {
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
 
