4b9f34935e1ba623a6ccb308f76ed5ef00403cdb
Alex Hong <hongalex@google.com>
2019-09-27 17:20:18 -0700

    increase test timeout for pull_concurrency to prevent flakiness

--- pubsub/subscriptions/pull_concurrency.go
+++ pubsub/subscriptions/pull_concurrency.go
@@ -42,8 +42,8 @@ func pullMsgsConcurrenyControl(w io.Writer, projectID, subName string) error {
 	// NumGoroutines is the number of goroutines sub.Receive will spawn to pull messages concurrently.
 	sub.ReceiveSettings.NumGoroutines = runtime.NumCPU()
 
-	// Receive messages for 5 seconds.
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	// Receive messages for 10 seconds.
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
 
 	// Create a channel to handle messages to as they come in.
