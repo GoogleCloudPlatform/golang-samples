b9bb7277d1378ec747a42d09ba93f9039ce6beaa
Tyler Bui-Palsulich <tbp@google.com>
2019-11-12 14:46:31 -0500

    getting-started/gce: comments, minor cleanup

--- getting-started/gce/startup-script.sh
+++ getting-started/gce/startup-script.sh
@@ -22,9 +22,9 @@ curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-in
 service google-fluentd restart &
 
 APP_LOCATION=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/app-location" -H "Metadata-Flavor: Google")
-gsutil cp $APP_LOCATION app.tar
-tar -xzf app.tar
+gsutil cp $APP_LOCATION app.tar.gz
+tar -xzf app.tar.gz
 
-# Start the service included in app.tar.
+# Start the service included in app.tar.gz.
 service my-app start
 # [END getting_started_gce_startup_script]
