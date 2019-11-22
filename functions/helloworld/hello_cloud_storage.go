6167be60e7df881ab6da085d9e9cd0d7995a313a
Frank Natividad <franknatividad@google.com>
2019-11-21 17:02:39 -0800

    lint

--- functions/helloworld/hello_cloud_storage.go
+++ functions/helloworld/hello_cloud_storage.go
@@ -24,32 +24,32 @@ import (
 
 // GCSEvent is the payload of a GCS event.
 type GCSEvent struct {
-	Kind                    string    `json:"kind"`
-	ID                      string    `json:"id"`
-	SelfLink                string    `json:"selfLink"`
-	Name                    string    `json:"name"`
-	Bucket                  string    `json:"bucket"`
-	Generation              string    `json:"generation"`
-	Metageneration          string    `json:"metageneration"`
-	ContentType             string    `json:"contentType"`
-	TimeCreated             time.Time `json:"timeCreated"`
-	Updated                 time.Time `json:"updated"`
-	TemporaryHold           bool      `json:"temporaryHold"`
-	EventBasedHold          bool      `json:"eventBasedHold"`
-	RetentionExpirationTime time.Time `json:"retentionExpirationTime"`
-	StorageClass            string    `json:"storageClass"`
-	TimeStorageClassUpdated time.Time `json:"timeStorageClassUpdated"`
-	Size                    string    `json:"size"`
-	Md5Hash                 string    `json:"md5Hash"`
-	MediaLink               string    `json:"mediaLink"`
-	ContentEncoding         string    `json:"contentEncoding"`
-	ContentDisposition      string    `json:"contentDisposition"`
-	CacheControl            string    `json:"cacheControl"`
-	Metadata map[string]interface{} `json:"metadata"`
-	Crc32c             string `json:"crc32c"`
-	ComponentCount     int    `json:"componentCount"`
-	Etag               string `json:"etag"`
-	CustomerEncryption struct {
+	Kind                    string                 `json:"kind"`
+	ID                      string                 `json:"id"`
+	SelfLink                string                 `json:"selfLink"`
+	Name                    string                 `json:"name"`
+	Bucket                  string                 `json:"bucket"`
+	Generation              string                 `json:"generation"`
+	Metageneration          string                 `json:"metageneration"`
+	ContentType             string                 `json:"contentType"`
+	TimeCreated             time.Time              `json:"timeCreated"`
+	Updated                 time.Time              `json:"updated"`
+	TemporaryHold           bool                   `json:"temporaryHold"`
+	EventBasedHold          bool                   `json:"eventBasedHold"`
+	RetentionExpirationTime time.Time              `json:"retentionExpirationTime"`
+	StorageClass            string                 `json:"storageClass"`
+	TimeStorageClassUpdated time.Time              `json:"timeStorageClassUpdated"`
+	Size                    string                 `json:"size"`
+	Md5Hash                 string                 `json:"md5Hash"`
+	MediaLink               string                 `json:"mediaLink"`
+	ContentEncoding         string                 `json:"contentEncoding"`
+	ContentDisposition      string                 `json:"contentDisposition"`
+	CacheControl            string                 `json:"cacheControl"`
+	Metadata                map[string]interface{} `json:"metadata"`
+	Crc32c                  string                 `json:"crc32c"`
+	ComponentCount          int                    `json:"componentCount"`
+	Etag                    string                 `json:"etag"`
+	CustomerEncryption      struct {
 		EncryptionAlgorithm string `json:"encryptionAlgorithm"`
 		KeySha256           string `json:"keySha256"`
 	}
