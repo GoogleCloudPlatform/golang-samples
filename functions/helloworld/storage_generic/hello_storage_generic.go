d713bd79d6ac866f427f95a579df13ddc3cb0db4
Frank Natividad <frankyn@users.noreply.github.com>
2019-11-21 16:13:45 -0800

    storage: add all resource metadata to GCSEvent

--- functions/helloworld/storage_generic/hello_storage_generic.go
+++ functions/helloworld/storage_generic/hello_storage_generic.go
@@ -28,12 +28,41 @@ import (
 
 // GCSEvent is the payload of a GCS event.
 type GCSEvent struct {
-	Bucket         string    `json:"bucket"`
-	Name           string    `json:"name"`
-	Metageneration string    `json:"metageneration"`
-	ResourceState  string    `json:"resourceState"`
-	TimeCreated    time.Time `json:"timeCreated"`
-	Updated        time.Time `json:"updated"`
+	Kind                    string    `json:"kind"`
+	ID                      string    `json:"id"`
+	SelfLink                string    `json:"selfLink"`
+	Name                    string    `json:"name"`
+	Bucket                  string    `json:"bucket"`
+	Generation              string    `json:"generation"`
+	Metageneration          string    `json:"metageneration"`
+	ContentType             string    `json:"contentType"`
+	TimeCreated             time.Time `json:"timeCreated"`
+	Updated                 time.Time `json:"updated"`
+	TemporaryHold           bool      `json:"temporaryHold"`
+	EventBasedHold          bool      `json:"eventBasedHold"`
+	RetentionExpirationTime time.Time `json:"retentionExpirationTime"`
+	StorageClass            string    `json:"storageClass"`
+	TimeStorageClassUpdated time.Time `json:"timeStorageClassUpdated"`
+	Size                    string    `json:"size"`
+	Md5Hash                 string    `json:"md5Hash"`
+	MediaLink               string    `json:"mediaLink"`
+	ContentEncoding         string    `json:"contentEncoding"`
+	ContentDisposition      string    `json:"contentDisposition"`
+	CacheControl            string    `json:"cacheControl"`
+	Metadata map[string]interface{} `json:"metadata"`
+
+	Owner struct {
+		Entity   string `json:"entity"`
+		EntityId string `json:"entityId"`
+	}
+	Crc32c             string `json:"crc32c"`
+	ComponentCount     int    `json:"componentCount"`
+	Etag               string `json:"etag"`
+	CustomerEncryption struct {
+		EncryptionAlgorithm string `json:"encryptionAlgorithm"`
+		KeySha256           string `json:"keySha256"`
+	}
+	KmsKeyName string `json:"kmsKeyName"`
 }
 
 // HelloGCSInfo prints information about a GCS event.
