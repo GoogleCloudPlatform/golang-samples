6935b2f3dae0484113d841363a7a732b385abc00
Tyler Bui-Palsulich <tbp@google.com>
2019-11-26 15:01:38 -0800

    internal/testutil: add comments

--- internal/testutil/testutil.go
+++ internal/testutil/testutil.go
@@ -98,8 +98,15 @@ func testContext() (Context, error) {
 	if err != nil {
 		return tc, fmt.Errorf("could not find golang-samples: %v", err)
 	}
+	// packages.Load returns multiple values, some with files and some without.
+	// Some of the files are generated as part of the build and some are the
+	// normal Go source files we're looking for.
+	// We can probably assume the one we want is pkgs[2], but loop through
+	// looking for the one we want in case it ever changes.
 	for _, pkg := range pkgs {
 		if len(pkg.GoFiles) > 0 && strings.HasSuffix(pkg.GoFiles[0], ".go") {
+			// Use the directory of a file in the root package as the module
+			// root directory.
 			tc.Dir = filepath.Dir(pkg.GoFiles[0])
 		}
 	}
