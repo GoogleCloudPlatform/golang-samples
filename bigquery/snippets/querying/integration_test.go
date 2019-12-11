62fe1847f3509bf2b8712427adf492317dcf38a1
Seth Hollyman <shollyman@google.com>
2019-12-11 23:15:35 +0000

    bigquery: refactor remaining snippets to new style.

---
+++ bigquery/snippets/querying/integration_test.go
@@ -0,0 +1,51 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package querying demonstrates usages of the BigQuery query interface.
+package querying
+
+import (
+	"context"
+	"io/ioutil"
+	"testing"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/GoogleCloudPlatform/golang-samples/bigquery/snippets/bqtestutil"
+	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
+)
+
+func TestAll(t *testing.T) {
+	tc := testutil.SystemTest(t)
+	ctx := context.Background()
+
+	client, err := bigquery.NewClient(ctx, tc.ProjectID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testDatasetID, err := bqtestutil.UniqueBQName("snippet_table_tests")
+	if err != nil {
+		t.Fatalf("couldn't generate unique resource name: %v", err)
+	}
+
+	meta := &bigquery.DatasetMetadata{
+		Location: "US", // See https://cloud.google.com/bigquery/docs/locations
+	}
+	if err := client.Dataset(testDatasetID).Create(ctx, meta); err != nil {
+		t.Fatalf("failed to create test dataset: %v", err)
+	}
+	// Cleanup dataset at end of test.
+	defer client.Dataset(testDatasetID).DeleteWithContents(ctx)
+
+}
---
+++ bigquery/snippets/table/bigquery_table_insert_rows.go
@@ -0,0 +1,61 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package table
+
+// [START bigquery_table_insert_rows]
+
+import (
+	"cloud.google.com/go/bigquery"
+	"context"
+	"fmt"
+)
+
+// Item represents a row item.
+type Item struct {
+	Name string
+	Age  int
+}
+
+// Save implements the ValueSaver interface.
+func (i *Item) Save() (map[string]bigquery.Value, string, error) {
+	return map[string]bigquery.Value{
+		"Name": i.Name,
+		"Age":  i.Age,
+	}, "", nil
+}
+
+func insertRows(projectID, datasetID, tableID string) error {
+	// projectID := "my-project-id"
+	// datasetID := "mydataset"
+	// tableID := "mytable"
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("bigquery.NewClient: %v", err)
+	}
+
+	inserter := client.Dataset(datasetID).Table(tableID).Inserter()
+	items := []*Item{
+		// Item implements the ValueSaver interface.
+		{Name: "Phred Phlyntstone", Age: 32},
+		{Name: "Wylma Phlyntstone", Age: 29},
+	}
+	if err := inserter.Put(ctx, items); err != nil {
+		return err
+	}
+	return nil
+}
+
+// [END bigquery_table_insert_rows]
