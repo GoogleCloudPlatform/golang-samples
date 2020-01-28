71d5777f8c3b4ca06c8946bc256d20ad5e4a05de
Seth Vargo <seth@sethvargo.com>
2020-01-28 00:24:23 -0500

    Add Secret Manager IAM samples

--- secretmanager/secretmanager_test.go
+++ secretmanager/secretmanager_test.go
@@ -106,6 +106,10 @@ func testCleanupSecret(tb testing.TB, name string) {
 	}
 }
 
+func testIamUser(tb testing.TB) string {
+	return "user:sethvargo@google.com"
+}
+
 func TestAccessSecretVersion(t *testing.T) {
 	tc := testutil.SystemTest(t)
 
@@ -274,6 +278,73 @@ func TestGetSecret(t *testing.T) {
 
 	if got, want := b.String(), "Found secret"; !strings.Contains(got, want) {
 		t.Errorf("getSecret: expected %q to contain %q", got, want)
+	}
+}
+
+func TestIamGrantAccess(t *testing.T) {
+	tc := testutil.SystemTest(t)
+
+	secret := testSecret(t, tc.ProjectID)
+	defer testCleanupSecret(t, secret.Name)
+
+	iamUser := testIamUser(t)
+
+	var b bytes.Buffer
+	if err := iamGrantAccess(&b, secret.Name, iamUser); err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := b.String(), "Updated IAM policy"; !strings.Contains(got, want) {
+		t.Errorf("getSecret: expected %q to contain %q", got, want)
+	}
+
+	client, ctx := testClient(t)
+	policy, err := client.IAM(secret.Name).Policy(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	found := false
+	members := policy.Members("roles/secretmanager.secretAccessor")
+	for _, m := range members {
+		if m == iamUser {
+			found = true
+		}
+	}
+
+	if !found {
+		t.Errorf("expected %q to include %q", members, iamUser)
+	}
+}
+
+func TestIamRevokeAccess(t *testing.T) {
+	tc := testutil.SystemTest(t)
+
+	secret := testSecret(t, tc.ProjectID)
+	defer testCleanupSecret(t, secret.Name)
+
+	iamUser := testIamUser(t)
+
+	var b bytes.Buffer
+	if err := iamRevokeAccess(&b, secret.Name, iamUser); err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := b.String(), "Updated IAM policy"; !strings.Contains(got, want) {
+		t.Errorf("getSecret: expected %q to contain %q", got, want)
+	}
+
+	client, ctx := testClient(t)
+	policy, err := client.IAM(secret.Name).Policy(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	members := policy.Members("roles/secretmanager.secretAccessor")
+	for _, m := range members {
+		if m == iamUser {
+			t.Errorf("expected %q to not include %q", members, iamUser)
+		}
 	}
 }
 
