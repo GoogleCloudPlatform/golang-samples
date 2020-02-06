bd07b8e7d5e2abc6dad70cb626793c567f25982a
Chris Cotter <cjcotter@google.com>
2020-02-06 18:38:47 -0500

    storage: fix broken s3_sdk test
    Changed this so it creates and cleans up its own HMAC key for
    testing purposes instead of relying on secrets in Keystore.
    
    Fixes #1213

--- storage/s3_sdk/list_gcs_buckets_test.go
+++ storage/s3_sdk/list_gcs_buckets_test.go
@@ -17,29 +17,78 @@ package s3sdk
 
 import (
 	"bytes"
+	"context"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 	"testing"
+
+	"cloud.google.com/go/storage"
+	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
 )
+
+var serviceAccountEmail = os.Getenv("GOLANG_SAMPLES_SERVICE_ACCOUNT_EMAIL")
+var storageClient *storage.Client
 
 func TestMain(m *testing.M) {
 	log.SetOutput(ioutil.Discard)
 	s := m.Run()
 	log.SetOutput(os.Stderr)
+
+	ctx := context.Background()
+	storageClient, _ = storage.NewClient(ctx)
+	defer storageClient.Close()
+
+	tc, _ := testutil.ContextMain(m)
+	key, err := createTestKey(tc.ProjectID)
+	if err != nil {
+		log.Fatalf("error setting up HMAC key: %v", err)
+	}
+	defer deleteTestKey(key)
+
 	os.Exit(s)
 }
 
+// Create a key for testing purposes and set environment variables
+func createTestKey(projectID string) (*storage.HMACKey, error) {
+	ctx := context.Background()
+	key, err := storageClient.CreateHMACKey(ctx, projectID, serviceAccountEmail)
+	if err != nil {
+		return nil, fmt.Errorf("storage.NewClient: %v", err)
+	}
+	if err = os.Setenv("STORAGE_HMAC_ACCESS_KEY_ID", key.AccessID); err != nil {
+		return key, fmt.Errorf("error setting STORAGE_HMAC_ACCESS_KEY_ID: %v", err)
+	}
+	if err = os.Setenv(key.Secret, "STORAGE_HMAC_ACCESS_SECRET_KEY"); err != nil {
+		return key, fmt.Errorf("error setting STORAGE_HMAC_ACCESS_SECRET_KEY: %v", err)
+	}
+	return key, nil
+}
+
+// Deactivate and delete the given key. Should operate as a teardown method.
+func deleteTestKey(key *storage.HMACKey) {
+	ctx := context.Background()
+	handle := storageClient.HMACKeyHandle(key.ProjectID, key.AccessID)
+	if key.State == "ACTIVE" {
+		handle.Update(ctx, storage.HMACKeyAttrsToUpdate{State: "INACTIVE"})
+	}
+	if key.State != "DELETED" {
+		handle.Delete(ctx)
+	}
+}
+
 func TestList(t *testing.T) {
-	t.Skip("HMAC secrets are not set up correctly: https://github.com/GoogleCloudPlatform/golang-samples/issues/1213")
 	googleAccessKeyID := os.Getenv("STORAGE_HMAC_ACCESS_KEY_ID")
 	googleAccessKeySecret := os.Getenv("STORAGE_HMAC_ACCESS_SECRET_KEY")
 
-	if googleAccessKeyID == "" || googleAccessKeySecret == "" {
-		t.Skip("STORAGE_HMAC_ACCESS_KEY_ID and STORAGE_HMAC_ACCESS_SECRET_KEY must be set. Skipping.")
+	if googleAccessKeyID == ""  {
+		t.Errorf("STORAGE_HMAC_ACCESS_KEY_ID must be set.")
 	}
-
+	if googleAccessKeySecret == "" {
+		t.Errorf("STORAGE_HMAC_ACCESS_SECRET_KEY must be set.")
+	}
 	buf := new(bytes.Buffer)
 	_, err := listGCSBuckets(buf, googleAccessKeyID, googleAccessKeySecret)
 	if err != nil {
