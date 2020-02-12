ae708275255b79c7c24631923b557f731b26d843
Chris Cotter <cjcotter@google.com>
2020-02-12 14:51:33 -0500

    add debug for request signing

--- storage/s3_sdk/list_gcs_buckets.go
+++ storage/s3_sdk/list_gcs_buckets.go
@@ -38,7 +38,7 @@ func listGCSBuckets(w io.Writer, googleAccessKeyID string, googleAccessKeySecret
 		Credentials: credentials.NewStaticCredentials(googleAccessKeyID, googleAccessKeySecret, ""),
 	}))
 
-	client := s3.New(sess, aws.NewConfig().WithLogLevel(aws.LogDebugWithHTTPBody))
+	client := s3.New(sess, aws.NewConfig().WithLogLevel(aws.LogDebugWithSigning))
 	ctx := context.Background()
 
 	ctx, cancel := context.WithTimeout(ctx, time.Second*10)
