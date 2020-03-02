9a03b3786a3c99d5340cb5f42003c30df95a4ac6
Tyler Bui-Palsulich <tbp@google.com>
2020-03-02 10:46:15 -0500

    all: goimports

--- bigquery/snippets/querying/bigquery_query_clustered_table.go
+++ bigquery/snippets/querying/bigquery_query_clustered_table.go
@@ -16,11 +16,12 @@ package querying
 
 // [START bigquery_query_clustered_table]
 import (
-	"cloud.google.com/go/bigquery"
 	"context"
 	"fmt"
-	"google.golang.org/api/iterator"
 	"io"
+
+	"cloud.google.com/go/bigquery"
+	"google.golang.org/api/iterator"
 )
 
 // queryClusteredTable demonstrates querying a table that has a clustering specification.
