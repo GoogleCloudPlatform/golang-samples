d136b66b01c8385720ea22900addec85bff02fd2
Jeff Williams <jefesaurus@google.com>
2020-03-01 20:39:42 -0800

    iam: replace deprecated iam.New() with iam.NewService()

--- iam/snippets/service_account_keys_list.go
+++ iam/snippets/service_account_keys_list.go
@@ -20,19 +20,15 @@ import (
 	"fmt"
 	"io"
 
-	"golang.org/x/oauth2/google"
 	iam "google.golang.org/api/iam/v1"
 )
 
 // listKey lists a service account's keys.
 func listKeys(w io.Writer, serviceAccountEmail string) ([]*iam.ServiceAccountKey, error) {
-	client, err := google.DefaultClient(context.Background(), iam.CloudPlatformScope)
+	ctx := context.Background()
+	service, err := iam.NewService(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("google.DefaultClient: %v", err)
-	}
-	service, err := iam.New(client)
-	if err != nil {
-		return nil, fmt.Errorf("iam.New: %v", err)
+		return nil, fmt.Errorf("iam.NewService: %v", err)
 	}
 
 	resource := "projects/-/serviceAccounts/" + serviceAccountEmail
