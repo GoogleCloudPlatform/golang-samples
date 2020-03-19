c82536f7e9a80e32c42af61ab165a34938452b2b
Anthony Wens <wens.ajw@gmail.com>
2020-03-19 16:31:37 -0700

    entity_management: removes unused create/delete samples

--- dialogflow/entity_management/entity_management.go
+++ dialogflow/entity_management/entity_management.go
@@ -23,7 +23,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 
 	dialogflow "cloud.google.com/go/dialogflow/apiv2"
 	dialogflowpb "google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
@@ -70,36 +69,6 @@ func main() {
 			}
 			fmt.Println("")
 		}
-	case "create":
-		creationFlagSet := flag.NewFlagSet("create", flag.ExitOnError)
-		var synonymsRaw string
-		creationFlagSet.StringVar(&synonymsRaw, "synonyms", "", "Comma-separated list of synonyms for the given entity: <SYNONYM_1>,<SYNONYM_2>,...,<SYNONYM_N>")
-		creationFlagSet.Parse(flag.Args()[1:])
-
-		if len(creationFlagSet.Args()) != 1 {
-			log.Fatal("No entity value passed to create")
-		}
-		entityValue := creationFlagSet.Arg(0)
-		synonyms := strings.Split(synonymsRaw, ",")
-
-		fmt.Printf("Creating entity %s...\n", entityValue)
-		err := CreateEntity(projectID, entityTypeID, entityValue, synonyms)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Entity type %s created under type %s\n", entityValue, entityTypeID)
-	case "delete":
-		if len(flag.Args()) != 2 {
-			log.Fatal("No entity value passed to delete")
-		}
-		entityValue := flag.Arg(1)
-
-		fmt.Printf("Deleting values %s under projects/%s/agent/entityTypes/%s...\n", entityValue, projectID, entityTypeID)
-		err = DeleteEntity(projectID, entityTypeID, entityValue)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Done!\n")
 	default:
 		flag.Usage()
 		os.Exit(1)
@@ -130,71 +99,3 @@ func ListEntities(projectID, entityTypeID string) ([]*dialogflowpb.EntityType_En
 
 	return entityType.GetEntities(), nil
 }
-
-// [START dialogflow_create_entity]
-func CreateEntity(projectID, entityTypeID, entityValue string, synonyms []string) error {
-	ctx := context.Background()
-
-	entityTypesClient, clientErr := dialogflow.NewEntityTypesClient(ctx)
-	if clientErr != nil {
-		return clientErr
-	}
-	defer entityTypesClient.Close()
-
-	if projectID == "" || entityTypeID == "" {
-		return errors.New(fmt.Sprintf("Received empty project (%s) or entity type (%s)", projectID, entityTypeID))
-	}
-
-	parent := fmt.Sprintf("projects/%s/agent/entityTypes/%s", projectID, entityTypeID)
-	entity := dialogflowpb.EntityType_Entity{Value: entityValue, Synonyms: synonyms}
-	entities := []*dialogflowpb.EntityType_Entity{&entity}
-
-	request := dialogflowpb.BatchCreateEntitiesRequest{Parent: parent, Entities: entities}
-
-	creationOp, requestErr := entityTypesClient.BatchCreateEntities(ctx, &request)
-	if requestErr != nil {
-		return requestErr
-	}
-
-	err := creationOp.Wait(ctx)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// [END dialogflow_create_entity]
-
-// [START dialogflow_delete_entity]
-func DeleteEntity(projectID, entityTypeID, entityValue string) error {
-	ctx := context.Background()
-
-	entityTypesClient, clientErr := dialogflow.NewEntityTypesClient(ctx)
-	if clientErr != nil {
-		return clientErr
-	}
-	defer entityTypesClient.Close()
-
-	if projectID == "" || entityTypeID == "" {
-		return errors.New(fmt.Sprintf("Received empty project (%s) or entity type (%s)", projectID, entityTypeID))
-	}
-
-	parent := fmt.Sprintf("projects/%s/agent/entityTypes/%s", projectID, entityTypeID)
-	entityValues := []string{entityValue}
-	request := dialogflowpb.BatchDeleteEntitiesRequest{Parent: parent, EntityValues: entityValues}
-
-	deletionOp, requestErr := entityTypesClient.BatchDeleteEntities(ctx, &request)
-	if requestErr != nil {
-		return requestErr
-	}
-
-	err := deletionOp.Wait(ctx)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// [END dialogflow_delete_entity]
