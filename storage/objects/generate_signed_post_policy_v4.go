e545f94cffe111f8190c3d0967d25d2260ed5d3e
Chris Cotter <cjcotter@google.com>
2020-05-05 16:16:06 -0400

    changes for Tyler's review

--- storage/objects/generate_signed_post_policy_v4.go
+++ storage/objects/generate_signed_post_policy_v4.go
@@ -17,6 +17,7 @@ package objects
 // [START storage_generate_signed_post_policy_v4]
 import (
 	"fmt"
+	"html/template"
 	"io"
 	"io/ioutil"
 	"time"
@@ -26,11 +27,11 @@ import (
 )
 
 // generateSignedPostPolicyV4 generates a signed post policy.
-func generateSignedPostPolicyV4(w io.Writer, bucket, object, serviceAccountJSON string) (*storage.PostPolicyV4, error) {
+func generateSignedPostPolicyV4(w io.Writer, bucket, object, serviceAccountJSONPath string) (*storage.PostPolicyV4, error) {
 	// bucket := "bucket-name"
 	// object := "object-name"
-	// serviceAccountJSON := "service_account.json"
-	jsonKey, err := ioutil.ReadFile(serviceAccountJSON)
+	// serviceAccountJSONPath := "service_account.json"
+	jsonKey, err := ioutil.ReadFile(serviceAccountJSONPath)
 	if err != nil {
 		return nil, fmt.Errorf("ioutil.ReadFile: %v", err)
 	}
@@ -56,16 +57,29 @@ func generateSignedPostPolicyV4(w io.Writer, bucket, object, serviceAccountJSON 
 	}
 
 	// Create an HTML form with the provided policy.
-	fmt.Fprintf(w, "<form action='%v' method='POST' enctype='multipart/form-data'>\n", policy.URL)
+	raw := fmt.Sprint("<form action='{{ index .url 0 }}' method='POST' enctype='multipart/form-data'>\n")
 
 	// Include all fields returned in the HTML form as they're required.
+	var data []string
+	var i int
 	for k, v := range policy.Fields {
-		fmt.Fprintf(w, "  <input name='%v' value='%v' type='hidden'/>\n", k, v)
+		raw += fmt.Sprintf("  <input name='{{ index .data %v }}' value='{{ index .data %v }}' type='hidden'/>\n", i, i+1)
+		data = append(data, k, v)
+		i += 2
 	}
+	raw += fmt.Sprint("  <input type='file' name='file'/><br />\n")
+	raw += fmt.Sprint("  <input type='submit' value='Upload File' name='submit'/><br />\n")
+	raw += fmt.Sprint("</form>")
 
-	fmt.Fprint(w, "  <input type='file' name='file'/><br />\n")
-	fmt.Fprint(w, "  <input type='submit' value='Upload File' name='submit'/><br />\n")
-	fmt.Fprint(w, "</form>")
+	t := template.New("post form")
+	t, err = t.Parse(raw)
+	if err != nil {
+		return policy, fmt.Errorf("parsing template: %v", err)
+	}
+	err = t.Execute(w, map[string][]string{"data": data, "url": {policy.URL}})
+	if err != nil {
+		return policy, fmt.Errorf("executing template: %v", err)
+	}
 
 	return policy, nil
 }
