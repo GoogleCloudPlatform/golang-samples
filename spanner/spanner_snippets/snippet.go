14772e9288149fd87ae8ec482419b7ef32bc1469
Hengfeng Li <hengfeng@google.com>
2020-07-01 09:05:22 +1000

    Remove build ignore.

--- spanner/spanner_snippets/snippet.go
+++ spanner/spanner_snippets/snippet.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build ignore
-
 // Command spanner_snippets contains runnable snippet code for Cloud Spanner.
 package main
 
