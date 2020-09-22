c158606d8f33158632f65cfc75190b78a1cbe616
Adam Ross <adamross@google.com>
2020-09-22 12:18:06 -0700

    fix(functions/console_snippets/http): error handling and unit tests (#1723)
    Fixes #1639 
    
    1. Invalid JSON results in a BadRequest error.
    2. No body / no message property results in default message.
    
    This is a retry on #1642, in which I broke the branch. This version has further refinements, so should be preferred regardless.

--- functions/console_snippets/http/http.go
+++ functions/console_snippets/http/http.go
@@ -19,6 +19,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"html"
+	"io"
+	"log"
 	"net/http"
 )
 
@@ -28,10 +30,19 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	var d struct {
 		Message string `json:"message"`
 	}
+
 	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello, World!")
-		return
+		switch err {
+		case io.EOF:
+			fmt.Fprint(w, "Hello, World!")
+			return
+		default:
+			log.Printf("json.NewDecoder: %v", err)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
 	}
+
 	if d.Message == "" {
 		fmt.Fprint(w, "Hello, World!")
 		return
