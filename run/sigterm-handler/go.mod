d2e817015bb2664fbad39035c526e932a31dd5f1
averikitsch <akitsch@google.com>
2021-01-19 10:14:00 -0800

    feat: Add sigterm handler sample

---
+++ run/sigterm-handler/go.mod
@@ -0,0 +1,5 @@
+module github.com/GoogleCloudPlatform/golang-samples/run/sigterm-handler
+
+go 1.13
+
+require golang.org/x/tools/gopls v0.6.6 // indirect
---
+++ run/sigterm-handler/main.go
@@ -0,0 +1,64 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Sample run-helloworld is a minimal Cloud Run service.
+package main
+
+import (
+	"context"
+	"cancel"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	go signalHandler(cancel)
+
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// [START cloudrun_sigterm_handler]
+func signalHandler(cancel context.CancelFunc) {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	sig := <-signalChan
+	log.Printf("%s signal caught.", sig)
+	cancel()
+}
+
+// [END cloudrun_sigterm_handler]
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World!\n")
+}
