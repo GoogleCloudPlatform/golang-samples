c1605ec9a4ef33d51c1eb7097d5841b0ec8e4a4e
Olav Loite <koloite@gmail.com>
2021-01-29 15:54:03 +0100

    fix: use 60 second timeout for GetOperation

--- spanner/spanner_snippets/spanner/spanner_add_column.go
+++ spanner/spanner_snippets/spanner/spanner_add_column.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"time"
 
 	database "cloud.google.com/go/spanner/admin/database/apiv1"
 	adminpb "google.golang.org/genproto/googleapis/spanner/admin/database/v1"
@@ -42,6 +43,8 @@ func addNewColumn(w io.Writer, db string) error {
 	if err != nil {
 		return err
 	}
+	ctx, cancel := context.WithTimeout(ctx, 60 * time.Second)
+	defer cancel()
 	if err := op.Wait(ctx); err != nil {
 		return err
 	}
