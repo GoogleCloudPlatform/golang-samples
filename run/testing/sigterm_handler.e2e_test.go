746edb892bbd5ee0780dff01279e984eb0c0b84c
averikitsch <akitsch@google.com>
2021-03-16 13:54:57 -0700

    Add tests

--- run/testing/sigterm_handler.e2e_test.go
+++ run/testing/sigterm_handler.e2e_test.go
@@ -15,10 +15,15 @@
 package cloudruntests
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
 	"testing"
 	"time"
+
+	"cloud.google.com/go/logging/logadmin"
+	"google.golang.org/api/iterator"
 
 	"github.com/GoogleCloudPlatform/golang-samples/internal/cloudrunci"
 	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
@@ -32,6 +37,7 @@ func TestSigtermHandlerService(t *testing.T) {
 	if err := service.Deploy(); err != nil {
 		t.Fatalf("service.Deploy %q: %v", service.Name, err)
 	}
+	defer GetLogEntries(service, t)
 	defer service.Clean()
 
 	requestPath := "/"
@@ -51,4 +57,41 @@ func TestSigtermHandlerService(t *testing.T) {
 	if got := resp.StatusCode; got != http.StatusOK {
 		t.Errorf("response status: got %d, want %d", got, http.StatusOK)
 	}
+}
+
+func GetLogEntries(service *cloudrunci.Service, t *testing.T) {
+	ctx := context.Background()
+	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	client, err := logadmin.NewClient(ctx, projectID)
+	if err != nil {
+		//FAIL?
+		t.Fatalf("Failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	fiveMinAgo := time.Now().Add(-5 * time.Minute)
+	timeFormat := fiveMinAgo.Format(time.RFC3339)
+	filter := fmt.Sprintf(`resource.labels.service_name="%s" timestamp>="%s"`, service.Name, timeFormat)
+	preparedFilter := fmt.Sprintf(`resource.type="cloud_run_revision" severity="default" %s  NOT protoPayload.serviceName="run.googleapis.com"`, filter)
+	MAX := 10
+	for i := 0; i < MAX; i++ {
+		fmt.Printf("get logs: %d \n", i)
+
+		time.Sleep(10 * time.Second)
+		it := client.Entries(ctx, logadmin.Filter(preparedFilter))
+		for {
+			entry, err := it.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				// TODO: Handle error.
+			}
+			fmt.Println(entry)
+			if entry.Payload == "terminated signal caught" {
+				break
+			}
+		}
+	}
+	t.Errorf("no log entries found.")
 }
