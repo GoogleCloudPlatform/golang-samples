110f94584b383991ac56b870737dd2c6f90e7283
Alex Hong <hongalex@google.com>
2021-03-18 11:48:58 -0700

    samples(pubsub): add schema samples

---
+++ pubsub/schemas/create_avro_schema.go
@@ -0,0 +1,48 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package schema
+
+// [START pubsub_create_avro_schema]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"cloud.google.com/go/pubsub"
+)
+
+func createAvroSchema(w io.Writer, projectID, schemaID string) error {
+	// projectID := "my-project-id"
+	// schemaID := "my-schema"
+	ctx := context.Background()
+	client, err := pubsub.NewSchemaClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("pubsub.NewSchemaClient: %v", err)
+	}
+	defer client.Close()
+
+	config := pubsub.SchemaConfig{
+		Type:       pubsub.SchemaAvro,
+		Definition: "",
+	}
+	s, err := client.CreateSchema(ctx, schemaID, config)
+	if err != nil {
+		return fmt.Errorf("CreateSchema: %v", err)
+	}
+	fmt.Fprintf(w, "Schema created: %v\n", s)
+	return nil
+}
+
+// [END pubsub_create_avro_schema]
