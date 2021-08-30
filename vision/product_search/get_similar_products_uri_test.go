2c8d8b59c173fc2666544cafa71307fcc811e6ce
Cody Oss <codyoss@google.com>
2021-08-30 09:29:21 -0600

    chore: skip some vision tests
    It appears some buckets/assests have disappeared. Skipping these
    tests to unblock other work.
    
    Updates: #2206

--- vision/product_search/get_similar_products_uri_test.go
+++ vision/product_search/get_similar_products_uri_test.go
@@ -23,6 +23,7 @@ import (
 )
 
 func TestGetSimilarProductsURI(t *testing.T) {
+	t.Skip("https://github.com/GoogleCloudPlatform/golang-samples/issues/2206")
 	tc := testutil.SystemTest(t)
 
 	const location = "us-west1"
