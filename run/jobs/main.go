28ef516bc0afc9da01ce894d497a80b1096c9b44
Dina Graves Portman <dinagraves@google.com>
2021-09-22 17:49:33 -0700

    feat: Adding cloud run jobs sample

---
+++ run/jobs/main.go
@@ -0,0 +1,69 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+
+// Retrieve Job-defined env vars
+var TASK_NUM = os.Getenv("TASK_NUM")
+var ATTEMPT_NUM = os.Getenv("ATTEMPT_NUM")
+
+// Retrieve User-defined env vars
+var SLEEP_MS = os.Getenv("SLEEP_MS")
+var FAIL_RATE = os.Getenv("FAIL_RATE")
+
+
+func main() {
+	log.Printf("Starting Task #%s, Attempt #%s ...", TASK_NUM, ATTEMPT_NUM)
+
+	// Simulate work
+    if SLEEP_MS != "" {
+    	// Convert SLEEP_MS from String to Int
+    	SLEEP_MS, _ := strconv.Atoi(SLEEP_MS)
+       	time.Sleep(time.Duration(SLEEP_MS) * time.Millisecond)
+    }
+
+    // Simulate errors
+    if FAIL_RATE != "" {
+    	randomFailure(FAIL_RATE)
+    }
+
+    log.Printf("Completed Task #%s, Attempt #%s", TASK_NUM, ATTEMPT_NUM)
+}
+
+
+// Throw an error based on fail rate
+func randomFailure(FAIL_RATE string) {
+	rate, err := strconv.ParseFloat(FAIL_RATE, 64)
+    
+    if (err != nil || rate < 0 || rate > 1) {
+     	log.Printf("Invalid FAIL_RATE env var value: %s. Must be a float between 0 and 1 inclusive.", FAIL_RATE);
+    	return 
+    }
+
+    rand.Seed(time.Now().UnixNano())
+    randomFailure := rand.Float64()
+
+    if (randomFailure < rate) {
+     	log.Fatalf("Task #%s, Attempt #%s failed.", TASK_NUM, ATTEMPT_NUM)
+    }
+}
---
+++ run/jobs/main_test.go
@@ -0,0 +1,53 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
+
+func init() {
+	os.Setenv("SLEEP_MS", "0")
+	os.Setenv("TASK_NUM", "1")
+	os.Setenv("ATTEMPT_NUM", "1")
+	os.Setenv("FAIL_RATE", "")
+}
+
+func TestSuccessfulJob(t *testing.T) {
+	var buf bytes.Buffer
+	writer := bufio.NewWriter(&buf)
+
+	main()
+
+	writer.Flush()
+	output := buf.String()
+
+	start := fmt.Sprintf("Starting Task #1, Attempt #1 ...")
+	finish := fmt.Sprintf("Completed Task #1, Attempt #1")
+	
+	if !(strings.Contains(output, start) && strings.Contains(output, finish)) {
+		t.Fatalf("\nExpected string to contain:\n%s\n%s\nGot: %s", start, finish, output)
+	}
+}
+
+func TestFailingJob(t *testing.T) {
+	os.Setenv("FAIL_RATE", "0.9999999")
+	main()
+}
