a62026b50806a1bb0e1517589dc0f0e5f89a6f4d
Christopher Wilcox <crwilcox@google.com>
2021-10-27 13:40:51 -0500

    refactor(firestore): Add tests, move firestore to firestore package, test main (#2276)
    Some snippets were previously only exercised by main and weren't evaluated on CI. Also some work to bring these samples closer to the contributor guide.
    
    b/144374993
    
    Tests elements of a main as part of their sub areas (save_test, query_test, etc)
    removes that main, as it isn't referenced, we have a firestore_quickstart/main.go
    add tests to firestore_quickstart/main.go
    move firestore_snippets to firestore/ and update package name to firestore (was main)

--- firestore/collection_group_query.go
+++ firestore/collection_group_query.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package firestore
 
 // [START firestore_query_collection_group_filter_eq]
 import (
