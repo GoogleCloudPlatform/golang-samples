08774b866b29d5638e2259f0d5e30f33fe90c963
BrennaEpp <brennae@google.com>
2021-12-07 14:52:40 -0600

    add explicit or

--- storage/objects/generate_v4_get_object_signed_url.go
+++ storage/objects/generate_v4_get_object_signed_url.go
@@ -39,7 +39,7 @@ func generateV4GetObjectSignedURL(w io.Writer, bucket, object string) (string, e
 	// Signing a URL requires credentials authorized to sign a URL. You can pass
 	// these in through SignedURLOptions with one of the following options:
 	// 		a. a Google service account private key, obtainable from the Google Developers Console
-	// 		b. a Google Access ID with iam.serviceAccounts.signBlob permissions
+	// or	b. a Google Access ID with iam.serviceAccounts.signBlob permissions
 	// or  	c. a SignBytes function implementing custom signing.
 	// In this example, none of these options are used, which means the following
 	// code only works when the credentials are defined via the environment variable
