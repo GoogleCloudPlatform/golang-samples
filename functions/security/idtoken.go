2bd24627dd5e411dc431c68a895f1348c2aed18e
Ace Nassri <anassri@google.com>
2022-03-10 15:09:46 -0800

    fix(serverless): clean up ID token example (#2472)
    * fix(serverless): clean up ID token example
    
    * Fix lint
    
    * Address comments
    
    * Address comments pt 2

--- functions/security/idtoken.go
+++ functions/security/idtoken.go
@@ -17,7 +17,6 @@ package security
 
 // [START functions_bearer_token]
 // [START cloudrun_service_to_service_auth]
-// [START run_service_to_service_auth]
 
 import (
 	"context"
@@ -27,14 +26,22 @@ import (
 	"google.golang.org/api/idtoken"
 )
 
-// makeGetRequest makes a request to the provided targetURL with an authenticated client.
-func makeGetRequest(w io.Writer, targetURL string) error {
-	// functionURL := "https://TARGET_URL"
+// `makeGetRequest` makes a request to the provided `targetURL`
+// with an authenticated client using audience `audience`.
+func makeGetRequest(w io.Writer, targetURL string, audience string) error {
+	// [END functions_bearer_token]
+	// Example `audience` value (Cloud Run): https://my-cloud-run-service.run.app/
+	// (`targetURL` and `audience` will differ for non-root URLs and GET parameters)
+	// [END cloudrun_service_to_service_auth]
+	// [START functions_bearer_token]
+	// Example `audience` value (Cloud Functions): https://<PROJECT>-<REGION>-<PROJECT_ID>.cloudfunctions.net/myFunction
+	// (`targetURL` and `audience` will differ for GET parameters)
+	// [START cloudrun_service_to_service_auth]
 	ctx := context.Background()
 
 	// client is a http.Client that automatically adds an "Authorization" header
 	// to any requests made.
-	client, err := idtoken.NewClient(ctx, targetURL)
+	client, err := idtoken.NewClient(ctx, audience)
 	if err != nil {
 		return fmt.Errorf("idtoken.NewClient: %v", err)
 	}
@@ -51,6 +58,5 @@ func makeGetRequest(w io.Writer, targetURL string) error {
 	return nil
 }
 
-// [END run_service_to_service_auth]
 // [END cloudrun_service_to_service_auth]
 // [END functions_bearer_token]
