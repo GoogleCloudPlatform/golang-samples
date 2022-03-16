b542a3a190464d0410eab049bc7bdf864177e65f
Cathy Ouyang <cathyo@google.com>
2022-03-16 15:00:29 -0700

    feat(storage): add setting client endpoint sample

---
+++ storage/service_account/client_endpoint/client_endpoint_test.go
@@ -0,0 +1,37 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clientendpoint
+
+import (
+    "bytes"
+    "strings"
+    "testing"
+
+    "github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
+)
+
+func TestSetClientEndpoint(t *testing.T) {
+    tc := testutil.SystemTest(t)
+    customEndpoint := "http://localhost:8080/storage/v1/"
+
+    var buf bytes.Buffer
+    if err := setClientEndpoint(&buf, customEndpoint); err != nil {
+        t.Errorf("setClientEndpoint in project %s: %s", tc.ProjectID, err)
+    }
+
+    if got, want := buf.String(), "request endpoint set for the client"; !strings.Contains(got, want) {
+        t.Errorf("setClientEndpoint: got %q; want to contain %q", got, want)
+    }
+} 
