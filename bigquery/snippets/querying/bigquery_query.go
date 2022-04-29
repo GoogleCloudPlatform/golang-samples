66107a06748ad0f17581fc54f795a3ca650a8e31
shollyman <shollyman@google.com>
2022-04-29 09:19:57 -0700

    chore(bigquery): switch bigquery query snippets to direct iterator (#2546)
    * chore(bigquery): switch bigquery query snippets to direct iterator

--- bigquery/snippets/querying/bigquery_query.go
+++ bigquery/snippets/querying/bigquery_query.go
@@ -40,19 +40,11 @@ func queryBasic(w io.Writer, projectID string) error {
 			"LIMIT 100")
 	// Location must match that of the dataset(s) referenced in the query.
 	q.Location = "US"
-	// Run the query and print results when the query job is completed.
-	job, err := q.Run(ctx)
+	// Run the query and process the returned row iterator.
+	it, err := q.Read(ctx)
 	if err != nil {
-		return err
+		return fmt.Errorf("query.Read(): %v", err)
 	}
-	status, err := job.Wait(ctx)
-	if err != nil {
-		return err
-	}
-	if err := status.Err(); err != nil {
-		return err
-	}
-	it, err := job.Read(ctx)
 	for {
 		var row []bigquery.Value
 		err := it.Next(&row)
