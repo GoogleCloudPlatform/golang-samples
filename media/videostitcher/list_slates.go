c78b086a33e87485bbe0c7da388f5bde05a2061d
Nicholas Cook <irataxy@users.noreply.github.com>
2022-05-23 14:02:41 -0700

    Update media/videostitcher/list_slates.go
    Co-authored-by: Cody Oss <6331106+codyoss@users.noreply.github.com>

--- media/videostitcher/list_slates.go
+++ media/videostitcher/list_slates.go
@@ -33,7 +33,7 @@ func listSlates(w io.Writer, projectID, location string) error {
 	ctx := context.Background()
 	client, err := stitcher.NewVideoStitcherClient(ctx)
 	if err != nil {
-		return fmt.Errorf("NewVideoStitcherClient: %v", err)
+		return fmt.Errorf("stitcher.NewVideoStitcherClient: %v", err)
 	}
 	defer client.Close()
 
