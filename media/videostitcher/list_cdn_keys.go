b50375a0e684e79dddfd003b696de08047519fa7
Nick Cook <nickcook@google.com>
2022-05-26 18:51:08 -0700

    feat: add Video Stitcher CDN key samples and tests

---
+++ media/videostitcher/list_cdn_keys.go
@@ -0,0 +1,60 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package videostitcher
+
+// [START video_stitcher_list_cdn_keys]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"google.golang.org/api/iterator"
+
+	stitcher "cloud.google.com/go/video/stitcher/apiv1"
+	stitcherpb "google.golang.org/genproto/googleapis/cloud/video/stitcher/v1"
+)
+
+// listCdnKeys gets all of the CDN keys for a given location.
+func listCdnKeys(w io.Writer, projectID string) error {
+	// projectID := "my-project-id"
+	location := "us-central1"
+	ctx := context.Background()
+	client, err := stitcher.NewVideoStitcherClient(ctx)
+	if err != nil {
+		return fmt.Errorf("stitcher.NewVideoStitcherClient: %v", err)
+	}
+	defer client.Close()
+
+	req := &stitcherpb.ListCdnKeysRequest{
+		Parent: fmt.Sprintf("projects/%s/locations/%s", projectID, location),
+	}
+
+	it := client.ListCdnKeys(ctx, req)
+	fmt.Fprintln(w, "CDN keys:")
+	for {
+		response, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return fmt.Errorf("it.Next(): %v", err)
+		}
+		fmt.Fprintln(w, response.GetName())
+	}
+
+	return nil
+}
+
+// [END video_stitcher_list_cdn_keys]
