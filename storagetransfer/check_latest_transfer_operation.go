c0f747edc2eaa4444f4219e7e4666ec9434199a5
JesseLovelace <43148100+JesseLovelace@users.noreply.github.com>
2022-06-08 15:27:21 -0700

    fix package statement

--- storagetransfer/check_latest_transfer_operation.go
+++ storagetransfer/check_latest_transfer_operation.go
@@ -10,7 +10,9 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.package storagetransfer
+// limitations under the License.
+
+package storagetransfer
 
 // [START storagetransfer_get_latest_transfer_operation]
 import (
