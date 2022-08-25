cde2a3cce88f83505c0ffb3ff4536b3355512a84
Alvaro Viebrantz <aviebrantz@google.com>
2022-08-24 21:36:23 -0400

    fix(bigquery): remove %w directive from log.Fatalf

--- bigquery/simpleapp/simpleapp.go
+++ bigquery/simpleapp/simpleapp.go
@@ -42,7 +42,7 @@ func main() {
 
 	client, err := bigquery.NewClient(ctx, projectID)
 	if err != nil {
-		log.Fatalf("bigquery.NewClient: %w", err)
+		log.Fatalf("bigquery.NewClient: %v", err)
 	}
 	defer client.Close()
 	// [END bigquery_simple_app_client]
