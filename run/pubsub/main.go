f5d4091bc26fdf0a0487ffdb32d75594c1f49b29
Jennifer Davis <iennae@gmail.com>
2022-08-26 11:04:54 -0700

    docs: clears up difference of go from other languages

--- run/pubsub/main.go
+++ run/pubsub/main.go
@@ -50,6 +50,7 @@ func main() {
 // PubSubMessage is the payload of a Pub/Sub event.
 // See the documentation for more details:
 // https://cloud.google.com/pubsub/docs/reference/rest/v1/PubsubMessage
+// byte slice unmarshalling handles base64 decoding
 type PubSubMessage struct {
 	Message struct {
 		Data []byte `json:"data,omitempty"`
