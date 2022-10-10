cb1f05a62bf96922a4da64c1475ab5c0f3225ec8
Sergey Davydov <sdavydov@google.com>
2022-10-10 18:20:32 +0200

    Flatten (most) of the Go Batch sample, getting rid of the hideous rightward drift

--- batch/create_with_script_no_mounting.go
+++ batch/create_with_script_no_mounting.go
@@ -38,71 +38,75 @@ func createScriptJob(w io.Writer, projectID, region, jobName string) error {
 	}
 	defer batchClient.Close()
 
+	// Define what will be done as part of the job.
+	task := new(batchpb.TaskSpec)
+	runnable := new(batchpb.Runnable)
+	// TODO: flatten this
+	runnable.Executable = &batchpb.Runnable_Script_{
+		Script: &batchpb.Runnable_Script{
+			Command: &batchpb.Runnable_Script_Text{
+				Text: "echo Hello world! This is task ${BATCH_TASK_INDEX}. This job has a total of ${BATCH_TASK_COUNT} tasks.",
+			},
+			// You can also run a script from a file. Just remember, that needs to be a script that's
+			// already on the VM that will be running the job. Using runnable.script.text and runnable.script.path is mutually exclusive.
+			// Command: &batchpb.Runnable_Script_Path{
+			// 	Path: "/tmp/test.sh",
+			// },
+		},
+	}
+	task.Runnables = []*batchpb.Runnable{runnable}
+
+	// We can specify what resources are requested by each task.
+	task.ComputeResource = &batchpb.ComputeResource {
+		CpuMilli:  2000, // in milliseconds per cpu-second. This means the task requires 2 whole CPUs.
+		MemoryMib: 16,
+	}
+
+	task.MaxRunDuration = &durationpb.Duration{
+		Seconds: 3600,
+	}
+	task.MaxRetryCount = 2
+
+	// Tasks are grouped inside a job using TaskGroups.
+	group := new(batchpb.TaskGroup)
+	group.TaskCount = 4;
+	group.TaskSpec = task;
+
+	// Policies are used to define on what kind of virtual machines the tasks will run on.
+	// In this case, we tell the system to use "e2-standard-4" machine type.
+	// Read more about machine types here: https://cloud.google.com/compute/docs/machine-types
+	allocationPolicy := new(batchpb.AllocationPolicy)
+	policy := new(batchpb.AllocationPolicy_InstancePolicy)
+	policy.MachineType = "e2-standard-4"
+	policyTemplate := new(batchpb.AllocationPolicy_InstancePolicyOrTemplate_Policy)
+	policyTemplate.Policy = policy
+	instances := new(batchpb.AllocationPolicy_InstancePolicyOrTemplate)
+	instances.PolicyTemplate = policyTemplate
+	allocationPolicy.Instances = []*batchpb.AllocationPolicy_InstancePolicyOrTemplate{instances}
+
+	job := new(batchpb.Job)
+	job.TaskGroups = []*batchpb.TaskGroup{group}
+	job.AllocationPolicy = allocationPolicy
+	job.Labels = map[string]string{"env": "testing", "type": "script"}
+	// We use Cloud Logging as it's an out of the box available option
+	job.LogsPolicy = new(batchpb.LogsPolicy)
+	job.LogsPolicy.Destination = batchpb.LogsPolicy_CLOUD_LOGGING
+
 	// The job's parent is the region in which the job will run
 	parent := fmt.Sprintf("projects/%s/locations/%s", projectID, region)
 
 	req := &batchpb.CreateJobRequest{
 		Parent: parent,
 		JobId:  jobName,
-		Job: &batchpb.Job{
-			// Tasks are grouped inside a job using TaskGroups.
-			TaskGroups: []*batchpb.TaskGroup{
-				{
-					TaskCount: 4,
-					// Define what will be done as part of the job.
-					TaskSpec: &batchpb.TaskSpec{
-						Runnables: []*batchpb.Runnable{{
-							Executable: &batchpb.Runnable_Script_{
-								Script: &batchpb.Runnable_Script{
-									Command: &batchpb.Runnable_Script_Text{
-										Text: "echo Hello world! This is task ${BATCH_TASK_INDEX}. This job has a total of ${BATCH_TASK_COUNT} tasks.",
-									},
-									// You can also run a script from a file. Just remember, that needs to be a script that's
-									// already on the VM that will be running the job. Using runnable.script.text and runnable.script.path is mutually exclusive.
-									// Command: &batchpb.Runnable_Script_Path{
-									// 	Path: "/tmp/test.sh",
-									// },
-								},
-							},
-						}},
-						// We can specify what resources are requested by each task.
-						ComputeResource: &batchpb.ComputeResource{
-							CpuMilli:  2000, // in milliseconds per cpu-second. This means the task requires 2 whole CPUs.
-							MemoryMib: 16,
-						},
-						MaxRunDuration: &durationpb.Duration{
-							Seconds: 3600,
-						},
-						MaxRetryCount: 2,
-					},
-				},
-			},
-			// Policies are used to define on what kind of virtual machines the tasks will run on.
-			// In this case, we tell the system to use "e2-standard-4" machine type.
-			// Read more about machine types here: https://cloud.google.com/compute/docs/machine-types
-			AllocationPolicy: &batchpb.AllocationPolicy{
-				Instances: []*batchpb.AllocationPolicy_InstancePolicyOrTemplate{{
-					PolicyTemplate: &batchpb.AllocationPolicy_InstancePolicyOrTemplate_Policy{
-						Policy: &batchpb.AllocationPolicy_InstancePolicy{
-							MachineType: "e2-standard-4",
-						},
-					},
-				}},
-			},
-			Labels: map[string]string{"env": "testing", "type": "script"},
-			// We use Cloud Logging as it's an out of the box available option
-			LogsPolicy: &batchpb.LogsPolicy{
-				Destination: batchpb.LogsPolicy_CLOUD_LOGGING,
-			},
-		},
+		Job: job,
 	}
 
-	job, err := batchClient.CreateJob(ctx, req)
+	created_job, err := batchClient.CreateJob(ctx, req)
 	if err != nil {
 		return fmt.Errorf("unable to create job: %v", err)
 	}
 
-	fmt.Fprintf(w, "Job created: %v\n", job)
+	fmt.Fprintf(w, "Job created: %v\n", created_job)
 
 	return nil
 }
