a061779b3796839d3ebbce23ec4dbe7a32406963
Alvaro Viebrantz <aviebrantz@google.com>
2022-11-25 17:52:54 -0400

    fix(bigquery): proper datatransfer package comment

--- bigquery/snippets/datatransfer/integration_test.go
+++ bigquery/snippets/datatransfer/integration_test.go
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package client demonstrates how to setup a BigQuery client.
+// Package datatransfer demonstrates interactions with the BigQuery
+// Data Transfer client.
 package client
 
 import (
