2584dd60c7aa4eca841990be93b8f276c258d683
Sergey Davydov <sdavydov@google.com>
2022-12-02 18:29:12 +0100

    Add initial sample of snapshot deletion by filter

---
+++ compute/snapshots/delete_snapshots_by_filter.go
@@ -0,0 +1,98 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package snippets
+
+// [START compute_snapshot_delete_by_filter]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	compute "cloud.google.com/go/compute/apiv1"
+	"google.golang.org/api/iterator"
+	computepb "google.golang.org/genproto/googleapis/cloud/compute/v1"
+)
+
+// Deletes ALL disk snapshots in the project that match the filter
+func deleteByFilter(w io.Writer, projectID, filter string) error {
+	// projectID := "your_project_id"
+	// Learn more about filters:
+	// https://cloud.google.com/python/docs/reference/compute/latest/google.cloud.compute_v1.types.ListSnapshotsRequest
+	// filter := ""
+	
+	it, err := snapshotList(w, projectID, filter)
+	if err != nil {
+		return fmt.Errorf("NewSnapshotsRESTClient: %v", err)
+	}
+
+	for {
+		snapshot, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		delete(w, projectID, *snapshot.Name)
+	}
+	return nil
+}
+
+// snapshotList returns an iterator over disk snapshots in the given project
+func snapshotList(w io.Writer, projectID, filter string) (*compute.SnapshotIterator, error) {
+	ctx := context.Background()
+	snapshotsClient, err := compute.NewSnapshotsRESTClient(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("NewSnapshotsRESTClient: %v", err)
+	}
+	defer snapshotsClient.Close()
+
+	req := &computepb.ListSnapshotsRequest{
+		Project: projectID,
+		Filter:  &filter,
+	}
+
+	return snapshotsClient.List(ctx, req), nil
+}
+
+// deletes a disk snapshot
+func delete(w io.Writer, projectID, snapshotName string) error {
+	ctx := context.Background()
+	snapshotsClient, err := compute.NewSnapshotsRESTClient(ctx)
+	if err != nil {
+		return fmt.Errorf("NewSnapshotsRESTClient: %v", err)
+	}
+	defer snapshotsClient.Close()
+
+	req := &computepb.DeleteSnapshotRequest{
+		Project:  projectID,
+		Snapshot: snapshotName,
+	}
+
+	op, err := snapshotsClient.Delete(ctx, req)
+	if err != nil {
+		return fmt.Errorf("unable to delete snapshot: %v", err)
+	}
+
+	if err = op.Wait(ctx); err != nil {
+		return fmt.Errorf("unable to wait for the operation: %v", err)
+	}
+
+	fmt.Fprintf(w, "Snapshot %s deleted\n", snapshotName)
+
+	return nil
+}
+
+// [END compute_snapshot_delete_by_filter]
