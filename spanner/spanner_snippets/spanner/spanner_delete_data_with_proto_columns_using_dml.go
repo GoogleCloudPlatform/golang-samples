de0a4954175afba71f51c9589b65b62434d31b88
harshachinta <sriharshach@google.com>
2022-12-05 17:27:12 +0000

    feat(spanner): add samples for Proto columns - Proto Message and Proto Enum

---
+++ spanner/spanner_snippets/spanner/spanner_delete_data_with_proto_columns_using_dml.go
@@ -0,0 +1,51 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package spanner
+
+// [START spanner_delete_data_with_proto_columns_using_dml]
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"cloud.google.com/go/spanner"
+	"google.golang.org/api/option"
+)
+
+func deleteDataWithProtoMsgAndEnumUsingDML(w io.Writer, db string) error {
+	ctx := context.Background()
+	endpoint := "staging-wrenchworks.sandbox.googleapis.com:443"
+	options := []option.ClientOption{option.WithEndpoint(endpoint)}
+	client, err := spanner.NewClient(ctx, db, options...)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	_, err = client.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		stmt := spanner.Statement{SQL: `DELETE FROM Singers WHERE SingerId = 6`}
+
+		rowCount, err := txn.Update(ctx, stmt)
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(w, "%d record(s) deleted.\n", rowCount)
+		return nil
+	})
+	return err
+}
+
+// [END spanner_delete_data_with_proto_columns_using_dml]
---
+++ spanner/spanner_snippets/spanner/spanner_insert_data_with_proto_columns.go
@@ -0,0 +1,87 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package spanner
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"cloud.google.com/go/spanner"
+	pb "github.com/GoogleCloudPlatform/golang-samples/spanner/spanner_snippets/spanner/testdata/protos"
+	"google.golang.org/api/option"
+	"google.golang.org/protobuf/proto"
+)
+
+// [START spanner_insert_data_with_proto_columns]
+
+func insertDataWithProtoMsgAndEnum(w io.Writer, db string) error {
+	ctx := context.Background()
+	endpoint := "staging-wrenchworks.sandbox.googleapis.com:443"
+	options := []option.ClientOption{option.WithEndpoint(endpoint)}
+	client, err := spanner.NewClient(ctx, db, options...)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	singer1ProtoEnum := pb.Genre_ROCK
+	singer1ProtoMsg := &pb.SingerInfo{
+		SingerId:    proto.Int64(1),
+		BirthDate:   proto.String("January"),
+		Nationality: proto.String("Country1"),
+		Genre:       &singer1ProtoEnum,
+	}
+
+	singer2ProtoEnum := pb.Genre_FOLK
+	singer2ProtoMsg := &pb.SingerInfo{
+		SingerId:    proto.Int64(2),
+		BirthDate:   proto.String("February"),
+		Nationality: proto.String("Country2"),
+		Genre:       &singer2ProtoEnum,
+	}
+
+	singer3ProtoEnum := pb.Genre_JAZZ
+	singer3ProtoMsg := &pb.SingerInfo{
+		SingerId:    proto.Int64(3),
+		BirthDate:   proto.String("March"),
+		Nationality: proto.String("Country3"),
+		Genre:       &singer3ProtoEnum,
+	}
+
+	singer4ProtoEnum := pb.Genre_POP
+	singer4ProtoMsg := &pb.SingerInfo{
+		SingerId:    proto.Int64(4),
+		BirthDate:   proto.String("April"),
+		Nationality: proto.String("Country4"),
+		Genre:       &singer4ProtoEnum,
+	}
+
+	cols := []string{"SingerId", "FirstName", "LastName", "SingerInfo", "SingerGenre"}
+	m := []*spanner.Mutation{
+		spanner.InsertOrUpdate("Singers", cols, []interface{}{1, "Singer1", "Singer1", singer1ProtoMsg, &singer1ProtoEnum}),
+		spanner.InsertOrUpdate("Singers", cols, []interface{}{2, "Singer2", "Singer2", singer2ProtoMsg, &singer2ProtoEnum}),
+		spanner.InsertOrUpdate("Singers", cols, []interface{}{3, "Singer3", "Singer3", singer3ProtoMsg, &singer3ProtoEnum}),
+		spanner.InsertOrUpdate("Singers", cols, []interface{}{4, "Singer4", "Singer4", singer4ProtoMsg, &singer4ProtoEnum}),
+	}
+	_, err = client.Apply(ctx, m)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(w, "Inserted data to SingerInfo and SingerGenre columns")
+	return nil
+}
+
+// [END spanner_insert_data_with_proto_columns]
