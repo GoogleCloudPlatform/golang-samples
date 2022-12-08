34db2773573236ae12f76b9ba34bd114a90c2901
Sergey Davydov <sdavydov@google.com>
2022-12-08 17:14:08 +0100

    Add sample compute_regional_disk_create_from_disk

---
+++ compute/disks/create_regional_from_disk.go
@@ -0,0 +1,76 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package snippets
+
+// [START compute_regional_disk_create_from_disk]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	compute "cloud.google.com/go/compute/apiv1"
+	computepb "google.golang.org/genproto/googleapis/cloud/compute/v1"
+	"google.golang.org/protobuf/proto"
+)
+
+// createRegionalDiskFromDisk creates a new regional disk with the contents of
+// an already existitng zonal disk or disk snapshot. Type and size may differ.
+func createRegionalDiskFromDisk(
+	w io.Writer,
+	projectID, region, diskName, diskType, diskLink, snapshotLink string,
+	diskSizeGb int64,
+) error {
+	// projectID := "your_project_id"
+	// region := "europe-central2-b"
+	// diskName := "your_disk_name"
+	// diskType := "regions/us-west3/diskTypes/pd-ssd"
+	// diskLink := "projects/project_name/global/disks/disk_name"
+	// snapshotLink := "projects/project_name/global/snapshots/snapshot_name"
+	// diskSizeGb := 120
+
+	ctx := context.Background()
+	disksClient, err := compute.NewRegionDisksRESTClient(ctx)
+	if err != nil {
+		return fmt.Errorf("NewRegionDisksRESTClient: %v", err)
+	}
+	defer disksClient.Close()
+
+	req := &computepb.InsertRegionDiskRequest{
+		Project: projectID,
+		Region:  region,
+		DiskResource: &computepb.Disk{
+			Name:       proto.String(diskName),
+			Region:     proto.String(region),
+			Type:       proto.String(diskType),
+			SourceDisk: proto.String(diskLink),
+			SizeGb:     proto.Int64(diskSizeGb),
+		},
+	}
+
+	op, err := disksClient.Insert(ctx, req)
+	if err != nil {
+		return fmt.Errorf("unable to create disk: %v", err)
+	}
+
+	if err = op.Wait(ctx); err != nil {
+		return fmt.Errorf("unable to wait for the operation: %v", err)
+	}
+
+	fmt.Fprintf(w, "Disk created\n")
+
+	return nil
+}
+
+// [END compute_regional_disk_create_from_disk]
