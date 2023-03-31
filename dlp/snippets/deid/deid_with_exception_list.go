e37a3ba10041c637b38bfa18ac467c3668376312
aarshdhokai_crest <aarsh.dhokai@crestdatasys.com>
2023-03-31 11:28:22 +0530

    added sample dlp_deidentify_exception_list with test

---
+++ dlp/snippets/deid/deid_with_exception_list.go
@@ -0,0 +1,130 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package deid
+
+// [START dlp_deidentify_exception_list]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	dlp "cloud.google.com/go/dlp/apiv2"
+	"cloud.google.com/go/dlp/apiv2/dlppb"
+)
+
+// deidentifyExceptionList creates an exception list for a regular custom dictionary detector.
+func deidentifyExceptionList(w io.Writer, projectID, input, infoType, excludeInfoType string, dictWordList []string) error {
+	// projectID := "my-project-id"
+	// input := "jack@example.org accessed customer record of user5@example.com"
+	// infoType := "EMAIL_ADDRESS"
+	// excludeInfoType := "DEVELOPER_EMAIL"
+	// dictWordList := []string{"jack@example.org", "jill@example.org"}
+	ctx := context.Background()
+	// Initialize a client once and reuse it to send multiple requests. Clients
+	// are safe to use across goroutines. When the client is no longer needed,
+	// call the Close method to cleanup its resources.
+	client, err := dlp.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("dlp.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	// Specify what content you want the service to DeIdentify.
+	var item = &dlppb.ContentItem{
+		DataItem: &dlppb.ContentItem_Value{
+			Value: input,
+		},
+	}
+
+	// Construct the custom word list to be detected.
+	var wordList = &dlppb.CustomInfoType_Dictionary_WordList{
+		Words: dictWordList,
+	}
+
+	// Specify the word list custom info type and build-in info type the inspection will look for.
+	var infoTypes = []*dlppb.InfoType{
+		{Name: infoType},
+	}
+	inspectRuleSet := &dlppb.InspectionRuleSet{
+		InfoTypes: infoTypes,
+		Rules: []*dlppb.InspectionRule{
+			{
+				Type: &dlppb.InspectionRule_ExclusionRule{
+					ExclusionRule: &dlppb.ExclusionRule{
+						MatchingType: dlppb.MatchingType_MATCHING_TYPE_FULL_MATCH,
+						Type: &dlppb.ExclusionRule_ExcludeInfoTypes{
+							ExcludeInfoTypes: &dlppb.ExcludeInfoTypes{
+								InfoTypes: []*dlppb.InfoType{
+									{Name: excludeInfoType},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	// Create a configured request.
+	req := &dlppb.DeidentifyContentRequest{
+		Parent: fmt.Sprintf("projects/%s/locations/global", projectID),
+		// Construct the configuration for the de-id request and list all desired transformations.
+		DeidentifyConfig: &dlppb.DeidentifyConfig{
+			Transformation: &dlppb.DeidentifyConfig_InfoTypeTransformations{
+				InfoTypeTransformations: &dlppb.InfoTypeTransformations{
+					Transformations: []*dlppb.InfoTypeTransformations_InfoTypeTransformation{
+						{
+							PrimitiveTransformation: &dlppb.PrimitiveTransformation{
+								Transformation: &dlppb.PrimitiveTransformation_ReplaceWithInfoTypeConfig{},
+							},
+						},
+					},
+				},
+			},
+		},
+		InspectConfig: &dlppb.InspectConfig{
+			InfoTypes: infoTypes,
+			// Construct the custom dictionary detector associated with the word list.
+			CustomInfoTypes: []*dlppb.CustomInfoType{
+				{
+					InfoType: &dlppb.InfoType{
+						Name: excludeInfoType,
+					},
+					Type: &dlppb.CustomInfoType_Dictionary_{
+						Dictionary: &dlppb.CustomInfoType_Dictionary{
+							Source: &dlppb.CustomInfoType_Dictionary_WordList_{
+								WordList: wordList,
+							},
+						},
+					},
+				},
+			},
+			RuleSet: []*dlppb.InspectionRuleSet{inspectRuleSet},
+		},
+		// The item to analyze.
+		Item: item,
+	}
+
+	// Send the request.
+	resp, err := client.DeidentifyContent(ctx, req)
+	if err != nil {
+		return fmt.Errorf("DeidentifyContent: %v", err)
+	}
+
+	// Print the result.
+	fmt.Fprintf(w, "output : %v", resp.GetItem().GetValue())
+	return nil
+
+}
+
+// [END dlp_deidentify_exception_list]
