d20098496fbe86628a835d1e3e707397e7f2cd45
Seth Hollyman <shollyman@google.com>
2023-04-05 22:23:20 +0000

    docs(bigquery): move package declaration inside sample scope
    Quick PR to add the package declaration to the sample scope.  Conforms
    to [style
    guidance](https://github.com/GoogleCloudPlatform/golang-samples/blob/main/CONTRIBUTING.md#include-imports-and-flags-in-region-tags)
    for quickstarts.
    
    Requested by TW for readability purposes.

--- bigquery/simpleapp/simpleapp.go
+++ bigquery/simpleapp/simpleapp.go
@@ -12,11 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// [START bigquery_simple_app_all]
+// [START bigquery_simple_app_deps]
+
 // Command simpleapp queries the Stack Overflow public dataset in Google BigQuery.
 package main
 
-// [START bigquery_simple_app_all]
-// [START bigquery_simple_app_deps]
 import (
 	"context"
 	"fmt"
