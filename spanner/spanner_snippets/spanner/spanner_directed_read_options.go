5fb335ce6b29078693150a9fcb40406a3ded98c4
Sri Harsha CH <sriharshach@google.com>
2023-04-10 07:07:58 +0000

    `feat(spanner): add samples for directed reads`

--- spanner/spanner_snippets/spanner/spanner_directed_read_options.go
+++ spanner/spanner_snippets/spanner/spanner_directed_read_options.go
@@ -24,6 +24,26 @@ import (
 	"google.golang.org/api/iterator"
 )
 
+/*
+	Shows how to run a query with directed read options.
+	Only one of ExcludeReplicas or IncludeReplicas can be set
+	Each accepts a list of ReplicaSelections which contains Location and Type
+	* `location` - The location must be one of the regions within the
+	multi-region configuration of your database.
+	* `type` - The type of the replica
+	Some examples of using replica_selectors are:
+	* `location:us-east1` --> The "us-east1" replica(s) of any available type
+		will be used to process the request.
+	* `type:READ_ONLY`    --> The "READ_ONLY" type replica(s) in nearest
+	available location will be used to process the
+	request.
+	* `location:us-east1 type:READ_ONLY` --> The "READ_ONLY" type replica(s)
+	in location "us-east1" will be used to process the request.
+		IncludeReplicas also contains an option for AutoFailover which when set
+	Spanner will not route requests to a replica outside the
+	IncludeReplicas list when all the specified replicas are unavailable
+	or unhealthy. The default value is `false`
+*/
 // [START spanner_directed_read]
 func directedReadOptions(w io.Writer, db string) error {
 	// db = `projects/<project>/instances/<instance-id>/database/<database-id>`
