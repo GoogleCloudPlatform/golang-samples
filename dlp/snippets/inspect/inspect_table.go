78a2584bfd17161672fae0b4dff0d0af355a1fcc
aarshdhokai_crest <aarsh.dhokai@crestdatasys.com>
2023-04-11 18:32:19 +0530

    dlp_inspect_table

---
+++ dlp/snippets/inspect/inspect_table.go
@@ -0,0 +1,112 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package inspect
+
+// [START dlp_inspect_table]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	dlp "cloud.google.com/go/dlp/apiv2"
+	"cloud.google.com/go/dlp/apiv2/dlppb"
+	"google.golang.org/api/option"
+)
+
+// inspectTable inspects a table for sensitive content
+func inspectTable(w io.Writer, projectID string, tableToInspect *dlppb.Table) error {
+	// projectID := "your-project-id"
+	// tableToInspect := "your-table"
+
+	ctx := context.Background()
+
+	// Initialize a client once and reuse it to send multiple requests. Clients
+	// are safe to use across goroutines. When the client is no longer needed,
+	// call the Close method to cleanup its resources.
+	client, err := dlp.NewRESTClient(ctx, option.WithCredentialsFile("C:/Users/aarsh.dhokai/Desktop/cred.json"))
+	if err != nil {
+		return err
+	}
+
+	// Closing the client safely cleans up background resources.
+	defer client.Close()
+
+	// create a default table
+	if tableToInspect == nil {
+		tableToInspect = &dlppb.Table{
+			Headers: []*dlppb.FieldId{
+				{Name: "name"},
+				{Name: "phone"},
+			},
+			Rows: []*dlppb.Table_Row{
+				{
+					Values: []*dlppb.Value{
+						{
+							Type: &dlppb.Value_StringValue{
+								StringValue: "John Doe",
+							},
+						},
+						{
+							Type: &dlppb.Value_StringValue{
+								StringValue: "(206) 555-0123",
+							},
+						},
+					},
+				},
+			},
+		}
+	}
+
+	// Specify the table to be inspected.
+	var contentItem = &dlppb.ContentItem{
+		DataItem: &dlppb.ContentItem_Table{
+			Table: tableToInspect,
+		},
+	}
+
+	// Specify the type of info the inspection will look for.
+	// See https://cloud.google.com/dlp/docs/infotypes-reference for complete list of info types
+	var infoTypes = []*dlppb.InfoType{
+		{Name: "PHONE_NUMBER"},
+	}
+
+	// Construct the Inspect request to be sent by the client.
+	req := &dlppb.InspectContentRequest{
+		Parent: fmt.Sprintf("projects/%s/locations/global", projectID),
+		Item:   contentItem,
+		InspectConfig: &dlppb.InspectConfig{
+			InfoTypes:    infoTypes,
+			IncludeQuote: true,
+		},
+	}
+
+	// Send the request.
+	resp, err := client.InspectContent(ctx, req)
+	if err != nil {
+		fmt.Fprintf(w, "Receive: %v", err)
+		return err
+	}
+
+	// Print the results.
+	fmt.Fprintf(w, "Findings: %v\n", len(resp.Result.Findings))
+	for _, v := range resp.GetResult().Findings {
+		fmt.Fprintf(w, "Quote: %v\n", v.GetQuote())
+		fmt.Fprintf(w, "Infotype Name: %v\n", v.GetInfoType().GetName())
+		fmt.Fprintf(w, "Likelihood: %v\n", v.GetLikelihood())
+	}
+	return nil
+
+}
+
+// [END dlp_inspect_table]
