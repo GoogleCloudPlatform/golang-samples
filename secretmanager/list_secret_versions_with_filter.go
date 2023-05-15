0eb07b21890d506366b92f7505864227ea8e2431
rsamborski <rsamborski@gmail.com>
2023-05-15 14:13:29 +0200

    chore(fixit): Go Error Visibility for secretmanager

--- secretmanager/list_secret_versions_with_filter.go
+++ secretmanager/list_secret_versions_with_filter.go
@@ -37,7 +37,7 @@ func listSecretVersionsWithFilter(w io.Writer, parent string, filter string) err
 	ctx := context.Background()
 	client, err := secretmanager.NewClient(ctx)
 	if err != nil {
-		return fmt.Errorf("failed to create secretmanager client: %v", err)
+		return fmt.Errorf("failed to create secretmanager client: %w", err)
 	}
 	defer client.Close()
 
@@ -56,7 +56,7 @@ func listSecretVersionsWithFilter(w io.Writer, parent string, filter string) err
 		}
 
 		if err != nil {
-			return fmt.Errorf("failed to list secret versions: %v", err)
+			return fmt.Errorf("failed to list secret versions: %w", err)
 		}
 
 		fmt.Fprintf(w, "Found secret version %s with state %s\n",
