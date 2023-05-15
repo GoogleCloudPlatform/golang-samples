9cbfc6c0db68323b0a566dcfaaf5e60b74ee049d
Aarsh Dhokai <105281389+AarshD-crest@users.noreply.github.com>
2023-05-16 03:42:02 +0530

    feat(dlp): added a sample for inspect string with exclusion regex (#3013)
    Co-authored-by: Eric Schmidt <erschmid@google.com>

--- dlp/snippets/inspect/inspect_test.go
+++ dlp/snippets/inspect/inspect_test.go
@@ -277,6 +277,25 @@ func TestInspectBigquery(t *testing.T) {
 	}
 }
 
+func TestInspectStringWithExclusionRegex(t *testing.T) {
+	tc := testutil.SystemTest(t)
+
+	var buf bytes.Buffer
+
+	if err := inspectStringWithExclusionRegex(&buf, tc.ProjectID, "Some email addresses: gary@example.com, bob@example.org", ".+@example.com"); err != nil {
+		t.Errorf("inspectStringWithExclusionRegex: %v", err)
+	}
+
+	got := buf.String()
+
+	if want := "Quote: bob@example.org"; !strings.Contains(got, want) {
+		t.Errorf("inspectStringWithExclusionRegex got %q, want %q", got, want)
+	}
+	if want := "Quote: gary@example.com"; strings.Contains(got, want) {
+		t.Errorf("inspectStringWithExclusionRegex got %q, want %q", got, want)
+	}
+}
+
 func TestInspectStringCustomExcludingSubstring(t *testing.T) {
 	tc := testutil.SystemTest(t)
 	var buf bytes.Buffer
@@ -310,6 +329,7 @@ func TestInspectStringMultipleRules(t *testing.T) {
 		t.Errorf("inspectStringMultipleRules got %q, want %q", got, want)
 	}
 }
+
 func TestInspectWithHotWordRules(t *testing.T) {
 	tc := testutil.SystemTest(t)
 	var buf bytes.Buffer
