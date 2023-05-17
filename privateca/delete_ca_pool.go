741c24f0c7fba8aea36c47c7c55b6c161ff3c137
rsamborski <rsamborski@gmail.com>
2023-05-17 13:35:09 +0200

    Fix linter (attempt 2)

--- privateca/delete_ca_pool.go
+++ privateca/delete_ca_pool.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package snippets
+package privateca
 
 // [START privateca_delete_ca_pool]
 import (
