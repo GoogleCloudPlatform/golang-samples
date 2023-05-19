c48d2e4a21cb7560dd06f783eb2cf9a086c8f3f4
aarshdhokai_crest <aarsh.dhokai@crestdatasys.com>
2023-05-19 16:18:30 +0530

    minor changes

--- dlp/snippets/inspect/inspect_test.go
+++ dlp/snippets/inspect/inspect_test.go
@@ -514,23 +514,6 @@ func TestInspectImageFile(t *testing.T) {
 	}
 }
 
-func TestInspectWithCustomRegex(t *testing.T) {
-	tc := testutil.SystemTest(t)
-
-	var buf bytes.Buffer
-	if err := inspectWithCustomRegex(&buf, tc.ProjectID, "Patients MRN 444-5-22222", "[1-9]{3}-[1-9]{1}-[1-9]{5}", "C_MRN"); err != nil {
-		t.Fatal(err)
-	}
-
-	got := buf.String()
-	if want := "Infotype Name: C_MRN"; !strings.Contains(got, want) {
-		t.Errorf("inspectWithCustomRegex got %q, want %q", got, want)
-	}
-	if want := "Likelihood: POSSIBLE"; !strings.Contains(got, want) {
-		t.Errorf("inspectWithCustomRegex got %q, want %q", got, want)
-	}
-}
-
 func TestInspectImageFileAllInfoTypes(t *testing.T) {
 	tc := testutil.SystemTest(t)
 	inputPath := "testdata/image.jpg"
