85ed580385b5447a07cec0d84ee231868ce02559
Yucen Tao <yucentao@google.com>
2023-05-22 14:03:46 -0700

    Add copyright header

--- logging/get_sink.go
+++ logging/get_sink.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package snippets contains examples of using the Cloud Logging API.
 package snippets
 
 // [START logging_get_sink]
