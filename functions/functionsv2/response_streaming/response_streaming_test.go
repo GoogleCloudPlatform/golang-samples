dd550ac83af4234b7689e07a8006177ae2f033d7
janell-chen <122311137+janell-chen@users.noreply.github.com>
2023-05-30 10:57:14 -0700

    Add new sample for response streaming in Golang

---
+++ functions/functionsv2/response_streaming/response_streaming_test.go
@@ -0,0 +1,51 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package responsestreaming
+
+import (
+	"fmt"
+	"net/http/httptest"
+	"testing"
+	"context"
+	"os"
+
+	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
+	"cloud.google.com/go/bigquery"
+)
+
+func TestResponseStreaming(t *testing.T) {
+	tc := testutil.SystemTest(t)
+
+	client, err := bigquery.NewClient(ctx, tc.ProjectID)
+	if err != nil {
+		t.Fatalf("bigquery.NewClient: %v", err)
+	}
+	ctx := context.Background()
+
+	rows, err := query(ctx, client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	if err := streamResults(w, rows); err != nil {
+		t.Fatal(err)
+	}
+
+	want := true
+	if got := strings.Contains(rr.Body.String(), "Successfully flushed row"); got != want {
+		t.Errorf("Response contains Successfully flushed row: %q, want %q", got, want)
+	}
+}
