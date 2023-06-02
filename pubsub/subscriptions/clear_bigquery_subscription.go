f083dbb4a10a98752a53aab95aab708913552e42
Alex Hong <9397363+hongalex@users.noreply.github.com>
2023-06-02 15:24:45 -0700

    add actual sample

---
+++ pubsub/subscriptions/clear_bigquery_subscription.go
@@ -0,0 +1,48 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package subscriptions
+
+// [START pubsub_clear_bigquery_subscription]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"cloud.google.com/go/pubsub"
+)
+
+// clearBigQuerySubscription reverts a BigQuery subscription back to a pull based subscription.
+func clearBigQuerySubscription(w io.Writer, projectID, subID string) error {
+	// projectID := "my-project-id"
+	// subID := "my-sub"
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("pubsub.NewClient: %w", err)
+	}
+	defer client.Close()
+
+	sub := client.Subscription(subID)
+	_, err = sub.Update(ctx, pubsub.SubscriptionConfigToUpdate{
+		BigQueryConfig: &pubsub.BigQueryConfig{},
+	})
+	if err != nil {
+		return fmt.Errorf("sub.Update: %w", err)
+	}
+
+	return nil
+}
+
+// [END pubsub_clear_bigquery_subscription]
