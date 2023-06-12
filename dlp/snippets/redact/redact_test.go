ece093b1d8db265879e1719a38535b8bcb9c403a
Aarsh Dhokai <105281389+AarshD-crest@users.noreply.github.com>
2023-06-12 17:58:47 +0530

    feat(dlp): added a sample for dlp redact image all text (#3149)
    * initial commit for dlp_redact_image_all_text sample with unit test case
    
    * refactor the code
    
    * sync the branch
    
    * sync the branch
    
    * fixing the syncing issues
    
    * reverting last unnecessary changes
    
    * syncing with gcp main
    
    * minor fix
    
    * refactor the code
    
    * refactor the code
    
    * refactor the code
    
    * updated the test case
    
    * updated the image
    
    ---------
    
    Co-authored-by: Eric Schmidt <erschmid@google.com>

--- dlp/snippets/redact/redact_test.go
+++ dlp/snippets/redact/redact_test.go
@@ -16,6 +16,11 @@ package redact
 
 import (
 	"bytes"
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"io"
+	"os"
 	"strings"
 	"testing"
 
@@ -61,4 +66,55 @@ func TestRedactImage(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestRedactImageFileAllText(t *testing.T) {
+	tc := testutil.SystemTest(t)
+	inputPath := "testdata/image.jpg"
+	outputPath := "testdata/test-output-sensitive-data-image-redacted.jpeg"
+
+	var buf bytes.Buffer
+	if err := redactImageFileAllText(&buf, tc.ProjectID, inputPath, outputPath); err != nil {
+		t.Fatal(err)
+	}
+
+	hash1, err := calculateImageHash(inputPath)
+	if err != nil {
+		t.Errorf("redactImageFileAllText: Error calculating hash for image 1: %q", err)
+	}
+
+	if _, err := os.Stat(outputPath); errors.Is(err, os.ErrNotExist) {
+		t.Error("redactImageFileAllText: the output file is not generated")
+	} else {
+		hash2, err := calculateImageHash(outputPath)
+		if err != nil {
+			t.Errorf("redactImageFileAllText: Error calculating hash for image 2: %q", err)
+		}
+
+		if hash1 == hash2 {
+			t.Error("redactImageFileAllText: image is not redacted.")
+		}
+	}
+
+	got := buf.String()
+	if want := "Wrote output to"; !strings.Contains(got, want) {
+		t.Errorf("redactImageFileAllText got %q, want %q", got, want)
+	}
+
+}
+
+func calculateImageHash(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", err
+	}
+
+	hashSum := hash.Sum(nil)
+	return hex.EncodeToString(hashSum), nil
 }
