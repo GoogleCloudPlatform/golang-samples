3675aadb8242d6546d9188cc7897a8cfd1b890a3
Aarsh Dhokai <105281389+AarshD-crest@users.noreply.github.com>
2023-06-12 22:06:55 +0530

    feat(dlp): added a sample for dlp redact image listed infotypes (#3150)
    * initial commit for dlp_redact_image_listed_infotypes sample with unit test case
    
    * sync the branch
    
    * sync the branch
    
    * fixing the syncing issues
    
    * reverting last unnecessary changes
    
    * syncing with gcp main
    
    * minor fix
    
    * refactor the sample
    
    * updated the test case

--- dlp/snippets/redact/redact_test.go
+++ dlp/snippets/redact/redact_test.go
@@ -68,6 +68,45 @@ func TestRedactImage(t *testing.T) {
 	}
 }
 
+func TestRedactImageFileListedInfoTypes(t *testing.T) {
+	tc := testutil.SystemTest(t)
+	inputPath := "testdata/image.jpg"
+	outputPath := "testdata/test-output-image-file-listed-infoTypes-redacted.jpeg"
+
+	var buf bytes.Buffer
+	if err := redactImageFileListedInfoTypes(&buf, tc.ProjectID, inputPath, outputPath); err != nil {
+		t.Fatal(err)
+	}
+
+	hash1, err := calculateImageHash(inputPath)
+	if err != nil {
+		t.Errorf("redactImageFileListedInfoTypes: Error calculating hash for image 1: %q", err)
+	}
+
+	if _, err := os.Stat(outputPath); errors.Is(err, os.ErrNotExist) {
+		t.Error("redactImageFileListedInfoTypes: the output file is not generated")
+	} else {
+		hash2, err := calculateImageHash(outputPath)
+		if err != nil {
+			t.Errorf("redactImageFileListedInfoTypes: Error calculating hash for image 2: %q", err)
+		}
+
+		if hash1 == hash2 {
+			t.Error("redactImageFileListedInfoTypes: image is not redacted.")
+		}
+	}
+
+	got := buf.String()
+	if want := "Wrote output to"; !strings.Contains(got, want) {
+		t.Errorf("redactImageFileListedInfoTypes got %q, want %q", got, want)
+	}
+
+	if want := "ioutil.ReadFile: open testdata/image.jpg: The system cannot find the path specified."; strings.Contains(got, want) {
+		t.Errorf("redactImageFileListedInfoTypes got %q, want %q", got, want)
+	}
+
+}
+
 func TestRedactImageFileAllText(t *testing.T) {
 	tc := testutil.SystemTest(t)
 	inputPath := "testdata/image.jpg"
