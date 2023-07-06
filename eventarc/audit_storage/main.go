864d025cdfd4593655221213a28d77d1e4aaa894
Adam Ross <adamross@google.com>
2023-07-06 15:39:41 -0700

    fix: explicit error handling of non-POST requests

--- eventarc/audit_storage/main.go
+++ eventarc/audit_storage/main.go
@@ -29,6 +29,11 @@ import (
 
 // HelloEventsStorage receives and processes a Cloud Audit Log event with Cloud Storage data.
 func HelloEventsStorage(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Expected HTTP POST request with CloudEvent payload", http.StatusBadRequest)
+		return
+	}
+
 	event, err := cloudevent.NewEventFromHTTPRequest(r)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
