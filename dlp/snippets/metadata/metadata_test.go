d073c95ac51f362ce191b47437d267bb6440da70
AarshD-crest <aarsh.dhokai@crestdatasys.com>
2023-07-21 15:41:50 +0530

    added sample of dlp_update_stored_infotype with test case

--- dlp/snippets/metadata/metadata_test.go
+++ dlp/snippets/metadata/metadata_test.go
@@ -19,16 +19,23 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 	"testing"
+	"time"
 
 	dlp "cloud.google.com/go/dlp/apiv2"
 	"cloud.google.com/go/dlp/apiv2/dlppb"
 	"cloud.google.com/go/storage"
 	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
 	"github.com/google/uuid"
+)
+
+const (
+	termListFileName = "term_list.txt"
+	filePathToUpload = "./testdata/term_list_storedInfotype.txt"
 )
 
 func TestInfoTypes(t *testing.T) {
@@ -130,7 +137,7 @@ func bucketForStoredInfoType(t *testing.T, projectID string) (string, error) {
 	if !bucketExists {
 		if err := client.Bucket(bucketName).Create(ctx, projectID, &storage.BucketAttrs{
 			StorageClass: "STANDARD",
-			Location:     "us-central1",
+			Location:     "us",
 		}); err != nil {
 			log.Fatalf("Failed to create bucket: %v", err)
 		}
@@ -181,4 +188,191 @@ func deleteStoredInfoTypeAfterTest(t *testing.T, name string) error {
 		return err
 	}
 	return nil
+}
+
+func TestUpdateStoredInfoType(t *testing.T) {
+	tc := testutil.SystemTest(t)
+
+	if err := bucketForUpdateStoredInfoType(t, tc.ProjectID); err != nil {
+		t.Fatal(err)
+	}
+
+	var buf bytes.Buffer
+
+	fileSetUrl := fmt.Sprint("gs://" + "dlp-go-lang-test" + "/" + termListFileName)
+	gcsUri := fmt.Sprint("gs://" + "dlp-go-lang-test")
+
+	outputPath, err := bucketForStoredInfoType(t, tc.ProjectID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	infoTypeId, err := createStoredInfoTypeForTesting(t, tc.ProjectID, outputPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	infoTypeId = strings.TrimPrefix(infoTypeId, "projects/bdp-2059-is-31084/locations/global/storedInfoTypes/")
+
+	duration := time.Duration(30) * time.Second
+	time.Sleep(duration)
+
+	if err := updateStoredInfoType(&buf, tc.ProjectID, gcsUri, fileSetUrl, infoTypeId); err != nil {
+		t.Fatal(err)
+	}
+
+	got := buf.String()
+
+	if want := "output: "; !strings.Contains(got, want) {
+		t.Errorf("error from create stored infoType %q", got)
+	}
+
+	name := strings.TrimPrefix(got, "output: ")
+
+	defer deleteStoredInfoTypeAfterTest(t, name)
+}
+
+func bucketForUpdateStoredInfoType(t *testing.T, projectID string) error {
+	t.Helper()
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+	u := uuid.New().String()[:8]
+	bucketName := "dlp-go-lang-test" + u
+	dirPath := "update-stored-infoType-data/"
+
+	// Check if the bucket already exists.
+	bucketExists := false
+	_, err = client.Bucket(bucketName).Attrs(ctx)
+	if err == nil {
+		bucketExists = true
+	}
+
+	// If the bucket doesn't exist, create it.
+	if !bucketExists {
+		if err := client.Bucket(bucketName).Create(ctx, projectID, &storage.BucketAttrs{
+			StorageClass: "STANDARD",
+			Location:     "us",
+		}); err != nil {
+			log.Fatalf("Failed to create bucket: %v", err)
+		}
+		fmt.Printf("Bucket '%s' created successfully.\n", bucketName)
+	} else {
+		fmt.Printf("Bucket '%s' already exists.\n", bucketName)
+	}
+
+	// Check if the directory already exists in the bucket.
+	dirExists := false
+	query := &storage.Query{Prefix: dirPath}
+	it := client.Bucket(bucketName).Objects(ctx, query)
+	_, err = it.Next()
+	if err == nil {
+		dirExists = true
+	}
+
+	// If the directory doesn't exist, create it.
+	if !dirExists {
+		obj := client.Bucket(bucketName).Object(dirPath)
+		if _, err := obj.NewWriter(ctx).Write([]byte("")); err != nil {
+			log.Fatalf("Failed to create directory: %v", err)
+		}
+		fmt.Printf("Directory '%s' created successfully in bucket '%s'.\n", dirPath, bucketName)
+	} else {
+		fmt.Printf("Directory '%s' already exists in bucket '%s'.\n", dirPath, bucketName)
+	}
+
+	// file upload code
+
+	// Open local file.
+	file, err := ioutil.ReadFile(filePathToUpload)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+
+	// Get a reference to the bucket
+	bucket := client.Bucket(bucketName)
+
+	// Upload the file
+	object := bucket.Object(termListFileName)
+	writer := object.NewWriter(ctx)
+	_, err = writer.Write(file)
+	if err != nil {
+		log.Fatalf("Failed to write file: %v", err)
+	}
+	err = writer.Close()
+	if err != nil {
+		log.Fatalf("Failed to close writer: %v", err)
+	}
+	fmt.Printf("File uploaded successfully: %v\n", termListFileName)
+
+	// Check if the file exists in the bucket
+	_, err = bucket.Object(termListFileName).Attrs(ctx)
+	if err != nil {
+		if err == storage.ErrObjectNotExist {
+			fmt.Printf("File %v does not exist in bucket %v\n", termListFileName, bucketName)
+		} else {
+			log.Fatalf("Failed to check file existence: %v", err)
+		}
+	} else {
+		fmt.Printf("File %v exists in bucket %v\n", termListFileName, bucketName)
+	}
+
+	return err
+}
+
+func createStoredInfoTypeForTesting(t *testing.T, projectID, outputPath string) (string, error) {
+	t.Helper()
+	ctx := context.Background()
+	client, err := dlp.NewClient(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer client.Close()
+
+	displayName := "Go Test - 4"
+	description := "Dictionary of GitHub usernames used in commits"
+
+	cloudStoragePath := &dlppb.CloudStoragePath{
+		Path: outputPath,
+	}
+
+	bigQueryField := &dlppb.BigQueryField{
+		Table: &dlppb.BigQueryTable{
+			ProjectId: "bigquery-public-data",
+			DatasetId: "samples",
+			TableId:   "github_nested",
+		},
+		Field: &dlppb.FieldId{
+			Name: "actor",
+		},
+	}
+
+	largeCustomDictionaryConfig := &dlppb.LargeCustomDictionaryConfig{
+		OutputPath: cloudStoragePath,
+		Source: &dlppb.LargeCustomDictionaryConfig_BigQueryField{
+			BigQueryField: bigQueryField,
+		},
+	}
+
+	storedInfoTypeConfig := &dlppb.StoredInfoTypeConfig{
+		DisplayName: displayName,
+		Description: description,
+		Type: &dlppb.StoredInfoTypeConfig_LargeCustomDictionary{
+			LargeCustomDictionary: largeCustomDictionaryConfig,
+		},
+	}
+	req := &dlppb.CreateStoredInfoTypeRequest{
+		Parent:           fmt.Sprintf("projects/%s/locations/global", projectID),
+		Config:           storedInfoTypeConfig,
+		StoredInfoTypeId: "go-sample-test-stored-infoType",
+	}
+	resp, err := client.CreateStoredInfoType(ctx, req)
+	if err != nil {
+		return "nil", err
+	}
+
+	return resp.Name, nil
 }
