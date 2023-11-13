e33592f8b2a4eed60996da28b07c7571cb864001
Marc Dougherty <muncus@users.noreply.github.com>
2023-11-13 14:15:02 -0800

    add some debugging for issues with go121 image.

--- testing/kokoro/system_tests.sh
+++ testing/kokoro/system_tests.sh
@@ -39,7 +39,16 @@ TIMEOUT=60m
 
 # Also see trampoline.sh - system_tests.sh is only run for PRs when there are
 # significant changes.
-SIGNIFICANT_CHANGES=$(git --no-pager diff --name-only main..HEAD | grep -Ev '(\.md$|^\.github)' || true)
+set +x
+SIGNIFICANT_CHANGES=$(git --no-pager diff --name-only main..HEAD | grep -Ev '(\.md$|^\.github)')
+if [ $? != 0 ]; then
+  echo "ERROR: diff detection failed"
+  git status
+  ls -la .
+  exit 1
+fi
+
+set -x
 # CHANGED_DIRS is the list of significant top-level directories that changed,
 # but weren't deleted by the current PR.
 # CHANGED_DIRS will be empty when run on main.
