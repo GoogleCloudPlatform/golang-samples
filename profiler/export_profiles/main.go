42005de531059b0e207cfe69d0a031b84ba890b0
Pranav Sharma <sharmapranav@google.com>
2023-12-15 00:02:05 +0000

    Add license header and  region tags

--- profiler/export_profiles/main.go
+++ profiler/export_profiles/main.go
@@ -1,3 +1,21 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START export_profiles]
+
+// Sample export_profiles shows how ListProfiles API can be used to download
+// existing pprof profiles for a given project from GCP.
 package main
 
 import (
@@ -107,3 +125,5 @@ func main() {
 	}
 	log.Println("Finished reading all profiles")
 }
+
+// [END export_profiles]
