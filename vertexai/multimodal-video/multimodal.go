30f42bcf3ed461ab13ed14b48e17a92d5b184286
Valentin Deleplace <deleplace2015@gmail.com>
2024-02-12 18:56:18 +0100

    feat(vertexai): add a multimodal sample with a video file
    Same code as the multimodal sample, but with the URL of a video.

---
+++ vertexai/multimodal-video/multimodal.go
@@ -0,0 +1,81 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// multimodal-video shows an example of understanding multimodal input involving video files
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"mime"
+	"os"
+	"path/filepath"
+
+	"cloud.google.com/go/vertexai/genai"
+)
+
+func main() {
+	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	location := "us-central1"
+	modelName := "gemini-pro-vision"
+
+	prompt := "What is in this video?"
+
+	video := "gs://cloud-samples-data/video/animals.mp4"
+
+	if projectID == "" {
+		log.Fatal("require environment variable GOOGLE_CLOUD_PROJECT")
+	}
+
+	err := generateMultimodalContent(os.Stdout, prompt, video, projectID, location, modelName)
+	if err != nil {
+		log.Fatalf("unable to generate: %v", err)
+	}
+}
+
+// generateMultimodalContent generates a response into w, based upon the prompt
+// and video provided.
+// video is a Google Cloud Storage path starting with "gs://"
+func generateMultimodalContent(w io.Writer, prompt, video, projectID, location, modelName string) error {
+	ctx := context.Background()
+
+	client, err := genai.NewClient(ctx, projectID, location)
+	if err != nil {
+		return fmt.Errorf("unable to create client: %v", err)
+	}
+	defer client.Close()
+
+	model := client.GenerativeModel(modelName)
+	model.SetTemperature(0.4)
+
+	// Given a video file URL, prepare video file as genai.Part
+	img := genai.FileData{
+		MIMEType: mime.TypeByExtension(filepath.Ext(video)),
+		FileURI:  video,
+	}
+
+	res, err := model.GenerateContent(ctx, img, genai.Text(prompt))
+	if err != nil {
+		return fmt.Errorf("unable to generate contents: %v", err)
+	}
+
+	if len(res.Candidates) == 0 || len(res.Candidates[0].Content.Parts) == 0 {
+		return fmt.Errorf("empty response from model")
+	}
+
+	fmt.Fprintf(w, "generated response: %s\n", res.Candidates[0].Content.Parts[0])
+	return nil
+}
