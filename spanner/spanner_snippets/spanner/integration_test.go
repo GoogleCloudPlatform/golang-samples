ae40f4d38fe07e016f2cd4590b13b07e6119df18
Knut Olav Løite <koloite@gmail.com>
2024-03-14 18:16:18 +0100

    docs(samples): add Spanner statement timeout sample (#3809)
    Adds a sample for setting a timeout for a single statement for Spanner.

--- spanner/spanner_snippets/spanner/integration_test.go
+++ spanner/spanner_snippets/spanner/integration_test.go
@@ -273,6 +273,9 @@ func TestSample(t *testing.T) {
 	out = runSample(t, setCustomTimeoutAndRetry, dbName, "failed to insert using DML with custom timeout and retry")
 	assertContains(t, out, "record(s) inserted")
 
+	out = runSample(t, setStatementTimeout, dbName, "failed to execute statement with a timeout")
+	assertContains(t, out, "record(s) inserted")
+
 	out = runSample(t, transactionTimeout, dbName, "failed to run transaction with a timeout")
 	assertContains(t, out, "Transaction with timeout was executed successfully")
 
