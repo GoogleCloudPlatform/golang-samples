e55a129880d2b40dc634eddf7802121c05932468
Valentin Deleplace <deleplace2015@gmail.com>
2024-05-03 12:43:36 +0200

    feat(vertexai): system instruction

---
+++ vertexai/system-instruction/go.mod
@@ -0,0 +1,47 @@
+module github.com/GoogleCloudPlatform/golang-samples/vertexai/system-instruction
+
+go 1.21
+
+require (
+	cloud.google.com/go/vertexai v0.7.2-0.20240430000329-84e3236355de
+	github.com/GoogleCloudPlatform/golang-samples v0.0.0-20240215053133-28d24e441ba2
+)
+
+require (
+	cloud.google.com/go v0.112.2 // indirect
+	cloud.google.com/go/aiplatform v1.67.0 // indirect
+	cloud.google.com/go/auth v0.3.0 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.2 // indirect
+	cloud.google.com/go/compute/metadata v0.3.0 // indirect
+	cloud.google.com/go/iam v1.1.7 // indirect
+	cloud.google.com/go/longrunning v0.5.6 // indirect
+	cloud.google.com/go/storage v1.39.1 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.3 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.49.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
+	go.opentelemetry.io/otel v1.24.0 // indirect
+	go.opentelemetry.io/otel/metric v1.24.0 // indirect
+	go.opentelemetry.io/otel/trace v1.24.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/oauth2 v0.19.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/api v0.176.1 // indirect
+	google.golang.org/genproto v0.0.0-20240401170217-c3f982113cda // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240415180920-8c6c420018be // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240415180920-8c6c420018be // indirect
+	google.golang.org/grpc v1.63.2 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
---
+++ vertexai/system-instruction/systeminstruction.go
@@ -0,0 +1,70 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// systeminstruction shows an example of providing the model with additional context to understand the task
+// and get customized responses.
+// For developers, product-level behavior can be specified in system instructions, separate from prompts
+// provided by end users.
+package systeminstruction
+
+// [START generativeaionvertexai_gemini_system_instruction]
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+
+	"cloud.google.com/go/vertexai/genai"
+)
+
+// systemInstruction shows how to provide a system instruction to the generative model.
+func systemInstruction(w io.Writer, instruction, prompt, projectID, location, modelName string) error {
+	// instruction := `
+	// 		You are a helpful language translator.
+	// 		Your mission is to translate text in English to French.`
+	// prompt := `
+	//		User input: I like bagels.
+	//		Answer:`
+	// location := "us-central1"
+	// modelName := "gemini-1.0-pro"
+
+	ctx := context.Background()
+
+	client, err := genai.NewClient(ctx, projectID, location)
+	if err != nil {
+		return fmt.Errorf("unable to create client: %v", err)
+	}
+	defer client.Close()
+
+	// The System Instruction is set at model creation
+	model := client.GenerativeModel(modelName)
+	model.SystemInstruction = &genai.Content{
+		Parts: []genai.Part{genai.Text(instruction)},
+	}
+
+	chat := model.StartChat()
+	res, err := chat.SendMessage(ctx, genai.Text(prompt))
+	if err != nil {
+		return err
+	}
+	if len(res.Candidates) == 0 ||
+		len(res.Candidates[0].Content.Parts) == 0 {
+		return errors.New("empty response from model")
+	}
+	fmt.Fprintf(w, "generated response: %s\n", res.Candidates[0].Content.Parts[0])
+
+	return nil
+}
+
+// [END generativeaionvertexai_gemini_system_instruction]
