d2437938ef2e8d7de4206be372a0a4503563c890
Nick Cook <nickcook@google.com>
2024-05-08 19:11:15 -0700

    update review text

--- vertexai/snippets/text_input.go
+++ vertexai/snippets/text_input.go
@@ -11,9 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// laishers. "A New Era in Cartoon History." Review of Steamboat Willie.
-// 4 January 2001. https://www.imdb.com/review/rw0005574/?ref_=rw_urv
 package snippets
 
 // [START generativeaionvertexai_gemini_generate_from_text_input]
@@ -36,20 +33,18 @@ func textInput(w io.Writer, projectID string, location string, modelName string)
 		return fmt.Errorf("error creating client: %w", err)
 	}
 	gemini := client.GenerativeModel(modelName)
-	// Movie review from https://www.imdb.com/review/rw0005574/?ref_=rw_urv.
 	// Does the returned sentiment score match the reviewer's movie rating?
 	prompt := genai.Text(`Give a score from 1 - 10 to suggest if the
             following movie review is negative or positive (1 is most
             negative, 10 is most positive, 5 will be neutral). Include an
             explanation.
 
-            This era was not just the dawn of sound in cartoons, but of a
-            cartoon character which would go down in history as the world's
-            most famous mouse. Yes, Mickey makes his debut here, in this
-            cheery tale of life on board a steamboat. The animation is good
-            for it's time, and the plot - though a little simple - is quite
-            jolly. A true classic, and if you ever manage to get it on
-            video, you won't regret it.`)
+            The movie takes some time to build, but that is part of its beauty.
+            By the time you are hooked, this tale of friendship and hope is
+            thrilling and affecting, until the very last scene. You will find
+            yourself rooting for the hero every step of the way. This is the
+            sharpest, most original animated film I have seen in years.
+            I would give it 8 out of 10 stars.`)
 
 	resp, err := gemini.GenerateContent(ctx, prompt)
 	if err != nil {
