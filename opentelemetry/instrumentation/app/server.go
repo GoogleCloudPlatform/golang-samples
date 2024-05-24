6828417886cfb5912c680ab803a97d30f41020f7
Pranav Sharma <sharmapranav@google.com>
2024-05-24 20:14:55 +0000

    Revert the change to log message in /multi

--- opentelemetry/instrumentation/app/server.go
+++ opentelemetry/instrumentation/app/server.go
@@ -40,7 +40,7 @@ func handleMulti(w http.ResponseWriter, r *http.Request) {
 	subRequests := 3 + rand.Intn(4)
 	// Write a structured log with the request context, which allows the log to
 	// be linked with the trace for this request.
-	slog.InfoContext(r.Context(), "computing multiple requests", slog.Int("subRequests", subRequests))
+	slog.InfoContext(r.Context(), "handle /multi request", slog.Int("subRequests", subRequests))
 
 	err := computeSubrequests(r, subRequests)
 	if err != nil {
