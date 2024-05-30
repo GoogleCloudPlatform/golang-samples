256f3a182aefc52172868da572caf1230c5601e1
Valentin Deleplace <deleplace2015@gmail.com>
2024-05-30 17:59:59 +0200

    feat(vertexai): Add token count multimodal

---
+++ vertexai/token-count/tokencount_multimodal.go
@@ -0,0 +1,78 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// token-count shows an example of determining how many tokens correspond to
+// a given prompt string
+package tokencount
+
+// [START generativeaionvertexai_gemini_token_count_multimodal]
+import (
+	"context"
+	"fmt"
+	"io"
+	"mime"
+	"path/filepath"
+
+	"cloud.google.com/go/vertexai/genai"
+)
+
+// countTokensMultimodal returns the number of tokens for this prompt.
+//
+// video is a Google Cloud Storage path starting with "gs://"
+func countTokensMultimodal(w io.Writer, prompt, video, projectID, location, modelName string) error {
+	// prompt := "Provide a description of the video."
+	// video := "gs://cloud-samples-data/generative-ai/video/pixel8.mp4"
+	// location := "us-central1"
+	// modelName := "gemini-1.5-flash-001"
+
+	ctx := context.Background()
+
+	client, err := genai.NewClient(ctx, projectID, location)
+	if err != nil {
+		return fmt.Errorf("unable to create client: %w", err)
+	}
+	defer client.Close()
+
+	model := client.GenerativeModel(modelName)
+
+	part1 := genai.Text(prompt)
+
+	// Given a video file URL, prepare video file as genai.Part
+	part2 := genai.FileData{
+		MIMEType: mime.TypeByExtension(filepath.Ext(video)),
+		FileURI:  video,
+	}
+
+	resp, err := model.CountTokens(ctx, part1, part2)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(w, "Number of tokens for the multimodal video prompt: %d\n", resp.TotalTokens)
+
+	res, err := model.GenerateContent(ctx, part1, part2)
+	if err != nil {
+		return fmt.Errorf("unable to generate contents: %w", err)
+	}
+
+	fmt.Fprintln(w, "\nModel response")
+	md := res.UsageMetadata
+	fmt.Fprintf(w, "Prompt Token Count: %d\n", md.PromptTokenCount)
+	fmt.Fprintf(w, "Candidates Token Count: %d\n", md.CandidatesTokenCount)
+	fmt.Fprintf(w, "Total Token Count: %d\n", md.TotalTokenCount)
+
+	return nil
+}
+
+// [END generativeaionvertexai_gemini_token_count_multimodal]
--- vertexai/token-count/tokencount_test.go
+++ vertexai/token-count/tokencount_test.go
@@ -53,3 +53,48 @@ func Test_countTokens(t *testing.T) {
 		t.Errorf("Expected less than 20 tokens, got %d", n)
 	}
 }
+
+func Test_countTokensMultimodal(t *testing.T) {
+	tc := testutil.SystemTest(t)
+
+	prompt := "Provide a description of the video."
+	video := "gs://cloud-samples-data/generative-ai/video/pixel8.mp4"
+	location := "us-central1"
+	modelName := "gemini-1.5-flash-001"
+
+	var buf bytes.Buffer
+	err := countTokensMultimodal(&buf, prompt, video, tc.ProjectID, location, modelName)
+	if err != nil {
+		t.Fatalf("Test_countTokensMultimodal: %v", err.Error())
+	}
+
+	answer := buf.String()
+
+	for _, expected := range []string{
+		"Number of tokens for the multimodal video prompt: ",
+		"Prompt Token Count:",
+		"Candidates Token Count:",
+		"Total Token Count:",
+	} {
+		if !strings.Contains(answer, expected) {
+			t.Errorf("Response does not contain %q", expected)
+		}
+	}
+
+	s := strings.TrimPrefix(answer, "Number of tokens for the multimodal video prompt: ")
+	s, _, _ = strings.Cut(s, "\n")
+	s = strings.TrimSpace(s)
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		t.Fatalf("Test_countTokensMultimodal: %v", err.Error())
+	}
+
+	// The pixel8.mp4 video prompt is expected to account for about 17,000 tokens
+	// Extremely low or high values would not be correct
+	if n <= 100 {
+		t.Errorf("Expected more than 100 tokens, got %d", n)
+	}
+	if n >= 1_000_000 {
+		t.Errorf("Expected less than 1,000,000 tokens, got %d", n)
+	}
+}
