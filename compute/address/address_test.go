a1a366859647454a6b324a1232f1369ee1038887
Iurii Rudyk <irudyk@softserveinc.com>
2024-06-18 10:39:38 +0300

    feat(compute): compute_ip_address_unassign_static_address sample

--- compute/address/address_test.go
+++ compute/address/address_test.go
@@ -628,3 +628,114 @@ func TestAssignStaticAddressToExistingVM(t *testing.T) {
 	}
 	t.Error("IP address did not assigned properly") // address assign not verified
 }
+
+func TestUnassignStaticAddressFromExistingVM(t *testing.T) {
+	ctx := context.Background()
+	var seededRand = rand.New(
+		rand.NewSource(time.Now().UnixNano()))
+	tc := testutil.SystemTest(t)
+	instanceName := "test-instance-" + fmt.Sprint(seededRand.Int())
+	addressName := "test-address-" + fmt.Sprint(seededRand.Int())
+	zone := "us-central1-a"
+	region := "us-central1"
+	buf := &bytes.Buffer{}
+
+	// initiate instance
+	err := createTestInstance(tc.ProjectID, zone, instanceName)
+	if err != nil {
+		t.Errorf("createTestInstance got err: %v", err)
+		return
+	}
+
+	defer func() {
+		if err := deleteInstance(tc.ProjectID, zone, instanceName); err != nil {
+			t.Errorf("deleteInstance got err: %v", err)
+		}
+
+	}()
+
+	// create and retrieve address
+	address, err := reserveNewRegionalExternal(buf, tc.ProjectID, region, addressName, true)
+	if err != nil {
+		t.Errorf("reserveNewRegionalExternal got err: %v", err)
+		return
+	}
+
+	defer func() {
+
+		if err := deleteIPAddress(ctx, tc.ProjectID, region, addressName); err != nil {
+			t.Errorf("deleteIPAddress got err: %v", err)
+		}
+	}()
+
+	// assign address to test
+	if err := assignStaticAddressToExistingVM(buf, tc.ProjectID, zone, instanceName, address.GetAddress(), "nic0"); err != nil {
+		t.Errorf("assignStaticAddressToExistingVM got err: %v", err)
+	}
+
+	instancesClient, err := compute.NewInstancesRESTClient(ctx)
+	reqGet := &computepb.GetInstanceRequest{
+		Project:  tc.ProjectID,
+		Zone:     zone,
+		Instance: instanceName,
+	}
+
+	// verify address assign
+	instance, err := instancesClient.Get(ctx, reqGet)
+	if err != nil {
+		t.Errorf("instancesClient.Get got err: %v", err)
+	}
+
+	found := false
+	for _, ni := range instance.NetworkInterfaces {
+		if found {
+			break
+		}
+		if *ni.Name != "nic0" {
+			continue
+		}
+		for _, ac := range ni.AccessConfigs {
+			if ac.NatIP != nil && *ac.NatIP == address.GetAddress() {
+
+				found = true
+				break
+			}
+		}
+
+	}
+	if !found {
+		t.Errorf("address %v not found in the list of assigned addresses", addressName)
+	}
+
+	// unassign address
+	if err := unassignStaticAddressFromExistingVM(buf, tc.ProjectID, zone, instanceName, "nic0"); err != nil {
+		t.Errorf("unassignStaticAddressFromExistingVM got err: %v", err)
+	}
+
+	// verify output
+	expectedResult := fmt.Sprintf("Static address %s unassigned from the instance %s", address.GetAddress(), instanceName)
+	if got := buf.String(); !strings.Contains(got, expectedResult) {
+		t.Errorf("unassignStaticAddressFromExistingVM got %q, want %q", got, expectedResult)
+	}
+
+	// verify address unassign
+	instance, err = instancesClient.Get(ctx, reqGet)
+	if err != nil {
+		t.Errorf("instancesClient.Get got err: %v", err)
+	}
+
+	for _, ni := range instance.NetworkInterfaces {
+
+		if *ni.Name != "nic0" {
+			continue
+		}
+		for _, ac := range ni.AccessConfigs {
+			if ac.NatIP != nil && *ac.NatIP == address.GetAddress() {
+				t.Errorf("address %v still found in the list of assigned addresses", addressName)
+				return
+			}
+		}
+
+	}
+
+}
