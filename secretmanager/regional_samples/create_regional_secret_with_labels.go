ed7fa925878b7c4b6bc47d546fbd116d9bd9e19d
Vipul Mittal <vipulmittal@google.com>
2024-10-09 19:02:53 +0000

    feat:add samples for labels, regional SM

---
+++ secretmanager/regional_samples/create_regional_secret_with_labels.go
@@ -0,0 +1,68 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package regional_secretmanager
+
+// [START secretmanager_create_regional_secret_with_labels]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+	"google.golang.org/api/option"
+)
+
+// createSecretWithLabels creates a new secret with the given name and labels.
+func CreateRegionalSecretWithLabels(w io.Writer, projectId, locationId, id string) error {
+	// parent := "projects/my-project/locations/my-location"
+	// id := "my-secret"
+
+	labelKey := "labelkey"
+	labelValue := "labelvalue"
+
+	// Create the client.
+	ctx := context.Background()
+	//Endpoint to send the request to regional server
+	endpoint := fmt.Sprintf("secretmanager.%s.rep.googleapis.com:443", locationId)
+	client, err := secretmanager.NewClient(ctx, option.WithEndpoint(endpoint))
+	if err != nil {
+		return fmt.Errorf("failed to create secretmanager client: %w", err)
+	}
+	defer client.Close()
+
+	parent := fmt.Sprintf("projects/%s/locations/%s", projectId, locationId)
+
+	// Build the request.
+	req := &secretmanagerpb.CreateSecretRequest{
+		Parent:   parent,
+		SecretId: id,
+		Secret: &secretmanagerpb.Secret{
+			Labels: map[string]string{
+				labelKey: labelValue,
+			},
+		},
+	}
+
+	// Call the API.
+	result, err := client.CreateSecret(ctx, req)
+	if err != nil {
+		return fmt.Errorf("failed to create secret: %w", err)
+	}
+	fmt.Fprintf(w, "Created secret with labels: %s\n", result.Name)
+	return nil
+}
+
+// [END secretmanager_create_regional_secret_with_labels]
---
+++ secretmanager/regional_samples/create_update_regional_secret_label.go
@@ -0,0 +1,84 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package regional_secretmanager
+
+// [START secretmanager_create_update_regional_secret_label]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+	"google.golang.org/api/option"
+	"google.golang.org/genproto/protobuf/field_mask"
+)
+
+// createUpdateSecretLabel updates the labels about an existing secret.
+// If the label key exists, it updates the label, otherwise it creates a new one.
+func CreateUpdateRegionalSecretLabel(w io.Writer, projectId, locationId, id string) error {
+	// name := "projects/my-project/locations/my-location/secrets/my-secret"
+
+	labelKey := "labelkey"
+	labelValue := "updatedlabelvalue"
+
+	// Create the client.
+	ctx := context.Background()
+	//Endpoint to send the request to regional server
+	endpoint := fmt.Sprintf("secretmanager.%s.rep.googleapis.com:443", locationId)
+	client, err := secretmanager.NewClient(ctx, option.WithEndpoint(endpoint))
+	if err != nil {
+		return fmt.Errorf("failed to create secretmanager client: %w", err)
+	}
+	defer client.Close()
+
+	name := fmt.Sprintf("projects/%s/locations/%s/secrets/%s", projectId, locationId, id)
+
+	// Build the request to get the secret.
+	req := &secretmanagerpb.GetSecretRequest{
+		Name: name,
+	}
+
+	// Call the API.
+	result, err := client.GetSecret(ctx, req)
+	if err != nil {
+		return fmt.Errorf("failed to get secret: %w", err)
+	}
+
+	labels := result.Labels
+
+	labels[labelKey] = labelValue
+
+	// Build the request to update the secret.
+	update_req := &secretmanagerpb.UpdateSecretRequest{
+		Secret: &secretmanagerpb.Secret{
+			Name:   name,
+			Labels: labels,
+		},
+		UpdateMask: &field_mask.FieldMask{
+			Paths: []string{"labels"},
+		},
+	}
+
+	// Call the API.
+	update_result, err := client.UpdateSecret(ctx, update_req)
+	if err != nil {
+		return fmt.Errorf("failed to update secret: %w", err)
+	}
+	fmt.Fprintf(w, "Updated secret: %s\n", update_result.Name)
+	return nil
+}
+
+// [END secretmanager_create_update_regional_secret_label]
