5c63f6f6413df08a72b12629b306283477e1a8c5
Eric Schmidt <erschmid@google.com>
2024-11-12 09:29:44 -0800

    Update client_with_database.go

--- datastore/tasks/client_with_database.go
+++ datastore/tasks/client_with_database.go
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// [START datastore_create_client_with_db]
 package main
 
+// [START datastore_create_client_with_db]
 import (
 	"context"
 	"fmt"
