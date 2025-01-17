bd794d09122903f00a202debebcbdfe9faf87768
Orlando Emiliano Garcia Diaz <garciadiaz@google.com>
2025-01-17 21:18:14 +0000

    fix(gae): migrate region deferred_tasks

--- docs/appengine/taskqueue/push/defer.go
+++ docs/appengine/taskqueue/push/defer.go
@@ -24,13 +24,11 @@ func example() {
 	ctx := context.Background()
 
 	// [START gae_deferred_tasks]
-	// [START deferred_tasks]
 	var expensiveFunc = delay.Func("some-arbitrary-key", func(ctx context.Context, a string, b int) {
 		// Do something expensive!
 	})
 
 	// Somewhere else.
 	expensiveFunc.Call(ctx, "Hello, world!", 42)
-	// [END deferred_tasks]
 	// [START gae_deferred_tasks]
 }
