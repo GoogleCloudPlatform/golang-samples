c666c95b0e2ce6c306bab33a32727e2d0b20e405
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2025-02-05 12:49:37 +0100

    feat: setup for genai dir

---
+++ genai/text_generation/textgen_with_txt.go
@@ -0,0 +1,59 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package text_generation shows examples of generating text using the GenAI SDK.
+package text_generation
+
+// [START googlegenaisdk_textgen_with_txt]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"google.golang.org/genai"
+)
+
+// generateWithText shows how to generate text using a text prompt.
+func generateWithText(w io.Writer) error {
+	ctx := context.Background()
+
+	client, err := genai.NewClient(ctx, &genai.ClientConfig{})
+	if err != nil {
+		return fmt.Errorf("unable to create genai client: %w", err)
+	}
+
+	resp, err := client.Models.GenerateContent(ctx,
+		"gemini-2.0-flash-001",
+		genai.Text("How does AI work?"),
+		nil,
+	)
+	if err != nil {
+		return fmt.Errorf("unable to generate content: %w", err)
+	}
+
+	respText, err := resp.Text()
+	if err != nil {
+		return fmt.Errorf("unable to convert model response to text: %w", err)
+	}
+	fmt.Fprintln(w, respText)
+	// Example response:
+	// That's a great question! Understanding how AI works can feel like ...
+	// ...
+	// **1. The Foundation: Data and Algorithms**
+	// ...
+
+	return nil
+}
+
+// [END googlegenaisdk_textgen_with_txt]
