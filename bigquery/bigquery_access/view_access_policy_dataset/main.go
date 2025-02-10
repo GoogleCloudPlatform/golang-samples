55b32c4862f683d092ff03c0d631c9aa8824f948
Orlando Emiliano Garcia Diaz <garciadiaz@google.com>
2025-02-10 20:06:14 +0000

    feature(bigquery): add feature view access policy for dataset

--- bigquery/bigquery_access/view_access_policy_dataset/main.go
+++ bigquery/bigquery_access/view_access_policy_dataset/main.go
@@ -12,24 +12,29 @@ import (
 func main() {
 	ctx := context.Background()
 
+	// Sets your Google Cloud Platform project ID.
 	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
 
-	// Sets the name for the new dataset.
-	datasetName := "my_new_dataset"
-
+	// Creates a client.
 	client, err := bigquery.NewClient(ctx, projectID)
 	if err != nil {
 		log.Fatalf("bigquery.NewClient: %v", err)
 	}
 	defer client.Close()
 
+	// Sets the name for the dataset which access list will be asked.
+	datasetName := "my_new_dataset"
+
+	// Creates handle for managing dataset
 	dataset := client.Dataset(datasetName)
 
+	// Gets dataset's metadata
 	metaData, err := dataset.Metadata(ctx)
 	if err != nil {
 		log.Fatalf("dataset.Metadata: %v", err)
 	}
 
+	// Iterate over access permissions
 	for _, val := range metaData.Access {
 		fmt.Printf("Role %s : %s\n", val.Role, val.Entity)
 	}
