64d8a1518df3195b228140d219417c511128ffd5
Orlando Emiliano Garcia Diaz <garciadiaz@google.com>
2025-02-11 20:28:01 +0000

    feature(bigquery): add feature view access policy for dataset

--- bigquery/bigquery_access/view_dataset_access_policy/view_dataset_access_policy.go
+++ bigquery/bigquery_access/view_dataset_access_policy/view_dataset_access_policy.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Command simpleapp queries the Stack Overflow public dataset in Google BigQuery.
-
 package viewdatasetaccesspolicy
 
 // [START bigquery_view_dataset_access_policies]
