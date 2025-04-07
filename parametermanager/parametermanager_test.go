8389123334c386b7dd7af1a80cf34a50b2f531c8
durgesh-ninave-crest <durgesh.ninave@crestdata.ai>
2025-04-07 17:53:59 +0530

    feat(parametermanager): Add samples for get, list and render parameter version

---
+++ parametermanager/parametermanager_test.go
@@ -0,0 +1,327 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package parametermanager
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+
+	parametermanager "cloud.google.com/go/parametermanager/apiv1"
+	parametermanagerpb "cloud.google.com/go/parametermanager/apiv1/parametermanagerpb"
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	secretmanagerpb "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
+	"github.com/gofrs/uuid"
+	grpccodes "google.golang.org/grpc/codes"
+	grpcstatus "google.golang.org/grpc/status"
+)
+
+// testName generates a unique name for testing purposes by creating a new UUID.
+// It returns the UUID as a string or fails the test if UUID generation fails.
+func testName(t *testing.T) string {
+	t.Helper()
+
+	u, err := uuid.NewV4()
+	if err != nil {
+		t.Fatalf("testName: failed to generate uuid: %v", err)
+	}
+	return u.String()
+}
+
+// testParameter creates a parameter in the specified GCP project with the given format.
+// It returns the created parameter and its ID or fails the test if parameter creation fails.
+func testParameter(t *testing.T, projectID string, format parametermanagerpb.ParameterFormat) (*parametermanagerpb.Parameter, string) {
+	t.Helper()
+	parameterID := testName(t)
+
+	ctx := context.Background()
+	client, err := parametermanager.NewClient(ctx)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	parent := fmt.Sprintf("projects/%s/locations/global", projectID)
+	parameter, err := client.CreateParameter(ctx, &parametermanagerpb.CreateParameterRequest{
+		Parent:      parent,
+		ParameterId: parameterID,
+		Parameter: &parametermanagerpb.Parameter{
+			Format: format,
+		},
+	})
+	if err != nil {
+		t.Fatalf("testParameter: failed to create parameter: %v", err)
+	}
+
+	return parameter, parameterID
+}
+
+// testParameterVersion creates a version of a parameter with the given payload in the specified GCP project.
+// It returns the created parameter version and its ID or fails the test if parameter version creation fails.
+func testParameterVersion(t *testing.T, projectID, parameterID, payload string) (*parametermanagerpb.ParameterVersion, string) {
+	t.Helper()
+	parameterVersionID := testName(t)
+
+	ctx := context.Background()
+	client, err := parametermanager.NewClient(ctx)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+	defer client.Close()
+	parent := fmt.Sprintf("projects/%s/locations/global/parameters/%s", projectID, parameterID)
+
+	parameterVersion, err := client.CreateParameterVersion(ctx, &parametermanagerpb.CreateParameterVersionRequest{
+		Parent:             parent,
+		ParameterVersionId: parameterVersionID,
+		ParameterVersion: &parametermanagerpb.ParameterVersion{
+			Payload: &parametermanagerpb.ParameterVersionPayload{
+				Data: []byte(payload),
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("testParameterVersion: failed to create parameter version: %v", err)
+	}
+
+	return parameterVersion, parameterVersionID
+}
+
+// testCleanupParameter deletes the specified parameter in the GCP project.
+// It fails the test if the parameter deletion fails.
+func testCleanupParameter(t *testing.T, name string) {
+	t.Helper()
+
+	ctx := context.Background()
+	client, err := parametermanager.NewClient(ctx)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	if err := client.DeleteParameter(ctx, &parametermanagerpb.DeleteParameterRequest{
+		Name: name,
+	}); err != nil {
+		if terr, ok := grpcstatus.FromError(err); !ok || terr.Code() != grpccodes.NotFound {
+			t.Fatalf("testCleanupParameter: failed to delete parameter: %v", err)
+		}
+	}
+}
+
+// testCleanupParameterVersion deletes the specified parameter version in the GCP project.
+// It fails the test if the parameter version deletion fails.
+func testCleanupParameterVersion(t *testing.T, name string) {
+	t.Helper()
+
+	ctx := context.Background()
+	client, err := parametermanager.NewClient(ctx)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	if err := client.DeleteParameterVersion(ctx, &parametermanagerpb.DeleteParameterVersionRequest{
+		Name: name,
+	}); err != nil {
+		if terr, ok := grpcstatus.FromError(err); !ok || terr.Code() != grpccodes.NotFound {
+			t.Fatalf("testCleanupParameterVersion: failed to delete parameter version: %v", err)
+		}
+	}
+}
+
+// testSecret creates a secret in the specified GCP project.
+// It returns the created secret or fails the test if secret creation fails.
+func testSecret(t *testing.T, projectID string) *secretmanagerpb.Secret {
+	t.Helper()
+	secretID := testName(t)
+
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	secret, err := client.CreateSecret(ctx, &secretmanagerpb.CreateSecretRequest{
+		Parent:   fmt.Sprintf("projects/%s", projectID),
+		SecretId: secretID,
+		Secret: &secretmanagerpb.Secret{
+			Replication: &secretmanagerpb.Replication{
+				Replication: &secretmanagerpb.Replication_Automatic_{
+					Automatic: &secretmanagerpb.Replication_Automatic{},
+				},
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("testSecret: failed to create secret: %v", err)
+	}
+
+	return secret
+}
+
+// testSecretVersion creates a version of a secret with the given payload in the specified GCP project.
+// It returns the created secret version or fails the test if secret version creation fails.
+func testSecretVersion(t *testing.T, parent string, payload []byte) *secretmanagerpb.SecretVersion {
+	t.Helper()
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	version, err := client.AddSecretVersion(ctx, &secretmanagerpb.AddSecretVersionRequest{
+		Parent: parent,
+		Payload: &secretmanagerpb.SecretPayload{
+			Data: payload,
+		},
+	})
+	if err != nil {
+		t.Fatalf("testSecretVersion: failed to create secret version: %v", err)
+	}
+	return version
+}
+
+// testIamGrantAccess grants the specified member access permissions to the secret.
+func testIamGrantAccess(t *testing.T, name, member string) error {
+	t.Helper()
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	handle := client.IAM(name)
+	policy, err := handle.Policy(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get policy: %w", err)
+	}
+
+	// Grant the member access permissions.
+	policy.Add(member, "roles/secretmanager.secretAccessor")
+	if err = handle.SetPolicy(ctx, policy); err != nil {
+		return fmt.Errorf("failed to save policy: %w", err)
+	}
+
+	return nil
+}
+
+// testCleanupSecret deletes the specified secret in the GCP project.
+// It fails the test if the secret deletion fails.
+func testCleanupSecret(t *testing.T, name string) {
+	t.Helper()
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		t.Fatalf("failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	if err := client.DeleteSecret(ctx, &secretmanagerpb.DeleteSecretRequest{
+		Name: name,
+	}); err != nil {
+		if terr, ok := grpcstatus.FromError(err); !ok || terr.Code() != grpccodes.NotFound {
+			t.Fatalf("testCleanupSecret: failed to delete secret: %v", err)
+		}
+	}
+}
+
+// TestGetParamVersion tests the getParamVersion function by creating a parameter and its version,
+// then attempts to retrieve the created parameter version. It verifies if the parameter version
+// was successfully retrieved by checking the output.
+func TestGetParamVersion(t *testing.T) {
+	tc := testutil.SystemTest(t)
+
+	parameter, parameterID := testParameter(t, tc.ProjectID, parametermanagerpb.ParameterFormat_JSON)
+	payload := `{"username": "test-user", "host": "localhost"}`
+	parameterVersion, parameterVersionID := testParameterVersion(t, tc.ProjectID, parameterID, payload)
+	defer testCleanupParameter(t, parameter.Name)
+	defer testCleanupParameterVersion(t, parameterVersion.Name)
+
+	var b bytes.Buffer
+	if err := getParamVersion(&b, tc.ProjectID, parameterID, parameterVersionID); err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := b.String(), fmt.Sprintf("Found parameter version %s with state enabled", parameterVersion.Name); !strings.Contains(got, want) {
+		t.Errorf("GetParameterVersion: expected %q to contain %q", got, want)
+	}
+}
+
+// TestRenderParamVersion tests the renderParamVersion function by creating a parameter,
+// its version, and a secret. It then attempts to render the created parameter version
+// and verifies if the parameter version was successfully rendered by checking the output.
+func TestRenderParamVersion(t *testing.T) {
+	tc := testutil.SystemTest(t)
+
+	parameter, parameterID := testParameter(t, tc.ProjectID, parametermanagerpb.ParameterFormat_JSON)
+	secret := testSecret(t, tc.ProjectID)
+	testSecretVersion(t, secret.Name, []byte("very secret data"))
+	payload := fmt.Sprintf(`{"username": "test-user","password": "__REF__(//secretmanager.googleapis.com/%s/versions/latest)"}`, secret.Name)
+	if err := testIamGrantAccess(t, secret.Name, parameter.PolicyMember.IamPolicyUidPrincipal); err != nil {
+		t.Fatal(err)
+	}
+	parameterVersion, parameterVersionID := testParameterVersion(t, tc.ProjectID, parameterID, payload)
+
+	defer testCleanupSecret(t, secret.Name)
+	defer testCleanupParameter(t, parameter.Name)
+	defer testCleanupParameterVersion(t, parameterVersion.Name)
+
+	var b bytes.Buffer
+	if err := renderParamVersion(&b, tc.ProjectID, parameterID, parameterVersionID); err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := b.String(), "Rendered parameter version:"; !strings.Contains(got, want) {
+		t.Errorf("RenderParameterVersion: expected %q to contain %q", got, want)
+	}
+
+	if got, want := b.String(), fmt.Sprintf(`Rendered payload: {"username": "test-user","password": "very secret data"}`); !strings.Contains(got, want) {
+		t.Errorf("RenderParameterVersion: expected %q to contain %q", got, want)
+	}
+}
+
+// TestListParamVersions tests the listParamVersion function by creating a parameter and its versions,
+// then attempts to list the created parameter versions. It verifies if the parameter versions
+// were successfully listed by checking the output.
+func TestListParamVersion(t *testing.T) {
+	tc := testutil.SystemTest(t)
+
+	parameter, parameterID := testParameter(t, tc.ProjectID, parametermanagerpb.ParameterFormat_JSON)
+	payload := `{"username": "test-user", "host": "localhost"}`
+	parameterVersion1, _ := testParameterVersion(t, tc.ProjectID, parameterID, payload)
+	parameterVersion2, _ := testParameterVersion(t, tc.ProjectID, parameterID, payload)
+
+	defer testCleanupParameter(t, parameter.Name)
+	defer testCleanupParameterVersion(t, parameterVersion1.Name)
+	defer testCleanupParameterVersion(t, parameterVersion2.Name)
+
+	var b bytes.Buffer
+	if err := listParamVersions(&b, tc.ProjectID, parameterID); err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := b.String(), fmt.Sprintf("Found parameter version %s with state enabled\n", parameterVersion1.Name); !strings.Contains(got, want) {
+		t.Errorf("ListParameterVersion: expected %q to contain %q", got, want)
+	}
+
+	if got, want := b.String(), fmt.Sprintf("Found parameter version %s with state enabled\n", parameterVersion2.Name); !strings.Contains(got, want) {
+		t.Errorf("ListParameterVersion: expected %q to contain %q", got, want)
+	}
+}
---
+++ parametermanager/render_param_version.go
@@ -0,0 +1,67 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package parametermanager
+
+// [START parametermanager_render_param_version]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	parametermanager "cloud.google.com/go/parametermanager/apiv1"
+	parametermanagerpb "cloud.google.com/go/parametermanager/apiv1/parametermanagerpb"
+)
+
+// renderParamVersion renders a parameter version using the Parameter Manager SDK for GCP.
+//
+// w: The io.Writer object used to write the output.
+// projectID: The ID of the project where the parameter is located.
+// parameterID: The ID of the parameter for which the version details is to be rendered.
+// versionID: The ID of the version to be rendered.
+//
+// The function returns an error if the parameter version rendering fails.
+func renderParamVersion(w io.Writer, projectID, parameterID, versionID string) error {
+	// Create a context and a Parameter Manager client.
+	ctx := context.Background()
+	client, err := parametermanager.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to create Parameter Manager client: %w", err)
+	}
+	defer client.Close()
+
+	// Construct the name of the parameter version to get render data.
+	name := fmt.Sprintf("projects/%s/locations/global/parameters/%s/versions/%s", projectID, parameterID, versionID)
+
+	// Build the request to render a parameter version.
+	req := &parametermanagerpb.RenderParameterVersionRequest{
+		Name: name,
+	}
+
+	// Call the API to render a parameter version.
+	rendered, err := client.RenderParameterVersion(ctx, req)
+	if err != nil {
+		return fmt.Errorf("failed to render parameter version: %w", err)
+	}
+
+	fmt.Fprintf(w, "Rendered parameter version: %s\n", rendered.ParameterVersion)
+
+	// If the parameter contains secret references, they will be resolved
+	// and the actual secret values will be included in the rendered output.
+	// Be cautious with logging or displaying this information.
+	fmt.Fprintf(w, "Rendered payload: %s\n", rendered.RenderedPayload)
+	return nil
+}
+
+// [END parametermanager_render_param_version]
