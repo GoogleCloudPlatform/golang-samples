d8e8da932eb4f2450bc099ec004d6a84d491da52
CadillacBurgess1 <emanuelburgess@google.com>
2025-04-09 00:07:09 +0000

    tidy

--- genai/text_generation/testdata/multi_img_test.go
+++ genai/text_generation/testdata/multi_img_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//    https://www.apache.org/licenses/LICENSE-2.0
+//	https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,12 +18,10 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
-
-	"github.com/GoogleCloudPlatform/golang-samples/genai/tools/text_generation"
 )
 
 func TestGenerateWithMultiImg(t *testing.T) {
-	
+
 	dummyImagePath := filepath.Join(os.TempDir(), "latte.jpg")
 	err := os.WriteFile(dummyImagePath, []byte("This is a dummy JPEG image."), 0644)
 	if err != nil {
--- genai/tools/testdata/code_exec_img_test.go
+++ genai/tools/testdata/code_exec_img_test.go
@@ -4,26 +4,24 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//    https://www.apache.org/licenses/LICENSE-2.0
+//	https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package tools_test 
+package tools_test
 
 import (
-    "bytes"
-    "testing"
-
-    "github.com/GoogleCloudPlatform/golang-samples/genai/tools"
+	"bytes"
+	"testing"
 )
 
 func TestGenerateWithCodeExecAndImg(t *testing.T) {
-    var buf bytes.Buffer
-    err := tools.generateWithCodeExecAndImg(&buf)
-    if err != nil {
-        t.Errorf("generateWithCodeExecAndImg failed: %v", err)
-    }
+	var buf bytes.Buffer
+	err := tools.generateWithCodeExecAndImg(&buf)
+	if err != nil {
+		t.Errorf("generateWithCodeExecAndImg failed: %v", err)
+	}
 }
