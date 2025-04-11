5d6058c8beec366329c886a2fa5362cb42b1126b
Tirthrajsinh Zala <tirthrajsinh.zala@crestdata.ai>
2025-04-11 18:43:48 +0530

    feat(modelarmor): code refactor

--- modelarmor/modelarmor_test.go
+++ modelarmor/modelarmor_test.go
@@ -88,12 +88,12 @@ func TestSanitizeModelResponse(t *testing.T) {
 	templateID := fmt.Sprintf("test-model-armor-%s", uuid.New().String())
 
 	var b bytes.Buffer
-	if _, err := createModelArmorTemplate(&b, tc.ProjectID, "us-central1", templateID); err != nil {
+	if _, err := createModelArmorTemplate(&b, tc.ProjectID, testLocation(t), templateID); err != nil {
 		t.Fatal(err)
 	}
-	defer testCleanupTemplate(t, fmt.Sprintf("projects/%s/locations/%s/templates/%s", tc.ProjectID, "us-central1", templateID))
+	defer testCleanupTemplate(t, fmt.Sprintf("projects/%s/locations/%s/templates/%s", tc.ProjectID, testLocation(t), templateID))
 
-	if _, err := sanitizeModelResponse(&b, tc.ProjectID, "us-central1", templateID, modelResponse); err != nil {
+	if _, err := sanitizeModelResponse(&b, tc.ProjectID, testLocation(t), templateID, modelResponse); err != nil {
 		t.Fatal(err)
 	}
 
