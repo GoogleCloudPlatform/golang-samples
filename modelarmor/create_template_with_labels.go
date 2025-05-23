df79804b16cfcb815b55ed963b18bd5148283770
tirthrajsinh-zala-crest <tirthrajsinh.zala@crestdata.ai>
2025-05-23 13:59:48 +0530

    feat(modelarmor): #9 Added snippets for create template with labels and metadata (#5273)
    * feat(modelarmor):  Added samples for creating, listing, updating and deleting model armor templates
    
    * feat(modelarmor): Added samples for creating, listing, updating and deleting modelarmor templates
    
    * feat(modelarmor): resolve review comments
    
    * feat(modelarmor): added samples for to create ma template with labels and metadata
    
    * feat(modelarmor): resolve review comments and code refactored
    
    * feat(modelarmor): refactor code
    
    * feat(modelarmor): code refactor
    
    * feat(modelarmor): resolve conflicts
    
    * feat(modelarmor): resolve conflicts
    
    * feat(modelarmor): resolve conflicts
    
    * feat(modelarmor): resolve comments
    
    * feat(modelarmor): resolve comments
    
    * feat(modelarmor): update-template-metadata-options
    
    * feat(modelarmor):fix-lints-after-updating-branch
    
    ---------
    
    Co-authored-by: Eric Schmidt <erschmid@google.com>
    Co-authored-by: harshnasitcrest <131268456+harshnasitcrest@users.noreply.github.com>

---
+++ modelarmor/create_template_with_labels.go
@@ -0,0 +1,94 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Sample code for creating a new model armor template with labels.
+
+package modelarmor
+
+// [START modelarmor_create_template_with_labels]
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	modelarmor "cloud.google.com/go/modelarmor/apiv1"
+	modelarmorpb "cloud.google.com/go/modelarmor/apiv1/modelarmorpb"
+	"google.golang.org/api/option"
+)
+
+// createModelArmorTemplateWithLabels method creates a
+// new Model Armor template with custom labels.
+//
+// w io.Writer: The writer to use for logging.
+// projectID string: The ID of the Google Cloud project.
+// locationID string: The ID of the Google Cloud location.
+// templateID string: The ID of the template to create.
+// labels map[string]string: A map of custom labels to apply to the template.
+func createModelArmorTemplateWithLabels(w io.Writer, projectID, locationID, templateID string, labels map[string]string) error {
+	ctx := context.Background()
+
+	// Create options for Model Armor client.
+	endpoint := fmt.Sprintf("modelarmor.%s.rep.googleapis.com:443", locationID)
+	opts := option.WithEndpoint(endpoint)
+	// Create the Model Armor client.
+	client, err := modelarmor.NewClient(ctx, opts)
+	if err != nil {
+		return fmt.Errorf("failed to create client for project %s, location %s: %w", projectID, locationID, err)
+	}
+	defer client.Close()
+
+	parent := fmt.Sprintf("projects/%s/locations/%s", projectID, locationID)
+
+	// Build the Model Armor template with your preferred filters.
+	// For more details on filters, please refer to the following doc:
+	// [https://cloud.google.com/security-command-center/docs/key-concepts-model-armor#ma-filters](https://cloud.google.com/security-command-center/docs/key-concepts-model-armor#ma-filters)
+	template := &modelarmorpb.Template{
+		FilterConfig: &modelarmorpb.FilterConfig{
+			RaiSettings: &modelarmorpb.RaiFilterSettings{
+				RaiFilters: []*modelarmorpb.RaiFilterSettings_RaiFilter{
+					{
+						FilterType:      modelarmorpb.RaiFilterType_HATE_SPEECH,
+						ConfidenceLevel: modelarmorpb.DetectionConfidenceLevel_HIGH,
+					},
+					{
+						FilterType:      modelarmorpb.RaiFilterType_SEXUALLY_EXPLICIT,
+						ConfidenceLevel: modelarmorpb.DetectionConfidenceLevel_MEDIUM_AND_ABOVE,
+					},
+				},
+			},
+		},
+		Labels: labels,
+	}
+
+	// Prepare the request for creating the template.
+	req := &modelarmorpb.CreateTemplateRequest{
+		Parent:     parent,
+		TemplateId: templateID,
+		Template:   template,
+	}
+
+	// Create the template.
+	response, err := client.CreateTemplate(ctx, req)
+	if err != nil {
+		return fmt.Errorf("failed to create template: %w", err)
+	}
+
+	// Print the new template name using fmt.Fprintf with the io.Writer.
+	fmt.Fprintf(w, "Created Template with labels: %s\n", response.Name)
+
+	return nil
+}
+
+// [END modelarmor_create_template_with_labels]
