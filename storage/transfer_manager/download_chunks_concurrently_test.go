ed5d8003fe1aee5d2375eb0be8b86e2d990d2e35
cpriti-os <cpriti@google.com>
2025-10-24 06:04:22 +0000

    Improve comments and tests for download_chunks_concurrently

--- storage/transfer_manager/download_chunks_concurrently_test.go
+++ storage/transfer_manager/download_chunks_concurrently_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,19 +18,26 @@ import (
 	"bytes"
 	"context"
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> d9e9ef31 (Improve comments and tests for download_chunks_concurrently)
 	"crypto/rand"
 	"fmt"
 	"io"
 	"log"
+<<<<<<< HEAD
 =======
 	"fmt"
 >>>>>>> df0f473c (Add sample download_chunks_concurrently with test.)
+=======
+>>>>>>> d9e9ef31 (Improve comments and tests for download_chunks_concurrently)
 	"os"
 	"strings"
 	"testing"
 
 	"cloud.google.com/go/storage"
 	"github.com/GoogleCloudPlatform/golang-samples/internal/testutil"
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 	"github.com/google/uuid"
@@ -104,46 +111,82 @@ func TestDownloadChunksConcurrently(t *testing.T) {
 	"google.golang.org/api/iterator"
 =======
 >>>>>>> 2ed49f9f (Improve test and comments.)
+=======
+	"github.com/google/uuid"
+>>>>>>> d9e9ef31 (Improve comments and tests for download_chunks_concurrently)
 )
 
-func TestDownloadChunksConcurrently(t *testing.T) {
-	tc := testutil.SystemTest(t)
-	bucketName := tc.ProjectID + "-tm-bucket-test"
-	blobName := "tm-blob-test"
-	fileName := "tm-file-test"
+const (
+	testPrefix     = "storage-objects-test"
+	downloadObject = "tm-obj-download"
+)
 
+var (
+	tmBucketName  string
+	storageClient *storage.Client
+	downloadData  []byte
+)
+
+func TestMain(m *testing.M) {
 	ctx := context.Background()
-	client, err := storage.NewClient(ctx)
+	tc, _ := testutil.ContextMain(m)
+
+	var err error
+
+	// Create fixture client & bucket to use across tests.
+	storageClient, err = storage.NewClient(ctx)
 	if err != nil {
-		t.Fatalf("storage.NewClient: %v", err)
+		log.Fatalf("storage.NewClient: %v", err)
 	}
-	defer client.Close()
-
-	bucket := client.Bucket(bucketName)
+	tmBucketName = fmt.Sprintf("%s-%s", testPrefix, uuid.New().String())
+	bucket := storageClient.Bucket(tmBucketName)
 	if err := bucket.Create(ctx, tc.ProjectID, nil); err != nil {
-		t.Fatalf("Bucket(%q).Create: %v", bucketName, err)
+		log.Fatalf("Bucket(%q).Create: %v", tmBucketName, err)
 	}
-	// Clean up the bucket at the end of the test.
-	defer func() {
-		if err := testutil.DeleteBucketIfExists(ctx, client, bucketName); err != nil {
-			t.Logf("testutil.DeleteBucketIfExists: %v", err)
-		}
-	}()
 
-	obj := bucket.Object(blobName)
-	w := obj.NewWriter(ctx)
-	if _, err := fmt.Fprint(w, "hello world"); err != nil {
-		t.Fatalf("Writer.Write: %v", err)
+	// Create object fixture for download tests.
+	w := bucket.Object(downloadObject).NewWriter(ctx)
+	downloadData = make([]byte, 2*1024*1024) // 2 MiB
+	if _, err := rand.Read(downloadData); err != nil {
+		log.Fatalf("rand.Read: %v", err)
+	}
+	if _, err := io.Copy(w, bytes.NewReader(downloadData)); err != nil {
+		log.Fatalf("uploading object: %v", err)
 	}
 	if err := w.Close(); err != nil {
-		t.Fatalf("Writer.Close: %v", err)
+		log.Fatalf("closing writer: %v", err)
 	}
 >>>>>>> df0f473c (Add sample download_chunks_concurrently with test.)
+
+	// Run tests.
+	exitCode := m.Run()
+
+	// Cleanup bucket and objects.
+	if err := testutil.DeleteBucketIfExists(ctx, storageClient, tmBucketName); err != nil {
+		log.Printf("deleting bucket: %v", err)
+	}
+	os.Exit(exitCode)
+}
+
+func TestDownloadChunksConcurrently(t *testing.T) {
+	bucketName := tmBucketName
+	blobName := downloadObject
+
+	// Create a temporary file to download to, ensuring we have permissions
+	// and the file is cleaned up.
+	f, err := os.CreateTemp("", "tm-file-test-")
+	if err != nil {
+		t.Fatalf("os.CreateTemp: %v", err)
+	}
+	fileName := f.Name()
+	f.Close() // Close the file so the download can write to it.
+	defer os.Remove(fileName)
 
 	var buf bytes.Buffer
 	if err := downloadChunksConcurrently(&buf, bucketName, blobName, fileName); err != nil {
 		t.Errorf("downloadChunksConcurrently: %v", err)
 	}
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 =======
@@ -157,6 +200,8 @@ func TestDownloadChunksConcurrently(t *testing.T) {
 		}
 	}()
 >>>>>>> e5956298 (Improve cleanup in download_chunks_concurrently_test.go)
+=======
+>>>>>>> d9e9ef31 (Improve comments and tests for download_chunks_concurrently)
 
 	if got, want := buf.String(), fmt.Sprintf("Downloaded %v to %v", blobName, fileName); !strings.Contains(got, want) {
 		t.Errorf("got %q, want to contain %q", got, want)
@@ -164,6 +209,9 @@ func TestDownloadChunksConcurrently(t *testing.T) {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> d9e9ef31 (Improve comments and tests for download_chunks_concurrently)
 
 	// Verify that the downloaded data is the same as the uploaded data.
 	downloadedBytes, err := os.ReadFile(fileName)
@@ -173,6 +221,7 @@ func TestDownloadChunksConcurrently(t *testing.T) {
 
 	if !bytes.Equal(downloadedBytes, downloadData) {
 		t.Errorf("downloaded data does not match uploaded data. got %d bytes, want %d bytes", len(downloadedBytes), len(downloadData))
+<<<<<<< HEAD
 =======
 }
 
@@ -204,4 +253,7 @@ func deleteBucket(ctx context.Context, t *testing.T, bucket *storage.BucketHandl
 	}
 =======
 >>>>>>> e5956298 (Improve cleanup in download_chunks_concurrently_test.go)
+=======
+	}
+>>>>>>> d9e9ef31 (Improve comments and tests for download_chunks_concurrently)
 }
