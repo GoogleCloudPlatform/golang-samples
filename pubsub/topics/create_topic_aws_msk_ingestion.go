ac2c4e24529b5890fb7b1c425385336382e568ff
Alex Hong <9397363+hongalex@users.noreply.github.com>
2025-11-06 11:11:47 -0800

    feat(pubsub/v2): add missing ingestion samples

---
+++ pubsub/topics/create_topic_aws_msk_ingestion.go
@@ -0,0 +1,64 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package topics
+
+// [START pubsub_create_topic_with_aws_msk_ingestion]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"cloud.google.com/go/pubsub/v2"
+	"cloud.google.com/go/pubsub/v2/apiv1/pubsubpb"
+)
+
+func createTopicWithAWSMSKIngestion(w io.Writer, projectID, topicID, clusterARN, mskTopic, awsRoleARN, gcpSA string) error {
+	// projectID := "my-project-id"
+	// topicID := "my-topic"
+
+	// // AWS MSK ingestion settings.
+	// clusterARN := "cluster-arn"
+	// mskTopic := "msk-topic"
+	// awsRoleARN := "aws-role-arn"
+	// gcpSA := "gcp-service-account"
+
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("pubsub.NewClient: %w", err)
+	}
+	defer client.Close()
+
+	topicpb := &pubsubpb.Topic{
+		IngestionDataSourceSettings: &pubsubpb.IngestionDataSourceSettings{
+			Source: &pubsubpb.IngestionDataSourceSettings_AwsMsk_{
+				AwsMsk: &pubsubpb.IngestionDataSourceSettings_AwsMsk{
+					ClusterArn:        clusterARN,
+					Topic:             mskTopic,
+					AwsRoleArn:        awsRoleARN,
+					GcpServiceAccount: gcpSA,
+				},
+			},
+		},
+	}
+	topic, err := client.TopicAdminClient.CreateTopic(ctx, topicpb)
+	if err != nil {
+		return fmt.Errorf("CreateTopic: %w", err)
+	}
+	fmt.Fprintf(w, "Created topic with AWS MSK ingestion settings: %v\n", topic)
+	return nil
+}
+
+// [END pubsub_create_topic_with_aws_msk_ingestion]
---
+++ pubsub/topics/create_topic_confluent_cloud_ingestion.go
@@ -0,0 +1,66 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package topics
+
+// [START pubsub_create_topic_with_confluent_cloud_ingestion]
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"cloud.google.com/go/pubsub/v2"
+	"cloud.google.com/go/pubsub/v2/apiv1/pubsubpb"
+)
+
+func createTopicWithConfluentCloudIngestion(w io.Writer, projectID, topicID, bootstrapServer, clusterID, confluentTopic, poolID, gcpSA string) error {
+	// projectID := "my-project-id"
+	// topicID := "my-topic"
+
+	// // Confluent Cloud ingestion settings.
+	// bootstrapServer := "bootstrap-server"
+	// clusterID := "cluster-id"
+	// confluentTopic := "confluent-topic"
+	// poolID := "identity-pool-id"
+	// gcpSA := "gcp-service-account"
+
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("pubsub.NewClient: %w", err)
+	}
+	defer client.Close()
+
+	topicpb := &pubsubpb.Topic{
+		IngestionDataSourceSettings: &pubsubpb.IngestionDataSourceSettings{
+			Source: &pubsubpb.IngestionDataSourceSettings_ConfluentCloud_{
+				ConfluentCloud: &pubsubpb.IngestionDataSourceSettings_ConfluentCloud{
+					BootstrapServer:   bootstrapServer,
+					ClusterId:         clusterID,
+					Topic:             confluentTopic,
+					IdentityPoolId:    poolID,
+					GcpServiceAccount: gcpSA,
+				},
+			},
+		},
+	}
+	topic, err := client.TopicAdminClient.CreateTopic(ctx, topicpb)
+	if err != nil {
+		return fmt.Errorf("CreateTopic: %w", err)
+	}
+	fmt.Fprintf(w, "Created topic with Confluent Cloud ingestion: %v\n", topic)
+	return nil
+}
+
+// [END pubsub_create_topic_with_confluent_cloud_ingestion]
