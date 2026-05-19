b7264cdc2a85a365ca99379702bbfd56e5cfc09c
Baha Aiman <bahaaiman@google.com>
2026-05-19 08:51:43 +0000

    feat(bigtable): add missing delete samples in Go

---
+++ bigtable/deletes/delete_from_column_family.go
@@ -0,0 +1,53 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package deletes
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"cloud.google.com/go/bigtable"
+)
+
+// [START bigtable_delete_from_column_family]
+
+func deleteFromColumnFamily(w io.Writer, projectID, instanceID, tableName string) error {
+	// projectID := "my-project-id"
+	// instanceID := "my-instance-id"
+	// tableName := "mobile-time-series"
+
+	ctx := context.Background()
+	client, err := bigtable.NewClient(ctx, projectID, instanceID)
+	if err != nil {
+		return fmt.Errorf("bigtable.NewClient: %w", err)
+	}
+	defer client.Close()
+	tbl := client.Open(tableName)
+
+	rowKey := "phone#5c10102#20190501"
+	columnFamilyName := "stats_summary"
+	mut := bigtable.NewMutation()
+	mut.DeleteCellsInFamily(columnFamilyName)
+
+	if err := tbl.Apply(ctx, rowKey, mut); err != nil {
+		return fmt.Errorf("Apply: %w", err)
+	}
+
+	fmt.Fprintf(w, "Successfully deleted cells from family %s for row: %s\n", columnFamilyName, rowKey)
+	return nil
+}
+
+// [END bigtable_delete_from_column_family]
